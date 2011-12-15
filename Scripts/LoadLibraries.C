38aa37ea1e837e1a96545519f390b19de77939bc
Ben LaRoque <benjamin.laroque@gmail.com>
2011-12-15 15:26:18 -0800

    LoadLibraries.C now knows the full path (via KatydidConfig_cmake.hh) to the installed libraries.

--- Scripts/LoadLibraries.C
+++ Scripts/LoadLibraries.C
@@ -6,16 +6,23 @@
  */
 
 #include <cstdio>
+#include "../cbuild/KatydidConfig.hh"
 
-void LoadLibraries(const char* dirRelativeToKatydid, Bool_t printDir=kFALSE) {
+void LoadLibraries(Bool_t printDir=kFALSE) {
     // establish the directory where the libraries are located relative to the run location
     char libDir[512];
-    sprintf(libDir, "%s/lib", dirRelativeToKatydid);
+    sprintf(libDir, "%s/lib", INSTALL_PREFIX);
     if (printDir) printf("lib dir: %s", libDir);
 
     // load libEgg
     char libEgg[512];
-    sprintf(libEgg, "%s/libEgg.so", libDir);
+    sprintf(libEgg, "%s/libKatydidEgg.so", libDir);
     if (printDir) printf("libEgg: ", libEgg);
     gSystem->Load(libEgg);
+
+    // load libFFT
+    char libFFT[512];
+    sprintf(libFFT, "%s/libKatydidFFT.so", libDir);
+    if (printDir) printf("libFFT: ", libFFT);
+    gSystem->Load(libFFT);
 }
