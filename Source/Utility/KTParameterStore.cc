b13786ba6bef7a6d3fd3cec08ef9b3d405b0cb5e
Noah Oblath <nsoblath@mit.edu>
2012-07-02 17:12:30 -0400

    Added KTParameterStore, and (only) implemented the parameter-access side of the interface.  Added a validation executable.

---
+++ Source/Utility/KTParameterStore.cc
@@ -0,0 +1,23 @@
+/*
+ * KTParameterStore.cc
+ *
+ *  Created on: Jul 2, 2012
+ *      Author: nsoblath
+ */
+
+#include "KTParameterStore.hh"
+
+namespace Katydid
+{
+
+    KTParameterStore::KTParameterStore() :
+            dummy()
+    {
+        dummy = 0;
+    }
+
+    KTParameterStore::~KTParameterStore()
+    {
+    }
+
+} /* namespace Katydid */
