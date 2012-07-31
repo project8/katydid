670ab4fd40f90fd2b245318c06e0222eb6888638
Noah Oblath <nsoblath@mit.edu>
2012-07-31 11:53:52 -0400

    Fixed error in the test config file and a few minor problems in the config reading code.

--- Source/Applications/Validation/KTTestConfigurable.cc
+++ Source/Applications/Validation/KTTestConfigurable.cc
@@ -5,12 +5,15 @@
  *      Author: nsoblath
  */
 
+#include "KTLogger.hh"
 #include "KTTestConfigurable.hh"
 
 using std::string;
 
 namespace Katydid
 {
+
+    KTLOGGER(testparamlog, "katydid.applications.validation");
 
     KTTestConfigurable::KTTestConfigurable() :
             fIntData(-9),
@@ -28,19 +31,19 @@ namespace Katydid
         if (node->HasData("int_data"))
         {
             fIntData = node->GetData< Int_t >("int_data", -1);
-            return true;
+            KTINFO(testparamlog, "Configured integer: " << fIntData);
         }
         if (node->HasData("double_data"))
         {
             fDoubleData = node->GetData< Double_t >("double_data", -11.);
-            return true;
+            KTINFO(testparamlog, "Configured double: " << fDoubleData);
         }
         if (node->HasData("string_data"))
         {
             fStringData = node->GetData<string>("string_data", "no value");
-            return true;
+            KTINFO(testparamlog, "Configured string: " << fStringData);
         }
-        return false;
+        return true;
     }
 
 } /* namespace Katydid */
--- Source/Applications/Validation/TestParameterStore.cc
+++ Source/Applications/Validation/TestParameterStore.cc
@@ -8,8 +8,13 @@
  *    1. Config filename (use TestParameterStoreConfig.json
  */
 
+#include "KTLogger.hh"
 #include "KTParameterStore.hh"
+#include "KTPStoreNode.hh"
 #include "KTTestConfigurable.hh"
+
+//#include <boost/property_tree/ptree.hpp>
+//#include <boost/property_tree/json_parser.hpp>
 
 #include <iostream>
 #include <string>
@@ -17,14 +22,47 @@
 using namespace Katydid;
 using namespace std;
 
+KTLOGGER(testparamlog, "katydid.applications.validation");
+
 int main(int argc, char** argv)
 {
     string configFilename(argv[1]);
+/*
+    using boost::property_tree::ptree;
+    ptree pt;
+
+    try
+    {
+        read_json(configFilename, pt);
+    }
+    catch (boost::property_tree::json_parser::json_parser_error& e)
+    {
+        cout << "Error reading config file: " << e.what() << endl;
+        return -1;
+    }
+
+    return 0;
+*/
 
     KTParameterStore* store = KTParameterStore::GetInstance();
     store->ReadConfigFile(configFilename);
 
+    KTPStoreNode* topNode = store->GetNode("TestConfigurable");
+    if (topNode == NULL)
+    {
+        KTERROR(testparamlog, "Top-level node was not found");
+        return -1;
+    }
+
     KTTestConfigurable* testObj = new KTTestConfigurable();
+    if (testObj->Configure(topNode))
+    {
+        KTINFO(testparamlog, "Configuration complete");
+    }
+    else
+    {
+        KTERROR(testparamlog, "Something went wrong during the configuration");
+    }
 
     delete testObj;
 
