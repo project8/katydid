1506d451079e1e2af6405c1266bc13885dc81e74
Noah Oblath <nsoblath@mit.edu>
2012-08-06 20:14:22 -0400

    Partial adaptation of KTEgg.

--- Source/Egg/KTEgg.cc
+++ Source/Egg/KTEgg.cc
@@ -7,260 +7,63 @@
 
 #include "KTEgg.hh"
 
+#include "KTEggHeader.hh"
 #include "KTEvent.hh"
 
-#include "rapidxml.hpp"
-//#include "rapidxml_print.hpp"
+#include "Monarch.hpp"
+#include "MonarchIO.hpp"
 
-#include <cstring>
-#include <iostream>
-#include <vector>
+#include <string>
 
-using std::ifstream;
-using std::fstream;
 using std::string;
-using std::stringstream;
-using std::vector;
 
 namespace Katydid
 {
 
-    const ifstream::pos_type KTEgg::sPreludeSize = 9;
-
     KTEgg::KTEgg() :
-            fFileName(),
-            fEggStream(),
-            fPrelude(),
-            fHeaderSize(0),
-            fHeader(),
-            fHeaderInfo()
+            fMonarch(NULL),
+            fHeader(new KTEggHeader())
     {
     }
 
     KTEgg::~KTEgg()
     {
-        if (fEggStream.is_open()) fEggStream.close();
+        if (fMonarch != NULL)
+        {
+            fMonarch->Close();
+            delete fMonarch;
+        }
+        delete fHeader;
     }
 
-    bool KTEgg::BreakEgg()
+    bool KTEgg::BreakEgg(const string& filename)
     {
-        if (fEggStream.is_open()) fEggStream.close();
-
-        // open the file stream
-        fEggStream.open(fFileName.c_str(), ifstream::in|ifstream::binary);
-
-        if (! fEggStream.is_open())
+        if (fMonarch != NULL)
         {
-            return false;
+            fMonarch->Close();
+            delete fMonarch;
         }
 
-        // read the prelude (which states how long the header is in hex)
-        // add one to the size of the array to allow it to terminate in a null character
-        //int readBufferSize = (int)sPreludeSize + 1;
-        char* readBuffer = new char [(int)sPreludeSize];
-        int readSize = (int)sPreludeSize - 1;
-        fEggStream.read(readBuffer, readSize);
-        if (! fEggStream.good()) return false;
-        string newPrelude(readBuffer, sPreludeSize);
-        this->SetPrelude(newPrelude);
+        // open the file
+        fMonarch = Monarch::Open(filename, MonarchIO::ReadMode);
 
-        // convert the prelude to the header size
-        stringstream conversion;
-        conversion << readBuffer;
-        conversion >> std::hex >> fHeaderSize;
-        //std::cout << "header size: " << fHeaderSize << std::endl;
-
-        // read the header
-        delete [] readBuffer;
-        readBuffer = new char [fHeaderSize];
-        fEggStream.read(readBuffer, fHeaderSize);
-        if (! fEggStream.good()) return false;
-        string newHeader(readBuffer, fHeaderSize);
-        this->SetHeader(newHeader);
-        std::cout << "Header: " << newHeader << std::endl;
-
-        delete [] readBuffer;
-
-        return true;
-    }
-
-    bool KTEgg::ParseEggHeader()
-    {
-        // these items aren't included in the header, but maybe will be someday?
-        this->SetHertzPerSampleRateUnit(1.e6);
-        this->SetSecondsPerRunLengthUnit(1.e-3);
-
-        rapidxml::xml_document<char> headerDOM;
-        char* headerCopy = new char [fHeader.size() + 1];
-        strcpy(headerCopy, fHeader.c_str());
-        try
-        {
-            headerDOM.parse<0>(headerCopy);
-        }
-        catch (rapidxml::parse_error& e)
-        {
-            std::cout << e.what() << std::endl;
-            return false;
-        }
-        //std::cout << headerDOM;
-
-        rapidxml::xml_node<char>* nodeHeader = headerDOM.first_node("header");
-        if (nodeHeader == NULL)
-        {
-            std::cerr << "No header node" << std::endl;
-            return false;
-        }
-
-        rapidxml::xml_node<char>* nodeDataFormat = nodeHeader->first_node("data_format");
-        if (nodeDataFormat == NULL)
-        {
-            std::cerr << "No data format node" << std::endl;
-            return false;
-        }
-
-        rapidxml::xml_attribute<char>* attr = nodeDataFormat->first_attribute("id");
-        if (attr == NULL)        {
-            std::cerr << "No id attribute in the data format node" << std::endl;
-            return false;
-        };
-        this->SetFrameIDSize(ConvertFromCharArray< int >(attr->value()));
-
-        attr = nodeDataFormat->first_attribute("ts");
-        if (attr == NULL)
-        {
-            std::cerr << "No ts attribute in the data format node" << std::endl;
-            return false;
-        }
-        this->SetTimeStampSize(ConvertFromCharArray< int >(attr->value()));
-
-        attr = nodeDataFormat->first_attribute("data");
-        if (attr == NULL)
-        {
-            std::cerr << "No data attribute in the data format node" << std::endl;
-            return false;
-        }
-        this->SetRecordSize(ConvertFromCharArray< int >(attr->value()));
-
-        this->SetEventSize(this->GetFrameIDSize() + this->GetTimeStampSize() + this->GetRecordSize());
-
-        rapidxml::xml_node<char>* nodeDigitizer = nodeHeader->first_node("digitizer");
-        if (nodeDigitizer == NULL)
-        {
-            std::cerr << "No digitizer node" << std::endl;
-            return false;
-        }
-
-        attr = nodeDigitizer->first_attribute("rate");
-        if (attr == NULL)
-        {
-            std::cerr << "No rate attribute in the digitizer node" << std::endl;
-            return false;
-        }
-        this->SetSampleRate(ConvertFromCharArray< double >(attr->value()) * this->GetHertzPerSampleRateUnit());
-
-        rapidxml::xml_node<char>* nodeRun = nodeHeader->first_node("run");
-        if (nodeRun == NULL)
-        {
-            std::cerr << "No run node" << std::endl;
-            return false;
-        }
-
-        attr = nodeRun->first_attribute("length");
-        if (attr == NULL)        {
-            std::cerr << "No length attribute in the run node" << std::endl;
-            return false;
-        }
-        this->SetRunLength(ConvertFromCharArray< double >(attr->value()) * this->GetSecondsPerRunLengthUnit());
-
-        delete [] headerCopy;
-
-        std::cout << "Parsed header:\n";
-        std::cout << "Frame ID Size: " << this->GetFrameIDSize() << '\n';
-        std::cout << "Time Stamp Size: " << this->GetTimeStampSize() << '\n';
-        std::cout << "Record Size: " << this->GetRecordSize() << '\n';
-        std::cout << "Run Length: " << this->GetRunLength() << " s" << '\n';
-        std::cout << "Sample Rate: " << this->GetSampleRate() << " Hz " << '\n';
+        fHeader->TakeInformation(fMonarch->GetHeader());
 
         return true;
     }
 
     KTEvent* KTEgg::HatchNextEvent()
     {
-        if (! fEggStream.good()) return NULL;
+        if (fMonarch == NULL) return NULL;
 
         KTEvent* event = new KTEvent();
 
-        unsigned char* readBuffer;
+        // TODO: Figure out how to fill all of these things out
 
-        // read the time stamp
-        readBuffer = new unsigned char [this->GetTimeStampSize()];
-        fEggStream.read((char*)(&readBuffer[0]), this->GetTimeStampSize());
-        if (fEggStream.gcount() != this->GetTimeStampSize())
-        {
-            std::cerr << "Warning from KTEgg::HatchNextEvent: Size of the data read for the time stamp did not match the size expected" << std::endl;
-            std::cerr << "   Expected: " << this->GetTimeStampSize() << "   Read: " << fEggStream.gcount() << std::endl;
-            delete [] readBuffer;
-        }
-        else
-        {
-            vector< unsigned > newTimeStamp(readBuffer, readBuffer + this->GetTimeStampSize()/sizeof(unsigned char));
-            delete [] readBuffer;
-            event->SetTimeStamp(newTimeStamp);
-            std::cout << "Time stamp (" << newTimeStamp.size() << " chars): ";
-            for (int i=0; i<newTimeStamp.size(); i++)
-                std::cout << newTimeStamp[i];
-            std::cout << std::endl;
-        }
-        if (! fEggStream.good())
-        {
-            std::cerr << "Warning from KTEgg::HatchNextEvent: Reached end of file after reading time stamp size" << std::endl;
-            delete event;
-            return NULL;
-        }
+        event->SetTimeStamp(newTimeStamp);
+        event->SetFrameID(newFrameID);
+        event->SetRecord(newRecord);
 
-        // read the frame size
-        readBuffer = new unsigned char [this->GetFrameIDSize()];
-        fEggStream.read((char*)(&readBuffer[0]), this->GetFrameIDSize());
-        if (fEggStream.gcount() != this->GetFrameIDSize())
-        {
-            std::cerr << "Warning from KTEgg::HatchNextEvent: The size of the data read for the frame ID did not match the expected size" << std::endl;
-            std::cerr << "   Expected: " << this->GetFrameIDSize() << "   Read: " << fEggStream.gcount() << std::endl;
-            delete [] readBuffer;
-        }
-        else
-        {
-            vector< unsigned > newFrameID(readBuffer, readBuffer + this->GetFrameIDSize()/sizeof(unsigned char));
-            delete [] readBuffer;
-            event->SetFrameID(newFrameID);
-        }
-        if (! fEggStream.good())
-        {
-            std::cerr << "Warning from KTEgg::HatchNextEvent: Reached end of file after reading frame size" << std::endl;
-            delete event;
-            return NULL;
-        }
-
-        // read the record
-        readBuffer = new unsigned char [this->GetRecordSize()];
-        fEggStream.read((char*)(&readBuffer[0]), this->GetRecordSize());
-        if (fEggStream.gcount() != this->GetRecordSize())
-        {
-            std::cerr << "Warning from KTEgg::HatchNextEvent: Size of the data read for the record did not match the amount expected" << std::endl;
-            std::cerr << "   Expected: :" << this->GetRecordSize() << "  Read: " << fEggStream.gcount() << std::endl;
-            delete [] readBuffer;
-            delete event;
-            return NULL;
-        }
-        else
-        {
-            vector< unsigned > newRecord(readBuffer, readBuffer + this->GetRecordSize()/sizeof(unsigned char));
-            delete [] readBuffer;
-            event->SetRecord(newRecord);
-        }
-        if (! fEggStream.good())
-        {
-            std::cerr << "Warning from KTEgg::HatchNextEvent: Egg stream state is not good after reading in this event." << std::endl;
-        }
 
         //
         event->SetSampleRate(this->GetSampleRate());
--- Source/Egg/KTEgg.hh
+++ Source/Egg/KTEgg.hh
@@ -9,241 +9,40 @@
 #ifndef KTEGG_HH_
 #define KTEGG_HH_
 
-#include <algorithm>
-#include <fstream>
 #include <string>
-#include <sstream>
+
+class Monarch;
 
 namespace Katydid
 {
     class KTEvent;
+    class KTEggHeader;
 
     class KTEgg
     {
         public:
-            struct HeaderInfo
-            {
-                int fEventSize;
-                int fFrameIDSize;
-                int fRecordSize;
-                int fTimeStampSize;
-                double fRunLength;
-                double fSampleRate;
-                double fHertzPerSampleRateUnit;
-                double fSecondsPerRunLengthUnit;
-
-                HeaderInfo() :
-                    fTimeStampSize(0),
-                    fFrameIDSize(0),
-                    fRecordSize(0),
-                    fEventSize(0),
-                    fRunLength(0.),
-                    fSampleRate(0.),
-                    fHertzPerSampleRateUnit(1.),
-                    fSecondsPerRunLengthUnit(1.)
-                {}
-            };
-
-        public:
             KTEgg();
             virtual ~KTEgg();
 
-            bool BreakEgg();
-            bool ParseEggHeader();
+            bool BreakEgg(const std::string& filename);
             KTEvent* HatchNextEvent();
 
-            const std::string& GetFileName() const;
-            const std::ifstream& GetEggStream() const;
-            unsigned GetHeaderSize() const;
-            const std::string& GetHeader() const;
-            const std::string& GetPrelude() const;
-
-            const HeaderInfo& GetHeaderInfo() const;
-            int GetEventSize() const;
-            int GetFrameIDSize() const;
-            int GetRecordSize() const;
-            double GetRunLength() const;
-            double GetSampleRate() const;
-            int GetTimeStampSize() const;
+            KTEggHeader* GetHeader();
 
             double GetHertzPerSampleRateUnit() const;
             double GetSecondsPerRunLengthUnit() const;
 
-
-            void SetFileName(const std::string& fileName);
-            void SetHeaderSize(unsigned size);
-            void SetHeader(const std::string& header);
-            void SetPrelude(const std::string& prelude);
-
-            void SetEventSize(int size);
-            void SetFrameIDSize(int size);
-            void SetRecordSize(int size);
-            void SetRunLength(double length);
-            void SetSampleRate(double rate);
-            void SetTimeStampSize(int size);
+            void SetHeader(KTEggHeader* header);
 
             void SetHertzPerSampleRateUnit(double hpsru);
             void SetSecondsPerRunLengthUnit(double sprlu);
 
         private:
-            template< typename XReturnType >
-            XReturnType ConvertFromCharArray(char* value);
+            Monarch* fMonarch;
 
-            std::string fFileName;
-            std::ifstream fEggStream;
-            std::string fPrelude;
-            unsigned fHeaderSize;
-            std::string fHeader;
-
-            HeaderInfo fHeaderInfo;
-
-            static const std::ifstream::pos_type sPreludeSize;  // the prelude size is currently restricted to eight bytes
+            KTEggHeader* fHeader;
 
     };
-
-
-    inline const std::string& KTEgg::GetFileName() const
-    {
-        return fFileName;
-    }
-
-    inline const std::ifstream& KTEgg::GetEggStream() const
-    {
-        return fEggStream;
-    }
-
-    inline unsigned KTEgg::GetHeaderSize() const
-    {
-        return fHeaderSize;
-    }
-
-    inline const std::string& KTEgg::GetHeader() const
-    {
-        return fHeader;
-    }
-
-    inline const std::string& KTEgg::GetPrelude() const
-    {
-        return fPrelude;
-    }
-
-    inline const KTEgg::HeaderInfo& KTEgg::GetHeaderInfo() const
-    {
-        return fHeaderInfo;
-    }
-
-    inline int KTEgg::GetEventSize() const
-    {
-        return fHeaderInfo.fEventSize;
-    }
-
-    inline int KTEgg::GetFrameIDSize() const
-    {
-        return fHeaderInfo.fFrameIDSize;
-    }
-
-    inline int KTEgg::GetRecordSize() const
-    {
-        return fHeaderInfo.fRecordSize;
-    }
-
-    inline double KTEgg::GetRunLength() const
-    {
-        return fHeaderInfo.fRunLength;
-    }
-
-    inline double KTEgg::GetSampleRate() const
-    {
-        return fHeaderInfo.fSampleRate;
-    }
-
-    inline int KTEgg::GetTimeStampSize() const
-    {
-        return fHeaderInfo.fTimeStampSize;
-    }
-
-    inline double KTEgg::GetHertzPerSampleRateUnit() const
-    {
-        return fHeaderInfo.fHertzPerSampleRateUnit;
-    }
-
-    inline double KTEgg::GetSecondsPerRunLengthUnit() const
-    {
-        return fHeaderInfo.fSecondsPerRunLengthUnit;
-    }
-
-
-
-    inline void KTEgg::SetFileName(const std::string& fileName)
-    {
-        this->fFileName = fileName;
-    }
-
-    inline void KTEgg::SetHeaderSize(unsigned size)
-    {
-        this->fHeaderSize = size;
-    }
-
-    inline void KTEgg::SetHeader(const std::string& header)
-    {
-        this->fHeader = header;
-    }
-
-    inline void KTEgg::SetPrelude(const std::string& prelude)
-    {
-        this->fPrelude = prelude;
-    }
-
-    inline void KTEgg::SetEventSize(int size)
-    {
-        fHeaderInfo.fEventSize = size;
-    }
-
-    inline void KTEgg::SetFrameIDSize(int size)
-    {
-        fHeaderInfo.fFrameIDSize = size;
-    }
-
-    inline void KTEgg::SetRecordSize(int size)
-    {
-        fHeaderInfo.fRecordSize = size;
-    }
-
-    inline void KTEgg::SetRunLength(double length)
-    {
-        fHeaderInfo.fRunLength = length;
-    }
-
-    inline void KTEgg::SetSampleRate(double rate)
-    {
-        fHeaderInfo.fSampleRate = rate;
-    }
-
-    inline void KTEgg::SetTimeStampSize(int size)
-    {
-        fHeaderInfo.fTimeStampSize = size;
-    }
-
-    inline void KTEgg::SetHertzPerSampleRateUnit(double hpsru)
-    {
-        fHeaderInfo.fHertzPerSampleRateUnit = hpsru;
-    }
-
-    inline void KTEgg::SetSecondsPerRunLengthUnit(double sprlu)
-    {
-        fHeaderInfo.fSecondsPerRunLengthUnit = sprlu;
-    }
-
-
-    template< typename XReturnType >
-    XReturnType KTEgg::ConvertFromCharArray(char* value)
-    {
-        std::stringstream converter;
-        XReturnType converted;
-        converter << value;
-        converter >> converted;
-        return converted;
-    }
 
 } /* namespace Katydid */
 
