e644df5724682decdbfa47374e802d9a073d5838
Noah Oblath <nsoblath@mit.edu>
2012-08-23 19:12:29 -0400

    Added complex polar class and associated functions. Added TestComplexPolar. All equivalents to std::complex have been added and tested successfully except:  - std::complex*complexpolar and complexpolar*std::complex give vectors with different phases,  - transcendental function overloads have not been implemented.

---
+++ Source/Applications/Validation/TestComplexPolar.cc
@@ -0,0 +1,200 @@
+/*
+ * TestComplexPolar.cc
+ *
+ *  Created on: Aug 23, 2012
+ *      Author: nsoblath
+ */
+
+#include "complexpolar.hh"
+
+#include <complex>
+#include <iostream>
+
+using namespace Katydid;
+using namespace std;
+
+int main()
+{
+    cout << "Testing contruction and member functions" << endl;
+    complexpolar<double> cp1(1.,2.), cp2(2., 3.);
+    cout << "cp1: " << cp1 << endl;
+    cout << "cp2: " << cp2 << endl;
+
+    // member operators
+
+    // operator=
+    complexpolar<double> cp3(0.,0.);
+    cp3 = cp1;
+    cout << "cp3 = cp1: " << cp3 << endl;
+
+    // operators +=, -=, *=, /=
+    cp3 += cp1;
+    cout << "cp3 += cp1: " << cp3 << endl;
+    cp3 -= cp1;
+    cout << "cp3 -= cp1: " << cp3 << endl;
+    cp3 *= cp1;
+    cout << "cp3 *= cp1: " << cp3 << endl;
+    cp3 /= cp1;
+    cout << "cp3 /= cp1: " << cp3 << endl;
+
+    // non-member operators
+
+    cout << endl;
+    cout << "Testing non-member functions" << endl;
+
+    cout << "cp1: " << cp1 << "   cp2: " << cp2 << "   cp3: " << cp3 << endl;
+
+    // operators ==, !=
+    cout << "cp1 == cp3: " << bool(cp1 == cp3) << endl;
+    cout << "cp1 != cp3: " << bool(cp1 != cp3) << endl;
+    cout << "cp1 == cp2: " << bool(cp1 == cp2) << endl;
+    cout << "cp1 != cp2: " << bool(cp1 != cp2) << endl;
+
+    // operators +, -, /, *
+    cout << "cp1 + cp3: " << cp1 + cp3 << endl;
+    cout << "cp1 - cp3: " << cp1 - cp3 << endl;
+    cout << "cp1 * cp3: " << cp1 * cp3 << endl;
+    cout << "cp1 / cp3: " << cp1 / cp3 << endl;
+
+    cout << endl;
+    cout << "Testing non-member partially-real functions" << endl;
+
+    complexpolar<double> cp7(4.,0.);
+    double d7 = 4.;
+    cout << "cp7: " << cp7 << "   d7: " << d7 << endl;
+    cout << "cp2: " << cp2 << endl;
+
+    // operators ==, !=
+    cout << "cp7 == d7: " << bool(cp7 == d7) << endl;
+    cout << "d7 == cp2: " << bool(d7 == cp7) << endl;
+    cout << "cp7 != d7: " << bool(cp7 != d7) << endl;
+    cout << "d7 != cp2: " << bool(d7 != cp7) << endl;
+    cout << "cp2 == d7: " << bool(cp1 == d7) << endl;
+    cout << "d7 == cp2: " << bool(d7 == cp7) << endl;
+    cout << "cp2 != d7: " << bool(cp1 != d7) << endl;
+    cout << "d7 != cp2: " << bool(d7 != cp7) << endl;
+
+    // operators +, -, /, *
+    cout << "cp2 + d7: " << cp2 + d7 << endl;
+    cout << "d7 + cp2: " << d7 + cp2 << endl;
+    cout << "cp2 - d7: " << cp2 - d7 << endl;
+    cout << "d7 - cp2: " << d7 - cp2 << endl;
+    cout << "cp2 * d7: " << cp2 * d7 << endl;
+    cout << "d7 * cp2: " << d7 * cp2 << endl;
+    cout << "cp2 / d7: " << cp2 / d7 << endl;
+    cout << "d7 / cp2: " << d7 / cp2 << endl;
+
+
+    // compatibility with std::complex
+
+    cout << endl;
+    cout << "Testing compatibility with std::complex" << endl;
+
+    complex<double> c4(3.,3.), c5(1.,4.);
+    cout << "c4: " << c4 << "   c5: " << c5 << endl;
+
+    complexpolar<double> cp4(c4);
+    cout << "abs(c4), arg(c4): " << abs(c4) << ", " << arg(c4) << endl;
+    cout << "cp4 (=c4): " << cp4 << endl;
+
+    complexpolar<double> cp5(0.,0.);
+    cp5 = c5;
+    cout << "abs(c5), arg(c5): " << abs(c5) << ", " << arg(c5) << endl;
+    cout << "cp5 = c5: " << cp5 << endl;
+
+    complexpolar<double> cp8(cp4);
+    cout << "cp8 (=cp4): " << cp8 << endl;
+
+    cout << endl;
+
+    // operators +=, -=, *=, /=
+    cp4 += cp5;
+    c4 += c5;
+    cp8 += c5;
+    cout << "cp4 += cp5: " << cp4 << endl;
+    cout << "c4 += c5; abs, arg: " << abs(c4) << ", " << arg(c4) << endl;
+    cout << "cp8 += c5: " << cp8 << endl;
+    cp4 -= cp5;
+    c4 -= c5;
+    cp8 -= c5;
+    cout << "cp4 -= cp5: " << cp4 << endl;
+    cout << "c4 -= c5; abs, arg: " << abs(c4) << ", " << arg(c4) << endl;
+    cout << "cp8 -= c5: " << cp8 << endl;
+    cp4 *= cp5;
+    c4 *= c5;
+    cp8 *= c5;
+    cout << "cp4 *= cp5: " << cp4 << endl;
+    cout << "c4 *= c5; abs, arg: " << abs(c4) << ", " << arg(c4) << endl;
+    cout << "cp8 *= c5: " << cp8 << endl;
+    cp4 /= cp5;
+    c4 /= c5;
+    cp8 /= c5;
+    cout << "cp4 /= cp5: " << cp4 << endl;
+    cout << "c4 /= c5; abs, arg: " << abs(c4) << ", " << arg(c4) << endl;
+    cout << "cp8 /= c5: " << cp8 << endl;
+
+    cout << endl;
+
+    // mixed operators ==, !=
+    complex<double> c9(10.,18.);
+    complexpolar<double> cp9(c9);
+    cout << "cp9: " << cp9 << endl;
+    cout << "c9; abs, arg: " << abs(c9) << ", " << arg(c9) << endl;
+    cout << "cp9 == c9: " << bool(cp9 == c9) << endl;
+    cout << "c9 == cp9: " << bool(c9 == cp9) << endl;
+    cout << "cp9 != c9: " << bool(cp9 != c9) << endl;
+    cout << "c9 != cp9: " << bool(c9 != cp9) << endl;
+
+    cout << endl;
+
+    // mixed operators +, -, *, /
+    complex<double> c7(8., 2.);
+    cout << "cp2: " << cp2 << endl;
+    cout << "c7; abs, arg: " << abs(c7) << ", " << arg(c7) << endl;
+    cout << "cp2 + c7: " << cp2 + c7 << endl;
+    cout << "c7 + cp2: " << complexpolar<double>(c7 + cp2) << endl;
+    cout << "cp2 - c7: " << cp2 - c7 << endl;
+    cout << "c7 - cp2: " << complexpolar<double>(c7 - cp2) << endl;
+    cout << "cp2 * c7: " << cp2 * c7 << endl;
+    cout << "c7 * cp2: " << complexpolar<double>(c7 * cp2) << endl;
+    cout << "cp2 / c7: " << cp2 / c7 << endl;
+    cout << "c7 / cp2: " << complexpolar<double>(c7 / cp2) << endl;
+
+    cout << endl;
+
+    // external functions
+    cout << "Testing external functions" << endl;
+    cout << "cp9: " << cp9 << endl;
+    cout << "c9; abs, arg: " << abs(c9) << ", " << arg(c9) << endl;
+
+    cout << endl;
+
+    cout << "cp9; real, imag: " << real(cp9) << ", " << imag(cp9) << endl;
+    cout << "c9: " << c9 << endl;
+
+    cout << endl;
+
+    cout << "cp9: " << cp9 << endl;
+    cout << "rect(c9): " << rect(c9) << endl;
+    cout << "rect(c9.real(), c9.imag()): " << rect(c9.real(), c9.imag()) << endl;
+    cout << "complexpolar<double>(c9): " << complexpolar<double>(c9) << endl;
+
+    cout << endl;
+
+    cout << "c9: " << c9 << endl;
+    cout << "polar(cp9): " << polar(cp9) << endl;
+    cout << "polar(cp9.abs(), cp9.arg()): " << polar(cp9.abs(), cp9.arg()) << endl;
+
+    cout << endl;
+
+    cout << "norm(c9): " << norm(c9) << endl;
+    cout << "norm(cp9): " << norm(cp9) << endl;
+
+    cout << endl;
+
+    cout << "conj(c9); abs, arg: " << abs(conj(c9)) << ", " << arg(conj(c9)) << endl;
+    cout << "conj(cp9): " << conj(cp9) << endl;
+
+    return 0;
+}
+
---
+++ Source/Utility/complexpolar_specialization.hh
@@ -0,0 +1,414 @@
+/*
+ * complexpolar.hh
+ *
+ *  Created on: Aug 23, 2012
+ *      Author: nsoblath
+ */
+
+#ifndef COMPLEXPOLAR_SPECIALIZATION_HH_
+#define COMPLEXPOLAR_SPECIALIZATION_HH_
+
+#include "complexpolar_class.hh"
+
+namespace Katydid
+{
+    template<>
+    class complexpolar<float>
+    {
+            // typedefs
+        public:
+            typedef float value_type;
+            typedef std::complex<float> std_complex_type;
+
+            // constructors (and destructor)
+        public:
+            complexpolar(const float& abs = 0., const float& arg = 0.);
+            complexpolar(const complexpolar& cmplxp);
+            complexpolar(const std_complex_type& cmplx);
+            template< class X >
+            complexpolar(const complexpolar<X>& cmplxp) :
+                    fAbs(float(cmplxp.fAbs)), fArg(float(cmplxp.fArg))
+            {}
+            ~complexpolar();
+
+            // operators
+        public:
+            complexpolar& operator= (const complexpolar& rhs);
+
+            template<class X>
+            complexpolar<float>& operator=  (const complexpolar<X>& rhs)
+            {
+                fAbs = float(rhs.fAbs);
+                fArg = float(rhs.fArg);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<float>& operator+= (const complexpolar<X>& rhs)
+            {
+                float sumx = fAbs * std::cos(fArg) + rhs.fAbs * std::cos(rhs.fArg);
+                float sumy = fAbs * std::sin(fArg) + rhs.fAbs * std::sin(rhs.fArg);
+                fAbs = std::sqrt(sumx*sumx + sumy*sumy);
+                fArg = std::atan2(sumy, sumx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<float>& operator-= (const complexpolar<X>& rhs)
+            {
+                float diffx = fAbs * std::cos(fArg) - rhs.fAbs * std::cos(rhs.fArg);
+                float diffy = fAbs * std::sin(fArg) - rhs.fAbs * std::sin(rhs.fArg);
+                fAbs = std::sqrt(diffx*diffx + diffy*diffy);
+                fArg = std::atan2(diffy, diffx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<float>& operator*= (const complexpolar<X>& rhs)
+            {
+                fAbs = fAbs * float(rhs.fAbs);
+                fArg = fArg + float(rhs.fArg);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<float>& operator/= (const complexpolar<X>& rhs)
+            {
+                fAbs = fAbs / float(rhs.fAbs);
+                fArg = fArg - float(rhs.fArg);
+                return *this;
+            }
+
+            complexpolar& operator= (const std_complex_type& rhs);
+
+            template<class X>
+            complexpolar<float>& operator=  (const std::complex<X>& rhs)
+            {
+                fAbs = float(std::abs(rhs));
+                fArg = float(std::arg(rhs));
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<float>& operator+= (const std::complex<X>& rhs)
+            {
+                float sumx = fAbs * std::cos(fArg) + rhs.real();
+                float sumy = fAbs * std::sin(fArg) + rhs.imag();
+                fAbs = std::sqrt(sumx*sumx + sumy*sumy);
+                fArg = std::atan2(sumy, sumx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<float>& operator-= (const std::complex<X>& rhs)
+            {
+                float diffx = fAbs * std::cos(fArg) - rhs.real();
+                float diffy = fAbs * std::sin(fArg) - rhs.imag();
+                fAbs = std::sqrt(diffx*diffx + diffy*diffy);
+                fArg = std::atan2(diffy, diffx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<float>& operator*= (const std::complex<X>& rhs)
+            {
+                fAbs = fAbs * float(std::abs(rhs));
+                fArg = fArg + float(std::arg(rhs));
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<float>& operator/= (const std::complex<X>& rhs)
+            {
+                fAbs = fAbs / float(std::abs(rhs));
+                fArg = fArg - float(std::arg(rhs));
+                return *this;
+            }
+
+        public:
+            float abs() const;
+            float arg() const;
+
+        private:
+            float fAbs;
+            float fArg;
+    };
+
+    inline float complexpolar<float>::abs() const
+    {
+        return fAbs;
+    }
+    inline float complexpolar<float>::arg() const
+    {
+        return fArg;
+    }
+
+
+    template<>
+    class complexpolar<double>
+    {
+            // typedefs
+        public:
+            typedef double value_type;
+            typedef std::complex<double> std_complex_type;
+
+            // constructors (and destructor)
+        public:
+            complexpolar(const double& abs = 0., const double& arg = 0.);
+            complexpolar(const complexpolar& cmplxp);
+            complexpolar(const std_complex_type& cmplx);
+            template< class X >
+            complexpolar(const complexpolar<X>& cmplxp) :
+                    fAbs(double(cmplxp.fAbs)), fArg(double(cmplxp.fArg))
+            {}
+            ~complexpolar();
+
+            // operators
+        public:
+            complexpolar& operator= (const complexpolar& rhs);
+
+            template<class X>
+            complexpolar<double>& operator=  (const complexpolar<X>& rhs)
+            {
+                fAbs = double(rhs.fAbs);
+                fArg = double(rhs.fArg);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<double>& operator+= (const complexpolar<X>& rhs)
+            {
+                double sumx = fAbs * std::cos(fArg) + rhs.fAbs * std::cos(rhs.fArg);
+                double sumy = fAbs * std::sin(fArg) + rhs.fAbs * std::sin(rhs.fArg);
+                fAbs = std::sqrt(sumx*sumx + sumy*sumy);
+                fArg = std::atan2(sumy, sumx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<double>& operator-= (const complexpolar<X>& rhs)
+            {
+                double diffx = fAbs * std::cos(fArg) - rhs.fAbs * std::cos(rhs.fArg);
+                double diffy = fAbs * std::sin(fArg) - rhs.fAbs * std::sin(rhs.fArg);
+                fAbs = std::sqrt(diffx*diffx + diffy*diffy);
+                fArg = std::atan2(diffy, diffx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<double>& operator*= (const complexpolar<X>& rhs)
+            {
+                fAbs = fAbs * double(rhs.fAbs);
+                fArg = fArg + double(rhs.fArg);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<double>& operator/= (const complexpolar<X>& rhs)
+            {
+                fAbs = fAbs / double(rhs.fAbs);
+                fArg = fArg - double(rhs.fArg);
+                return *this;
+            }
+
+            complexpolar& operator= (const std_complex_type& rhs);
+
+            template<class X>
+            complexpolar<double>& operator=  (const std::complex<X>& rhs)
+            {
+                fAbs = double(std::abs(rhs));
+                fArg = double(std::arg(rhs));
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<double>& operator+= (const std::complex<X>& rhs)
+            {
+                double sumx = fAbs * std::cos(fArg) + rhs.real();
+                double sumy = fAbs * std::sin(fArg) + rhs.imag();
+                fAbs = std::sqrt(sumx*sumx + sumy*sumy);
+                fArg = std::atan2(sumy, sumx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<double>& operator-= (const std::complex<X>& rhs)
+            {
+                double diffx = fAbs * std::cos(fArg) - rhs.real();
+                double diffy = fAbs * std::sin(fArg) - rhs.imag();
+                fAbs = std::sqrt(diffx*diffx + diffy*diffy);
+                fArg = std::atan2(diffy, diffx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<double>& operator*= (const std::complex<X>& rhs)
+            {
+                fAbs = fAbs * double(std::abs(rhs));
+                fArg = fArg + double(std::arg(rhs));
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<double>& operator/= (const std::complex<X>& rhs)
+            {
+                fAbs = fAbs / double(std::abs(rhs));
+                fArg = fArg - double(std::arg(rhs));
+                return *this;
+            }
+
+        public:
+            double abs() const;
+            double arg() const;
+
+        private:
+            double fAbs;
+            double fArg;
+    };
+
+    inline double complexpolar<double>::abs() const
+    {
+        return fAbs;
+    }
+    inline double complexpolar<double>::arg() const
+    {
+        return fArg;
+    }
+
+
+
+    template<>
+    class complexpolar<long double>
+    {
+            // typedefs
+        public:
+            typedef long double value_type;
+            typedef std::complex<long double> std_complex_type;
+
+            // constructors (and destructor)
+        public:
+            complexpolar(const long double& abs = 0., const long double& arg = 0.);
+            complexpolar(const complexpolar& cmplxp);
+            complexpolar(const std_complex_type& cmplx);
+            template< class X >
+            complexpolar(const complexpolar<X>& cmplxp) :
+                    fAbs((long double)(cmplxp.fAbs)), fArg((long double)(cmplxp.fArg))
+            {}
+            ~complexpolar();
+
+            // operators
+        public:
+            complexpolar& operator= (const complexpolar& rhs);
+
+            template<class X>
+            complexpolar<long double>& operator=  (const complexpolar<X>& rhs)
+            {
+                fAbs = (long double)(rhs.fAbs);
+                fArg = (long double)(rhs.fArg);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<long double>& operator+= (const complexpolar<X>& rhs)
+            {
+                long double sumx = fAbs * std::cos(fArg) + rhs.fAbs * std::cos(rhs.fArg);
+                long double sumy = fAbs * std::sin(fArg) + rhs.fAbs * std::sin(rhs.fArg);
+                fAbs = std::sqrt(sumx*sumx + sumy*sumy);
+                fArg = std::atan2(sumy, sumx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<long double>& operator-= (const complexpolar<X>& rhs)
+            {
+                long double diffx = fAbs * std::cos(fArg) - rhs.fAbs * std::cos(rhs.fArg);
+                long double diffy = fAbs * std::sin(fArg) - rhs.fAbs * std::sin(rhs.fArg);
+                fAbs = std::sqrt(diffx*diffx + diffy*diffy);
+                fArg = std::atan2(diffy, diffx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<long double>& operator*= (const complexpolar<X>& rhs)
+            {
+                fAbs = fAbs * (long double)(rhs.fAbs);
+                fArg = fArg + (long double)(rhs.fArg);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<long double>& operator/= (const complexpolar<X>& rhs)
+            {
+                fAbs = fAbs / (long double)(rhs.fAbs);
+                fArg = fArg - (long double)(rhs.fArg);
+                return *this;
+            }
+
+            complexpolar& operator= (const std_complex_type& rhs);
+
+            template<class X>
+            complexpolar<long double>& operator=  (const std::complex<X>& rhs)
+            {
+                fAbs = (long double)(std::abs(rhs));
+                fArg = (long double)(std::arg(rhs));
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<long double>& operator+= (const std::complex<X>& rhs)
+            {
+                long double sumx = fAbs * std::cos(fArg) + rhs.real();
+                long double sumy = fAbs * std::sin(fArg) + rhs.imag();
+                fAbs = std::sqrt(sumx*sumx + sumy*sumy);
+                fArg = std::atan2(sumy, sumx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<long double>& operator-= (const std::complex<X>& rhs)
+            {
+                long double diffx = fAbs * std::cos(fArg) - rhs.real();
+                long double diffy = fAbs * std::sin(fArg) - rhs.imag();
+                fAbs = std::sqrt(diffx*diffx + diffy*diffy);
+                fArg = std::atan2(diffy, diffx);
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<long double>& operator*= (const std::complex<X>& rhs)
+            {
+                fAbs = fAbs * (long double)(std::abs(rhs));
+                fArg = fArg + (long double)(std::arg(rhs));
+                return *this;
+            }
+
+            template<class X>
+            complexpolar<long double>& operator/= (const std::complex<X>& rhs)
+            {
+                fAbs = fAbs / (long double)(std::abs(rhs));
+                fArg = fArg - (long double)(std::arg(rhs));
+                return *this;
+            }
+
+        public:
+            long double abs() const;
+            long double arg() const;
+
+        private:
+            long double fAbs;
+            long double fArg;
+    };
+
+    inline long double complexpolar<long double>::abs() const
+    {
+        return fAbs;
+    }
+    inline long double complexpolar<long double>::arg() const
+    {
+        return fArg;
+    }
+
+
+
+} /* namespace Katydid */
+#endif /* COMPLEXPOLAR_SPECIALIZATION_HH_ */
