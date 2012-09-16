9ccf71c409ed3a5acfdc3d5ee35d41d7f02e3b82
Noah Oblath <nsoblath@mit.edu>
2012-09-16 16:55:25 -0400

    Added the example cc file to the distribution. Added a few instructions as comments in the CMakeLists file.

---
+++ Scripts/MyApplication.cc
@@ -0,0 +1,15 @@
+/*
+ * MyApplication.cc
+ *
+ *  Created on: Sep 14, 2012
+ *      Author: nsoblath
+ */
+
+#include "KTTimeSeries.hh"
+
+int main()
+{
+    Katydid::KTTimeSeries ts(1, 0., 1.);
+    std::cout << "test" << std::endl;
+    return 0;
+}
