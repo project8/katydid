93c13e4d03963e2a370a9a9fc75a9a4b33d91330
Noah Oblath <nsoblath@mit.edu>
2012-10-05 11:50:55 -0400

    Added handling of rpath.

--- cmake/KatydidBuild.cmake
+++ cmake/KatydidBuild.cmake
@@ -34,6 +34,26 @@ if (ENABLE_TESTING)
     enable_testing ()
 endif (ENABLE_TESTING)
 
+# deal with the rpath settings (from http://www.cmake.org/Wiki/CMake_RPATH_handling)
+# use (i.e. don't skip) the full RPATH for the build tree
+SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
+
+# when building, don't use the install RPATH already
+# (but later on when installing)
+SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE) 
+
+SET(CMAKE_INSTALL_RPATH "${LIB_INSTALL_DIR}")
+
+# add the automatically determined parts of the RPATH
+# which point to directories outside the build tree to the install RPATH
+SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+# the RPATH to be used when installing, but only if it's not a system directory
+LIST(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${LIB_INSTALL_DIR}" isSystemDir)
+IF("${isSystemDir}" STREQUAL "-1")
+   SET(CMAKE_INSTALL_RPATH "${LIB_INSTALL_DIR}")
+ENDIF("${isSystemDir}" STREQUAL "-1")
+
 
 ##########
 # MACROS #
