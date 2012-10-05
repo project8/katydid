29ef0fbd96894660499b2bd6066549125e05fbf5
Noah Oblath <nsoblath@mit.edu>
2012-10-05 18:20:44 -0400

    Fixes to linking executables.

--- cmake/KatydidBuild.cmake
+++ cmake/KatydidBuild.cmake
@@ -36,13 +36,13 @@ endif (ENABLE_TESTING)
 
 # deal with the rpath settings (from http://www.cmake.org/Wiki/CMake_RPATH_handling)
 # use (i.e. don't skip) the full RPATH for the build tree
-SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
+#SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
 
 # when building, don't use the install RPATH already
 # (but later on when installing)
-SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE) 
+#SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE) 
 
-SET(CMAKE_INSTALL_RPATH "${LIB_INSTALL_DIR}")
+#SET(CMAKE_INSTALL_RPATH "${LIB_INSTALL_DIR}")
 
 # add the automatically determined parts of the RPATH
 # which point to directories outside the build tree to the install RPATH
