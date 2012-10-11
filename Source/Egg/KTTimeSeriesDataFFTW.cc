592f0059914d2db14d1c34ed1d503ce4bb8b182b
Noah Oblath <nsoblath@mit.edu>
2012-10-11 17:14:28 -0400

    Added KTTimeSeriesDataFFTW.

---
+++ Source/Egg/KTTimeSeriesDataFFTW.cc
@@ -0,0 +1,68 @@
+/*
+ * KTTimeSeriesDataFFTW.cc
+ *
+ *  Created on: Sep 9, 2011
+ *      Author: nsoblath
+ */
+
+#include "KTTimeSeriesDataFFTW.hh"
+
+#ifdef ROOT_FOUND
+#include "TH1.h"
+#endif
+
+using std::vector;
+
+namespace Katydid
+{
+    std::string KTTimeSeriesDataFFTW::fName("time-series-fftw");
+
+    const std::string& KTTimeSeriesDataFFTW::StaticGetName()
+    {
+        return fName;
+    }
+
+    KTTimeSeriesDataFFTW::KTTimeSeriesDataFFTW(UInt_t nChannels) :
+            KTData(),
+            fSampleRate(0.),
+            fRecordLength(0.),
+            fBinWidth(1.),
+            fRecordSize(0),
+            fChannelData(nChannels)
+    {
+    }
+
+    KTTimeSeriesDataFFTW::~KTTimeSeriesDataFFTW()
+    {
+        while (! fChannelData.empty())
+        {
+            delete fChannelData.back().fRecord;
+            fChannelData.pop_back();
+        }
+    }
+
+#ifdef ROOT_FOUND
+    TH1C* KTTimeSeriesDataFFTW::CreateTimeSeriesHistogram(UInt_t channelNum) const
+    {
+        TH1C* hist = new TH1C("hRecord", "Event Record", (int)GetRecordSize(), -0.5*fBinWidth, GetRecordLength() + fBinWidth*0.5);
+        for (UInt_t iBin=0; iBin<fChannelData[channelNum].fRecord->size(); iBin++)
+        {
+            hist->SetBinContent(iBin+1, (*(fChannelData[channelNum].fRecord))(iBin)[0]);
+        }
+        hist->SetXTitle("Time (s)");
+        return hist;
+    }
+
+    TH1I* KTTimeSeriesDataFFTW::CreateAmplitudeDistributionHistogram(UInt_t channelNum) const
+    {
+        TH1I* hist = new TH1I("hRecordAmpl", "Event Record Amplitude Distribution", 256, -0.5, 255.5);
+        for (int iBin=0; iBin<fChannelData[channelNum].fRecord->size(); iBin++)
+        {
+            hist->Fill(Double_t((*(fChannelData[channelNum].fRecord))(iBin)[0]));
+        }
+        hist->SetXTitle("ADC Bin");
+        return hist;
+    }
+#endif
+
+} /* namespace Katydid */
