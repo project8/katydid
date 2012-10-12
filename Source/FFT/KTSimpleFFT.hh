951ca1abf6d9714466340a75bf1eefd5499f75ce
Noah Oblath <nsoblath@mit.edu>
2012-10-12 16:08:51 -0400

    Minor documentation addition.

--- Source/FFT/KTSimpleFFT.hh
+++ Source/FFT/KTSimpleFFT.hh
@@ -59,6 +59,7 @@ namespace Katydid
      Slots:
      \li \c void ProcessHeader(const KTEggHeader* header)
      \li \c void ProcessEvent(UInt_t iEvent, const KTEvent* event)
+     \li \c void ProcessTimeSeriesData(const KTTimeSeriesData* data)
 
      Signals:
      \li \c void (UInt_t, const KTSimpleFFT*) emitted upon performance of a transform.
