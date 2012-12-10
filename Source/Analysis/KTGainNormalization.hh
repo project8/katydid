7e312975d4ace8c5ac4027fcfbb0e44278322829
Noah Oblath <nsoblath@mit.edu>
2012-12-10 15:30:49 -0500

    Moved the previous KTGainNormalization to KTOldGainNormalization, and started updating KTGainNormalization.

--- Source/Analysis/KTGainNormalization.hh
+++ Source/Analysis/KTGainNormalization.hh
@@ -14,8 +14,10 @@
 namespace Katydid
 {
     class KTFrequencySpectrum;
+    class KTFrequencySpectrumFFTW;
     class KTPStoreNode;
-    class KTSlidingWindowFSData;
+    //class KTSlidingWindowFSData;
+    class KTSlidingWindowFSDataFFTW;
     class KTPowerSpectrum;
 
     template< size_t NDims, typename XDataType >
@@ -24,18 +26,40 @@ namespace Katydid
     class KTGainNormalization : public KTProcessor
     {
         public:
+            //typedef KTSignal< void (const KTSlidingWindowFSData*) >::signal SWFSSignal;
+            typedef KTSignal< void (const KTSlidingWindowFSDataFFTW*) >::signal SWFSFFTWSignal;
+
+        public:
             KTGainNormalization();
             virtual ~KTGainNormalization();
 
             Bool_t Configure(const KTPStoreNode* node);
 
-            void PrepareNormalization(KTFrequencySpectrum* fullArray, UInt_t reducedNBins, Double_t reducedBinWidth);
+            //void PrepareNormalization(KTFrequencySpectrum* fullArray, UInt_t reducedNBins, Double_t reducedBinWidth);
+            void PrepareNormalizationFFTW(KTFrequencySpectrumFFTW* fullArray, UInt_t reducedNBins, Double_t reducedBinWidth);
 
-            void ProcessSlidingWindowFFT(KTSlidingWindowFSData* swFSData);
-            void ProcessFrequencySpectrum(UInt_t psNum, KTFrequencySpectrum* powerSpectrum);
+            //void NormalizeData(KTSlidingWindowFSData* swFSData);
+            //void NormalizeFrequencySpectrum(KTFrequencySpectrum* frequencySpectrum);
+            void NormalizeFFTWData(KTSlidingWindowFSDataFFTW* swFSData);
+            void NormalizeFFTWFrequencySpectrum(KTFrequencySpectrumFFTW* frequencySpectrum);
 
         private:
             KTFrequencySpectrum* fNormalization;
+
+            //***************
+            // Signals
+            //***************
+
+        private:
+            //SWFSSignal fFSSignal
+            SWFSFFTWSignal fFFTWFSSignal;
+
+            //***************
+            // Slots
+            //***************
+
+        public:
+            void ProcessEvent(boost::shared_ptr<KTEvent> event);
 
     };
 
