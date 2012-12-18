cc3d03f5b2fe816c5e268f4bceaf2cd3d20d0e6d
Noah Oblath <nsoblath@mit.edu>
2012-12-18 18:01:53 -0500

    Added Candidates directory, KTFrequencyCandidate, and KTFrequencyCandidateData.

--- Source/Analysis/KTCluster1DData.hh
+++ Source/Analysis/KTCluster1DData.hh
@@ -43,12 +43,12 @@ namespace Katydid
             KTCluster1DData(UInt_t nGroups=1);
             virtual ~KTCluster1DData();
 
-            const SetOfClusters& GetSetOfClusters(UInt_t channelNum = 0) const;
-            Double_t GetThreshold(UInt_t channelNum = 0) const;
+            const SetOfClusters& GetSetOfClusters(UInt_t groupNum = 0) const;
+            Double_t GetThreshold(UInt_t groupNum = 0) const;
             UInt_t GetNGroups() const;
 
-            void AddCluster(UInt_t firstPoint, UInt_t lastPoint, UInt_t channelNum = 0);
-            void SetThreshold(Double_t threshold, UInt_t channelNum = 0);
+            void AddCluster(UInt_t firstPoint, UInt_t lastPoint, UInt_t groupNum = 0);
+            void SetThreshold(Double_t threshold, UInt_t groupNum = 0);
             void SetNGroups(UInt_t channels);
 
             UInt_t GetNBins() const;
@@ -68,14 +68,14 @@ namespace Katydid
 
     };
 
-    inline const KTCluster1DData::SetOfClusters& KTCluster1DData::GetSetOfClusters(UInt_t channelNum) const
+    inline const KTCluster1DData::SetOfClusters& KTCluster1DData::GetSetOfClusters(UInt_t groupNum) const
     {
-        return fGroupData[channelNum].fClusters;
+        return fGroupData[groupNum].fClusters;
     }
 
-    inline Double_t KTCluster1DData::GetThreshold(UInt_t channelNum) const
+    inline Double_t KTCluster1DData::GetThreshold(UInt_t groupNum) const
     {
-        return fGroupData[channelNum].fThreshold;
+        return fGroupData[groupNum].fThreshold;
     }
 
     inline UInt_t KTCluster1DData::GetNGroups() const
@@ -83,16 +83,16 @@ namespace Katydid
         return UInt_t(fGroupData.size());
     }
 
-    inline void KTCluster1DData::AddCluster(UInt_t firstPoint, UInt_t lastPoint, UInt_t channelNum)
+    inline void KTCluster1DData::AddCluster(UInt_t firstPoint, UInt_t lastPoint, UInt_t groupNum)
     {
-        if (channelNum >= fGroupData.size()) fGroupData.resize(channelNum+1);
-        fGroupData[channelNum].fClusters.insert(std::make_pair(firstPoint, lastPoint));
+        if (groupNum >= fGroupData.size()) fGroupData.resize(groupNum+1);
+        fGroupData[groupNum].fClusters.insert(std::make_pair(firstPoint, lastPoint));
     }
 
-    inline void KTCluster1DData::SetThreshold(Double_t threshold, UInt_t channelNum)
+    inline void KTCluster1DData::SetThreshold(Double_t threshold, UInt_t groupNum)
     {
-        if (channelNum >= fGroupData.size()) fGroupData.resize(channelNum+1);
-        fGroupData[channelNum].fThreshold = threshold;
+        if (groupNum >= fGroupData.size()) fGroupData.resize(groupNum+1);
+        fGroupData[groupNum].fThreshold = threshold;
     }
 
     inline void KTCluster1DData::SetNGroups(UInt_t channels)
---
+++ Source/Candidates/KTFrequencyCandidate.cc
@@ -0,0 +1,26 @@
+/*
+ * KTFrequencyCandidate.cc
+ *
+ *  Created on: Dec 18, 2012
+ *      Author: nsoblath
+ */
+
+#include "KTFrequencyCandidate.hh"
+
+namespace Katydid
+{
+
+    KTFrequencyCandidate::KTFrequencyCandidate() :
+            fFirstBin(0),
+            fLastBin(0),
+            fMeanFrequency(0.)
+    {
+    }
+
+    KTFrequencyCandidate::~KTFrequencyCandidate()
+    {
+    }
+
+
+
+} /* namespace Katydid */
