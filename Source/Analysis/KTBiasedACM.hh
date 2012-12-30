de8e1ee2700adba06884bef3869a0a6e765b5a68
Jared Kofron <jared.kofron@gmail.com>
2012-12-30 13:44:11 -0800

    for now KTBiasedACM is just a typedef for a MatrixXf.

---
+++ Source/Analysis/KTBiasedACM.hh
@@ -0,0 +1,19 @@
+#ifndef __KT_BACM_HH
+#define __KT_BACM_HH
+/*
+ *  KTBiasedACM.hh
+ *    author: kofron
+ *    created: 12/30/2012
+ *  KTBiasedACM is an abstract representation of a biased autocorrelation 
+ *  matrix (ACM) R.  R_ij = R_xx(i-j) = sum_l x(l)x(l + (i-j)).
+ */
+
+#include <Eigen/Dense>
+
+namespace katydid {
+
+  typedef Eigen::MatrixXf KTBiasedACM;
+
+}; // namespace katydid
+
+#endif // __KT_BACM_HH
