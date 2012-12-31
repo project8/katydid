630fc6e4b446d38f8e4d34486519cb5dbb87ce96
Jared Kofron <jared.kofron@gmail.com>
2012-12-31 13:21:21 -0800

    added toeplitz matrix initialization for the ACM from noise samples.  LACF is not yet calculated.

--- Source/Analysis/KTRQProcessor.cc
+++ Source/Analysis/KTRQProcessor.cc
@@ -7,6 +7,7 @@ namespace Katydid {
     KTProcessor(),
     fRQSignal(),
     fChunkSize(512),
+    fNACMDidConverge(false),
     fNoiseACM(NULL)
   {
     fConfigName = "rayleigh-quotient";
@@ -76,12 +77,22 @@ namespace Katydid {
   {
     return fNoiseName;
   }
+
+  void KTRQProcessor::SetNACMConverged(Bool_t newval)
+  {
+    this->fNACMDidConverge = newval;
+  }
+  
+  Bool_t KTRQProcessor::GetNACMConverged()
+  {
+    return this->fNACMDidConverge;
+  }
   
   void KTRQProcessor::ProcessNoiseEvent(boost::shared_ptr<KTEvent> event)
   {
     // Grab time series data from event
     const KTTimeSeriesData* noise = event->GetData<KTTimeSeriesData>(fNoiseName);
-    if( noise != NULL ) {
+    if( noise != NULL && !(this->fNACMDidConverge) ) {
       // Grab the first channel of data out to use. 
       const KTTimeSeriesReal* noiseDt = dynamic_cast<const KTTimeSeriesReal*>(noise->GetTimeSeries(0));
 
@@ -95,9 +106,38 @@ namespace Katydid {
       // For now we only use the first chunk to form the ACM.  Despite the fact that this
       // looks like allocating memory, it isn't.
       new (&(this->fDataMap)) DataMapType(rawPtr, this->fChunkSize);
-      
-      
-      KTINFO(nrq_log,"using noise to update NACM estimate...")
+        
+      // Update noise estimate with current noise sample.
+      KTINFO(nrq_log,"using noise to update NACM estimate...");
+
+      // Calculate first row.  The NACM is a Toeplitz matrix which means we only need to
+      // calculate the first row.
+      for(unsigned rowIdx = 0; rowIdx < this->fChunkSize; rowIdx++) {
+	(*this->fNoiseACM)(rowIdx, 0) = 1.0;
+      }
+
+      for(unsigned row = 1; row < this->fChunkSize; row++) {
+	for(unsigned col = 0; col < this->fChunkSize; col++) {
+	  // If row = col, we are on the diagonal, which means this element is equal to
+	  // row = 0, col = 0.
+	  if( row == col ) (*this->fNoiseACM)(row,col) = (*this->fNoiseACM)(0,0);
+	  // If row < col, we are below the diagonal, so use the transposed element for
+	  // assignment.
+	  if( col < row ) (*this->fNoiseACM)(row,col) = (*this->fNoiseACM)(col,row);
+	  // If row > col, we are above the diagonal.  Look above and to the left to get this
+	  // element.
+	  if( col > row ) (*this->fNoiseACM)(row,col) = (*this->fNoiseACM)(row-1, col-1);
+	}
+      }
+
+      // Check convergence criterion (TEMPORARILY JUST TRUE) and set convergence flag if
+      // appropriate.
+      if(true) {
+	this->SetNACMConverged(true);
+	KTINFO(nrq_log,"NACM has converged.");
+	KTWARN(nrq_log,"DON'T BELIEVE CONVERGE MSG, TEMPORARY CONVERGENCE CRITERION USED.");
+      }
+
     }
     else {
       KTWARN(nrq_log,"time series " << fNoiseName << " not found in event!  skipping...");
