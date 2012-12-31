e8e575d0f9b930026860c20808e93c4cb3bec689
Jared Kofron <jared.kofron@gmail.com>
2012-12-30 18:34:51 -0800

    lots of changes.  first "working" version which just warns when it gets new events

--- Source/Analysis/KTRQProcessor.hh
+++ Source/Analysis/KTRQProcessor.hh
@@ -14,11 +14,60 @@
 
 #include "KTBiasedACM.hh"
 #include "KTProcessor.hh"
+#include "KTLogger.hh"
+
+#include "KTEvent.hh"
+#include "KTFactory.hh"
+#include "KTPStoreNode.hh"
+
+#include "boost/shared_ptr.hpp"
 
 namespace Katydid {
 
+  KTLOGGER(nrq_log, "katydid.nrq");
+
+  class KTTimeSeriesData;
+
   class KTRQProcessor : public KTProcessor {
 
+    /* 
+     * Constructors and destructors.
+     */
+  public:
+    KTRQProcessor();
+    virtual ~KTRQProcessor();
+
+    /* 
+     * Configuration, setters and getters.
+     */
+  public:
+    Bool_t Configure(const KTPStoreNode* node);
+    unsigned GetChunkSize();
+    void SetChunkSize(unsigned newsize);
+    std::string GetNoiseDataName();
+    void SetNoiseDataName(std::string noisename);
+
+    /*
+     * Slots and signals.
+     */
+  public:
+    typedef KTSignal< void (const double*) >::signal RQSignal;
+
+    void ProcessNoiseData(const KTTimeSeriesData* noise);
+    void ProcessCandidateData(const KTTimeSeriesData* signal);
+    void ProcessNoiseEvent(boost::shared_ptr<KTEvent> event);
+
+  private:
+    RQSignal fRQSignal;
+
+    /*
+     * Internal state related to processing
+     */ 
+  private:
+    unsigned fChunkSize;
+    KTBiasedACM* fNoiseACM;
+    std::string fNoiseName;
+    
   }; // class KTRQProcessor
 
 }; // namespace katydid
