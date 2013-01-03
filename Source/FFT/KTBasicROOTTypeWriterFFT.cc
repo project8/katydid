fafcebe1cdf11d14ea83d6e55fa51f6e711b6ffe
Noah Oblath <nsoblath@mit.edu>
2013-01-03 15:08:51 -0500

    Implemented FFT type writers.

--- Source/FFT/KTBasicROOTTypeWriterFFT.cc
+++ Source/FFT/KTBasicROOTTypeWriterFFT.cc
@@ -5,18 +5,16 @@
  *      Author: nsoblath
  */
 
-#include "KTBasicROOTFileWriter.hh"
+#include "KTBasicROOTTypeWriterFFT.hh"
 
 #include "KTEvent.hh"
-#include "KTFactory.hh"
+#include "KTTIFactory.hh"
 #include "KTLogger.hh"
-#include "KTPStoreNode.hh"
-#include "KTTimeSeries.hh"
-#include "KTTimeSeriesData.hh"
-
+#include "KTFrequencySpectrum.hh"
+#include "KTFrequencySpectrumData.hh"
+#include "KTFrequencySpectrumDataFFTW.hh"
 #include "KTSlidingWindowFSData.hh"
 #include "KTSlidingWindowFSDataFFTW.hh"
-#include "KTGainVariationData.hh"
 
 #include "TH1.h"
 #include "TH2.h"
@@ -30,76 +28,26 @@ namespace Katydid
 {
     KTLOGGER(publog, "katydid.output");
 
-    static KTDerivedRegistrar< KTWriter, KTBasicROOTFileWriter > sBRFWriterRegistrar("basic-root-writer");
-    static KTDerivedRegistrar< KTProcessor, KTBasicROOTFileWriter > sBRFWProcRegistrar("basic-root-writer");
 
-    KTBasicROOTFileWriter::KTBasicROOTFileWriter() :
-            KTWriter(),
-            fFilename("basic_output.root"),
-            fFileFlag("recreate"),
-            fFile(NULL)
+    static KTDerivedTIRegistrar< KTBasicROOTTypeWriter, KTBasicROOTTypeWriterFFT > sBRTWFFTRegistrar;
+
+    KTBasicROOTTypeWriterFFT::KTBasicROOTTypeWriterFFT() :
+            KTBasicROOTTypeWriter()
+            //KTTypeWriterEgg()
     {
-        fConfigName = "basic-root-writer";
-
-        RegisterSlot("write-data", this, &KTBasicROOTFileWriter::Publish);
-
-        RegisterSlot("write-frequency-spectrum", this, &KTBasicROOTFileWriter::WriteFrequencySpectrumData);
-        RegisterSlot("write-frequency-spectrum-fftw", this, &KTBasicROOTFileWriter::WriteFrequencySpectrumDataFFTW);
-        RegisterSlot("write-correlation", this, &KTBasicROOTFileWriter::WriteCorrelationData);
-        RegisterSlot("write-sliding-window-fs", this, &KTBasicROOTFileWriter::WriteSlidingWindowFSData);
-        RegisterSlot("write-sliding-window-fs-fftw", this, &KTBasicROOTFileWriter::WriteSlidingWindowFSDataFFTW);
-        RegisterSlot("write-gain-variation", this, &KTBasicROOTFileWriter::WriteGainVariationData);
     }
 
-    KTBasicROOTFileWriter::~KTBasicROOTFileWriter()
+    KTBasicROOTTypeWriterFFT::~KTBasicROOTTypeWriterFFT()
     {
-        if (fFile != NULL)
-        {
-            fFile->Close();
-        }
-        delete fFile;
     }
 
-    Bool_t KTBasicROOTFileWriter::Configure(const KTPStoreNode* node)
+
+    void KTBasicROOTTypeWriterFFT::RegisterSlots()
     {
-        // Config-file settings
-        if (node != NULL)
-        {
-            SetFilename(node->GetData<string>("output-file", fFilename));
-            SetFileFlag(node->GetData<string>("file-flag", fFileFlag));
-        }
-
-        // Command-line settings
-        //SetFilename(fCLHandler->GetCommandLineValue< string >("broot-output-file", fTransformFlag));
-
-        return true;
-    }
-
-    Bool_t KTBasicROOTFileWriter::OpenAndVerifyFile()
-    {
-        if (fFile == NULL)
-        {
-            fFile = new TFile(fFilename.c_str(), fFileFlag.c_str());
-        }
-        if (! fFile->IsOpen())
-        {
-            delete fFile;
-            fFile = NULL;
-            KTERROR(publog, "Output file <" << fFilename << "> did not open!");
-            return false;
-        }
-        return true;
-    }
-
-    void KTBasicROOTFileWriter::Publish(const KTWriteableData* data)
-    {
-        data->Accept(this);
-        return;
-    }
-
-    void KTBasicROOTFileWriter::Write(const KTWriteableData* data)
-    {
-        KTWARN(publog, "Generic Write function called; no data written");
+        fWriter->RegisterSlot("fs-data", this, &KTBasicROOTTypeWriterFFT::WriteFrequencySpectrumData, "void (const KTFrequencySpectrumData*)");
+        fWriter->RegisterSlot("fs-data-fftw", this, &KTBasicROOTTypeWriterFFT::WriteFrequencySpectrumDataFFTW, "void (const KTFrequencySpectrumDataFFTW*)");
+        fWriter->RegisterSlot("swfs-data", this, &KTBasicROOTTypeWriterFFT::WriteSlidingWindowFSData, "void (const WriteSlidingWindowFSData*)");
+        fWriter->RegisterSlot("swfs-data-fftw", this, &KTBasicROOTTypeWriterFFT::WriteSlidingWindowFSDataFFTW, "void (const WriteSlidingWindowFSDataFFTW*)");
         return;
     }
 
@@ -108,50 +56,14 @@ namespace Katydid
     // Frequency Spectrum Data
     //************************
 
-    void KTBasicROOTFileWriter::Write(const KTTimeSeriesData* data)
-    {
-        KTEvent* event = data->GetEvent();
-        UInt_t eventNumber = 0;
-        if (event != NULL) eventNumber = event->GetEventNumber();
-        UInt_t nChannels = data->GetNTimeSeries();
-
-        if (! OpenAndVerifyFile()) return;
-
-        for (unsigned iChannel=0; iChannel<nChannels; iChannel++)
-        {
-            const KTTimeSeries* spectrum = data->GetTimeSeries(iChannel);
-            if (spectrum != NULL)
-            {
-                stringstream conv;
-                conv << "histTS_" << eventNumber << "_" << iChannel;
-                string histName;
-                conv >> histName;
-                TH1D* powerSpectrum = spectrum->CreateHistogram(histName);
-                powerSpectrum->SetDirectory(fFile);
-                powerSpectrum->Write();
-                KTDEBUG(publog, "Histogram <" << histName << "> written to ROOT file");
-            }
-        }
-        return;
-    }
-
-    //************************
-    // Frequency Spectrum Data
-    //************************
-
-    void KTBasicROOTFileWriter::Write(const KTFrequencySpectrumData* data)
-    {
-        return WriteFrequencySpectrumData(data);
-    }
-
-    void KTBasicROOTFileWriter::WriteFrequencySpectrumData(const KTFrequencySpectrumData* data)
+    void KTBasicROOTTypeWriterFFT::WriteFrequencySpectrumData(const KTFrequencySpectrumData* data)
     {
         KTEvent* event = data->GetEvent();
         UInt_t eventNumber = 0;
         if (event != NULL) eventNumber = event->GetEventNumber();
         UInt_t nChannels = data->GetNChannels();
 
-        if (! OpenAndVerifyFile()) return;
+        if (! fWriter->OpenAndVerifyFile()) return;
 
         for (unsigned iChannel=0; iChannel<nChannels; iChannel++)
         {
@@ -163,7 +75,7 @@ namespace Katydid
                 string histName;
                 conv >> histName;
                 TH1D* powerSpectrum = spectrum->CreatePowerHistogram(histName);
-                powerSpectrum->SetDirectory(fFile);
+                powerSpectrum->SetDirectory(fWriter->GetFile());
                 powerSpectrum->Write();
                 KTDEBUG(publog, "Histogram <" << histName << "> written to ROOT file");
             }
@@ -171,19 +83,14 @@ namespace Katydid
         return;
     }
 
-    void KTBasicROOTFileWriter::Write(const KTFrequencySpectrumDataFFTW* data)
-    {
-        return WriteFrequencySpectrumDataFFTW(data);
-    }
-
-    void KTBasicROOTFileWriter::WriteFrequencySpectrumDataFFTW(const KTFrequencySpectrumDataFFTW* data)
+    void KTBasicROOTTypeWriterFFT::WriteFrequencySpectrumDataFFTW(const KTFrequencySpectrumDataFFTW* data)
     {
         KTEvent* event = data->GetEvent();
         UInt_t eventNumber = 0;
         if (event != NULL) eventNumber = event->GetEventNumber();
         UInt_t nChannels = data->GetNChannels();
 
-        if (! OpenAndVerifyFile()) return;
+        if (! fWriter->OpenAndVerifyFile()) return;
 
         for (unsigned iChannel=0; iChannel<nChannels; iChannel++)
         {
@@ -195,48 +102,8 @@ namespace Katydid
                 string histName;
                 conv >> histName;
                 TH1D* powerSpectrum = spectrum->CreatePowerHistogram(histName);
-                powerSpectrum->SetDirectory(fFile);
+                powerSpectrum->SetDirectory(fWriter->GetFile());
                 powerSpectrum->Write();
-                KTDEBUG(publog, "Histogram <" << histName << "> written to ROOT file");
-            }
-        }
-        return;
-    }
-
-    //************************
-    // Correlation Data
-    //************************
-
-    void KTBasicROOTFileWriter::Write(const KTCorrelationData* data)
-    {
-        return WriteCorrelationData(data);
-    }
-
-    void KTBasicROOTFileWriter::WriteCorrelationData(const KTCorrelationData* data)
-    {
-        KTEvent* event = data->GetEvent();
-        UInt_t eventNumber = 0;
-        if (event != NULL) eventNumber = event->GetEventNumber();
-        UInt_t nPairs = data->GetNPairs();
-
-        if (! OpenAndVerifyFile()) return;
-
-        for (unsigned iPair=0; iPair<nPairs; iPair++)
-        {
-            const KTFrequencySpectrum* spectrum = data->GetCorrelation(iPair);
-            if (spectrum != NULL)
-            {
-                stringstream conv;
-                conv << "histCorr_" << eventNumber << "_" << iPair;
-                string histName;
-                conv >> histName;
-                TH1D* corrHist = spectrum->CreateMagnitudeHistogram(histName);
-                stringstream titleStream;
-                titleStream << "Event " << eventNumber << ", Correlation " << iPair << ", "
-                        "Channels (" << data->GetFirstChannel(iPair) << ", " << data->GetSecondChannel(iPair) << ")";
-                corrHist->SetTitle(titleStream.str().c_str());
-                corrHist->SetDirectory(fFile);
-                corrHist->Write();
                 KTDEBUG(publog, "Histogram <" << histName << "> written to ROOT file");
             }
         }
@@ -247,19 +114,14 @@ namespace Katydid
     // Sliding Window Data
     //************************
 
-    void KTBasicROOTFileWriter::Write(const KTSlidingWindowFSData* data)
-    {
-        return WriteSlidingWindowFSData(data);
-    }
-
-    void KTBasicROOTFileWriter::WriteSlidingWindowFSData(const KTSlidingWindowFSData* data)
+    void KTBasicROOTTypeWriterFFT::WriteSlidingWindowFSData(const KTSlidingWindowFSData* data)
     {
         KTEvent* event = data->GetEvent();
         UInt_t eventNumber = 0;
         if (event != NULL) eventNumber = event->GetEventNumber();
         UInt_t nPlots = data->GetNChannels();
 
-        if (! OpenAndVerifyFile()) return;
+        if (! fWriter->OpenAndVerifyFile()) return;
 
         for (unsigned iPlot=0; iPlot<nPlots; iPlot++)
         {
@@ -268,26 +130,21 @@ namespace Katydid
             string histName;
             conv >> histName;
             TH2D* swHist = data->CreateMagnitudeHistogram(iPlot, histName);
-            swHist->SetDirectory(fFile);
+            swHist->SetDirectory(fWriter->GetFile());
             swHist->Write();
             KTDEBUG(publog, "Histogram <" << histName << "> written to ROOT file");
         }
         return;
     }
 
-    void KTBasicROOTFileWriter::Write(const KTSlidingWindowFSDataFFTW* data)
-    {
-        return WriteSlidingWindowFSDataFFTW(data);
-    }
-
-    void KTBasicROOTFileWriter::WriteSlidingWindowFSDataFFTW(const KTSlidingWindowFSDataFFTW* data)
+    void KTBasicROOTTypeWriterFFT::WriteSlidingWindowFSDataFFTW(const KTSlidingWindowFSDataFFTW* data)
     {
         KTEvent* event = data->GetEvent();
         UInt_t eventNumber = 0;
         if (event != NULL) eventNumber = event->GetEventNumber();
         UInt_t nPlots = data->GetNChannels();
 
-        if (! OpenAndVerifyFile()) return;
+        if (! fWriter->OpenAndVerifyFile()) return;
 
         for (unsigned iPlot=0; iPlot<nPlots; iPlot++)
         {
@@ -296,92 +153,12 @@ namespace Katydid
             string histName;
             conv >> histName;
             TH2D* swHist = data->CreateMagnitudeHistogram(iPlot, histName);
-            swHist->SetDirectory(fFile);
+            swHist->SetDirectory(fWriter->GetFile());
             swHist->Write();
             KTDEBUG(publog, "Histogram <" << histName << "> written to ROOT file");
         }
         return;
     }
 
-    //************************
-    // Hough Transform Data
-    //************************
-
-    void KTBasicROOTFileWriter::Write(const KTHoughData* data)
-    {
-        return WriteHoughData(data);
-    }
-
-    void KTBasicROOTFileWriter::WriteHoughData(const KTHoughData* data)
-    {
-        KTEvent* event = data->GetEvent();
-        UInt_t eventNumber = 0;
-        if (event != NULL) eventNumber = event->GetEventNumber();
-        UInt_t nPlots = data->GetNTransforms();
-
-        if (! OpenAndVerifyFile()) return;
-
-        for (unsigned iPlot=0; iPlot<nPlots; iPlot++)
-        {
-            stringstream conv;
-            conv << "histHT_" << eventNumber << "_" << iPlot;
-            string histName;
-            conv >> histName;
-            TH2D* swHist = data->CreateHistogram(iPlot, histName);
-            swHist->SetDirectory(fFile);
-            swHist->Write();
-            KTDEBUG(publog, "Histogram <" << histName << "> written to ROOT file");
-        }
-        return;
-    }
-
-    //************************
-    // Gain Variation Data
-    //************************
-
-    void KTBasicROOTFileWriter::Write(const KTGainVariationData* data)
-    {
-        return WriteGainVariationData(data);
-    }
-
-    void KTBasicROOTFileWriter::WriteGainVariationData(const KTGainVariationData* data)
-    {
-        KTEvent* event = data->GetEvent();
-        UInt_t eventNumber = 0;
-        if (event != NULL) eventNumber = event->GetEventNumber();
-        UInt_t nPlots = data->GetNChannels();
-
-        if (! OpenAndVerifyFile()) return;
-
-        for (unsigned iPlot=0; iPlot<nPlots; iPlot++)
-        {
-            stringstream conv;
-            conv << "histGV_" << eventNumber << "_" << iPlot;
-            string histName;
-            conv >> histName;
-            TH1D* gvHist = data->CreateGainVariationHistogram(100, iPlot, histName);
-            gvHist->SetDirectory(fFile);
-            gvHist->Write();
-            KTDEBUG(publog, "Histogram <" << histName << "> written to ROOT file");
-
-            /*
-            stringstream conv2;
-            string splineName;
-            conv2 << "splineGV_" << eventNumber << "_" << iPlot;
-            conv2 >> splineName;
-            const TSpline* spline = data->GetSpline(iPlot);
-            if (spline == NULL)
-            {
-                KTDEBUG(publog, "No spline in the data");
-                continue;
-            }
-            TSpline* splineClone = (TSpline*)spline->Clone();
-            splineClone->SetName(splineName.c_str());
-            splineClone->Write();
-            KTDEBUG(publog, "Spline <" << splineName << "> written to ROOT file");
-            */
-        }
-        return;
-    }
 
 } /* namespace Katydid */
--- Source/FFT/KTBasicROOTTypeWriterFFT.hh
+++ Source/FFT/KTBasicROOTTypeWriterFFT.hh
@@ -1,72 +1,33 @@
 /*
- * KTBasicROOTFileWriter.hh
+ * KTBasicROOTTypeWriterFFT.hh
  *
- *  Created on: Aug 24, 2012
+ *  Created on: Jan 3, 2013
  *      Author: nsoblath
  */
 
-#ifndef KTBASICROOTFILEWRITER_HH_
-#define KTBASICROOTFILEWRITER_HH_
+#ifndef KTBASICROOTTYPEWRITERFFT_HH_
+#define KTBASICROOTTYPEWRITERFFT_HH_
 
-#include "KTWriter.hh"
-#include "KTFrequencySpectrumData.hh"
-#include "KTFrequencySpectrumDataFFTW.hh"
-#include "KTCorrelationData.hh"
-#include "KTHoughData.hh"
-
-#include "TFile.h"
+#include "KTBasicROOTFileWriter.hh"
 
 namespace Katydid
 {
+    class KTFrequencySpectrumData;
+    class KTFrequencySpectrumDataFFTW;
     class KTSlidingWindowFSData;
     class KTSlidingWindowFSDataFFTW;
 
-    class KTBasicROOTFileWriter : public KTWriter
+    class KTBasicROOTTypeWriterFFT : public KTBasicROOTTypeWriter
     {
         public:
-            KTBasicROOTFileWriter();
-            virtual ~KTBasicROOTFileWriter();
+            friend class KTTimeSeriesData;
 
-            Bool_t Configure(const KTPStoreNode* node);
-
-            //***************************
-           // ROOT-file-specific members
-            //***************************
         public:
-            TFile* OpenFile(const std::string& filename, const std::string& flag);
-            void CloseFile();
+            KTBasicROOTTypeWriterFFT();
+            virtual ~KTBasicROOTTypeWriterFFT();
 
-            const std::string& GetFilename() const;
-            void SetFilename(const std::string& filename);
+            void RegisterSlots();
 
-            const std::string& GetFileFlag() const;
-            void SetFileFlag(const std::string& flag);
-
-            TFile* GetFile();
-
-        protected:
-            Bool_t OpenAndVerifyFile();
-
-        protected:
-            std::string fFilename;
-            std::string fFileFlag;
-
-            TFile* fFile;
-
-            //************************
-            // Basic Publish and Write
-            //************************
-        public:
-
-            void Publish(const KTWriteableData* data);
-
-            void Write(const KTWriteableData* data);
-
-            //************************
-            // Time Series Data
-            //************************
-        public:
-            void Write(const KTTimeSeriesData* data);
 
             //************************
             // Frequency Spectrum Data
@@ -74,15 +35,6 @@ namespace Katydid
         public:
             void WriteFrequencySpectrumData(const KTFrequencySpectrumData* data);
             void WriteFrequencySpectrumDataFFTW(const KTFrequencySpectrumDataFFTW* data);
-            void Write(const KTFrequencySpectrumData* data);
-            void Write(const KTFrequencySpectrumDataFFTW* data);
-
-            //************************
-            // Correlation Data
-            //************************
-        public:
-            void WriteCorrelationData(const KTCorrelationData* data);
-            void Write(const KTCorrelationData* data);
 
             //************************
             // Sliding Window Data
@@ -90,67 +42,8 @@ namespace Katydid
         public:
             void WriteSlidingWindowFSData(const KTSlidingWindowFSData* data);
             void WriteSlidingWindowFSDataFFTW(const KTSlidingWindowFSDataFFTW* data);
-            void Write(const KTSlidingWindowFSData* data);
-            void Write(const KTSlidingWindowFSDataFFTW* data);
-
-            //************************
-            // Hough Transform Data
-            //************************
-        public:
-            void WriteHoughData(const KTHoughData* data);
-            void Write(const KTHoughData* data);
-
-            //************************
-            // Gain Variation Data
-            //************************
-        public:
-            void WriteGainVariationData(const KTGainVariationData* data);
-            void Write(const KTGainVariationData* data);
 
     };
 
-    inline TFile* KTBasicROOTFileWriter::OpenFile(const std::string& filename, const std::string& flag)
-    {
-        CloseFile();
-        fFile = new TFile(filename.c_str(), flag.c_str());
-        return fFile;
-    }
-    inline void KTBasicROOTFileWriter::CloseFile()
-    {
-        if (fFile != NULL)
-        {
-            fFile->Close();
-            delete fFile;
-            fFile = NULL;
-        }
-        return;
-    }
-
-    inline const std::string& KTBasicROOTFileWriter::GetFilename() const
-    {
-        return fFilename;
-    }
-    inline void KTBasicROOTFileWriter::SetFilename(const std::string& filename)
-    {
-        fFilename = filename;
-        return;
-    }
-
-    inline const std::string& KTBasicROOTFileWriter::GetFileFlag() const
-    {
-        return fFileFlag;
-    }
-    inline void KTBasicROOTFileWriter::SetFileFlag(const std::string& flag)
-    {
-        fFileFlag = flag;
-        return;
-    }
-
-    inline TFile* KTBasicROOTFileWriter::GetFile()
-    {
-        return fFile;
-    }
-
-
 } /* namespace Katydid */
-#endif /* KTBASICROOTFILEWRITER_HH_ */
+#endif /* KTBASICROOTTYPEWRITERFFT_HH_ */
