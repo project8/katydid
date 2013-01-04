144c0ce05d84b3b1f7b3a6d7bb2202e92a769eeb
Noah Oblath <nsoblath@mit.edu>
2013-01-04 12:44:56 -0500

    Added KTJSONTypeWriterEgg, with the ability to print an egg header.

---
+++ Source/Egg/KTJSONTypeWriterEgg.hh
@@ -0,0 +1,34 @@
+/*
+ * KTJSONTypeWriterEgg.hh
+ *
+ *  Created on: Jan 4, 2013
+ *      Author: nsoblath
+ */
+
+#ifndef KTJSONTYPEWRITEREGG_HH_
+#define KTJSONTYPEWRITEREGG_HH_
+
+#include "KTJSONWriter.hh"
+
+namespace Katydid
+{
+    class KTEggHeader;
+
+    class KTJSONTypeWriterEgg : public KTJSONTypeWriter//, public KTTypeWriterEgg
+    {
+        public:
+            friend class KTTimeSeriesData;
+
+        public:
+            KTJSONTypeWriterEgg();
+            virtual ~KTJSONTypeWriterEgg();
+
+            void RegisterSlots();
+
+        public:
+            void WriteEggHeader(const KTEggHeader* header);
+
+    };
+
+} /* namespace Katydid */
+#endif /* KTJSONTYPEWRITEREGG_HH_ */
