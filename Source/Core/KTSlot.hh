c2872cba23d59918509be177ef308e50b17015cf
Noah Oblath <nsoblath@mit.edu>
2013-01-15 12:07:43 -0500

    KTDataSlotOneArg compiles and added KTEventSlotOneArg (also compiles). Added documentation on usage of KTSlotOneArg, KTDataSlotOneArg, and KTEventSlotOneArg.

--- Source/Core/KTSlot.hh
+++ Source/Core/KTSlot.hh
@@ -8,6 +8,12 @@
 #ifndef KTSLOT_HH_
 #define KTSLOT_HH_
 
+#include "KTEvent.hh"
+#include "KTSignalWrapper.hh"
+
+#include <boost/shared_ptr.hpp>
+
+#include <deque>
 #include <string>
 
 namespace Katydid
@@ -20,49 +26,68 @@ namespace Katydid
     };
 
 
-    template< class XProcessorType, class XReturnType, class XArgumentType >
+    /*!
+     @class KTSlotOneArg
+     @author N. S. Oblath
+
+     @brief Creates a slot function that calls a Processor member function taking one argument.
+
+     @details
+     Usage:
+     To use this slot type your processor must have a member function with the signature void (ArgumentType).
+
+     In your Processor's header add a member variable of type KTSlotOneArg< ProcessorType, ArgumentType >.
+
+     Initialize the slot with the address of the Processor.
+
+     In your Processor's constructor, call the KTSlotOneArg::RegisterSlot function, passing the following as arguments:
+     - slot name
+     - pointer to the member function that should be called (e.g. &KTMyProcessor::CallThisFunc)
+     - the function signature as a string
+    */
+    template< class XProcessorType, class XArgumentType >
     class KTSlotOneArg : public KTSlot
     {
         public:
             typedef XProcessorType processor_type;
-            typedef XReturnType return_type;
             typedef XArgumentType argument_type;
 
         public:
             KTSlotOneArg(XProcessorType* proc);
             virtual ~KTSlotOneArg();
 
-            XReturnType operator()(XArgumentType arg);
+            void operator()(XArgumentType arg);
 
-            void RegisterSlot(const std::string& name, XReturnType (XProcessorType::*func)(XArgumentType), const std::string& signature);
+            void RegisterSlot(const std::string& name, void (XProcessorType::*func)(XArgumentType), const std::string& signature);
 
         protected:
             XProcessorType* fProcessor;
 
-            XReturnType (XProcessorType::*fFuncPtr)(XArgumentType);
+            void (XProcessorType::*fFuncPtr)(XArgumentType);
     };
 
-    template< class XProcessorType, class XReturnType, class XArgumentType >
-    KTSlotOneArg< XProcessorType, XReturnType, XArgumentType >::KTSlotOneArg(XProcessorType* proc) :
+    template< class XProcessorType, class XArgumentType >
+    KTSlotOneArg< XProcessorType, XArgumentType >::KTSlotOneArg(XProcessorType* proc) :
             KTSlot(),
             fProcessor(proc),
             fFuncPtr(NULL)
     {
     }
 
-    template< class XProcessorType, class XReturnType, class XArgumentType >
-    KTSlotOneArg< XProcessorType, XReturnType, XArgumentType >::~KTSlotOneArg()
+    template< class XProcessorType, class XArgumentType >
+    KTSlotOneArg< XProcessorType, XArgumentType >::~KTSlotOneArg()
     {
     }
 
-    template< class XProcessorType, class XReturnType, class XArgumentType >
-    XReturnType KTSlotOneArg< XProcessorType, XReturnType, XArgumentType >::operator()(XArgumentType arg)
+    template< class XProcessorType, class XArgumentType >
+    void KTSlotOneArg< XProcessorType, XArgumentType >::operator()(XArgumentType arg)
     {
-        return fProcessor->fFuncPtr(arg);
+        (fProcessor->*fFuncPtr)(arg);
+        return;
     }
 
-    template< class XProcessorType, class XReturnType, class XArgumentType >
-    void KTSlotOneArg< XProcessorType, XReturnType, XArgumentType >::RegisterSlot(const std::string& name, XReturnType (XProcessorType::*func)(XArgumentType), const std::string& signature)
+    template< class XProcessorType, class XArgumentType >
+    void KTSlotOneArg< XProcessorType, XArgumentType >::RegisterSlot(const std::string& name, void (XProcessorType::*func)(XArgumentType), const std::string& signature)
     {
         fFuncPtr = func;
         fProcessor->template RegisterSlot< XProcessorType, void, XArgumentType >(name, fProcessor, fFuncPtr, signature);
@@ -70,62 +95,240 @@ namespace Katydid
     }
 
 
+    /*!
+     @class KTDataSlotOneArg
+     @author N. S. Oblath
 
-    template< class XProcessorType, class XReturnType, class XArgumentType >
+     @brief Creates a slot function that calls a Processor member function taking one Data type as an argument.
+
+     @details
+     Usage:
+     To use this slot type your processor must have a member function with the signature OutputDataType (const InputDataType*).
+     OutputDataType and InputDataType should be the data types themselves, without any const modifiers, ampersands, or asterisks.
+
+     In your Processor's header add a member variable of type KTDataSlotOneArg< ProcessorType, OutputDataType, InputDataType >.
+
+     Initialize the slot with the address of the Processor.
+
+     In your Processor's constructor, call the KTDataSlotOneArg::RegisterSlot function, passing the following as arguments:
+     - slot name
+     - pointer to the member function that should be called (e.g. &KTMyProcessor::CallThisFunc)
+     - the function signature as a string
+
+     Also in the Processor's constructor, if you want a signal called upon creation of the new data object, assign the address of the signal with KTDataSlotOneArg::SetSignal.
+    */
+    template< class XProcessorType, class XReturnDataType, class XDataType >
     class KTDataSlotOneArg : public KTSlot
     {
         public:
             typedef XProcessorType processor_type;
-            typedef XReturnType return_type;
-            typedef XArgumentType argument_type;
+            typedef XReturnDataType return_type;
+            typedef XDataType argument_type;
+
+            typedef typename KTSignal< void (const XReturnDataType* ) >::signal signal_type;
 
         public:
             KTDataSlotOneArg(XProcessorType* proc);
             virtual ~KTDataSlotOneArg();
 
-            XReturnType operator()(XArgumentType arg);
+            void operator()(const XDataType* data);
 
-            void RegisterSlot(const std::string& name, XReturnType (XProcessorType::*func)(XArgumentType), const std::string& signature);
+            void RegisterSlot(const std::string& name, XReturnDataType* (XProcessorType::*func)(const XDataType*), const std::string& signature);
+
+            template< typename XSignal >
+            void SetSignal(XSignal* signalPtr);
 
         protected:
             XProcessorType* fProcessor;
 
-            XReturnType (XProcessorType::*fFuncPtr)(XArgumentType);
+            XReturnDataType* (XProcessorType::*fFuncPtr)(const XDataType*);
+
+            signal_type* fSignalPtr;
     };
 
-    template< class XProcessorType, class XReturnType, class XArgumentType >
-    KTDataSlotOneArg< XProcessorType, XReturnType, XArgumentType >::KTDataSlotOneArg(XProcessorType* proc) :
+    template< class XProcessorType, class XReturnDataType, class XDataType >
+    KTDataSlotOneArg< XProcessorType, XReturnDataType, XDataType >::KTDataSlotOneArg(XProcessorType* proc) :
             KTSlot(),
             fProcessor(proc),
-            fFuncPtr(NULL)
+            fFuncPtr(NULL),
+            fSignalPtr(NULL)
     {
     }
 
-    template< class XProcessorType, class XReturnType, class XArgumentType >
-    KTDataSlotOneArg< XProcessorType, XReturnType, XArgumentType >::~KTDataSlotOneArg()
+    template< class XProcessorType, class XReturnType, class XDataType >
+    KTDataSlotOneArg< XProcessorType, XReturnType, XDataType >::~KTDataSlotOneArg()
     {
     }
 
-    template< class XProcessorType, class XReturnType, class XArgumentType >
-    XReturnType KTDataSlotOneArg< XProcessorType, XReturnType, XArgumentType >::operator()(XArgumentType arg)
+    template< class XProcessorType, class XReturnDataType, class XDataType >
+    void KTDataSlotOneArg< XProcessorType, XReturnDataType, XDataType >::operator()(const XDataType* data)
     {
-        const XReturnType& retVal = fProcessor->fFuncPtr(arg);
-        KTEvent* event = retVal->GetEvent();
+        // Standard data slot pattern:
+        // First the processor acts on the data, returning a new data object
+        XReturnDataType* newData = (fProcessor->*fFuncPtr)(data);
+        // Extract the event pointer from the old data, and set the event in the new data (even if it's NULL
+        KTEvent* event = data->GetEvent();
+        newData->SetEvent(event);
+        // If the event isn't NULL, add the new data to the event
         if (event != NULL)
         {
-            retVal->SetEvent(event);
-            event->AddData(retVal);
+            event->AddData(newData);
         }
-        return retVal;
-    }
-
-    template< class XProcessorType, class XReturnType, class XArgumentType >
-    void KTDataSlotOneArg< XProcessorType, XReturnType, XArgumentType >::RegisterSlot(const std::string& name, XReturnType (XProcessorType::*func)(XArgumentType), const std::string& signature)
-    {
-        fFuncPtr = func;
-        fProcessor->template RegisterSlot< XProcessorType, void, XArgumentType >(name, fProcessor, fFuncPtr, signature);
+        // If there's a signal pointer, emit the signal
+        if (fSignalPtr != NULL)
+        {
+            (*fSignalPtr)(newData);
+        }
         return;
     }
+
+    template< class XProcessorType, class XReturnDataType, class XDataType >
+    void KTDataSlotOneArg< XProcessorType, XReturnDataType, XDataType >::RegisterSlot(const std::string& name, XReturnDataType* (XProcessorType::*func)(const XDataType*), const std::string& signature)
+    {
+        fFuncPtr = func;
+        fProcessor->RegisterSlot(name, this, &KTDataSlotOneArg< XProcessorType, XReturnDataType, XDataType >::operator(), signature);
+        return;
+    }
+
+    template< class XProcessorType, class XReturnDataType, class XDataType >
+    template< typename XSignal >
+    void KTDataSlotOneArg< XProcessorType, XReturnDataType, XDataType >::SetSignal(XSignal* signalPtr)
+    {
+        fSignalPtr = (signal_type*)signal;
+        return;
+    }
+
+
+
+    /*!
+     @class KTEventSlotOneArg
+     @author N. S. Oblath
+
+     @brief Creates a slot function that calls a Processor member function taking an event pointer as the argument.
+
+     @details
+     Usage:
+     To use this slot type your processor must one ore more KTDataSlotOneArg members that you wish to use.
+
+     In your Processor's header add a member variable of type KTEventSlotOneArg< ProcessorType >.
+
+     Initialize the slot with the address of the Processor.
+
+     In your Processor's constructor, call the KTEventSlotOneArg::RegisterSlot function, passing the following as arguments:
+     - slot name
+     - pointer to the input data name
+
+     Also in the Processor's constructor, call the KTEventSlotOneArg::AddDataSlot function for each data slot you wish to be called.
+     The argument for this function is the address of the KTDataSlotOneArg object.
+    */
+    template< class XProcessorType >
+    class KTEventSlotOneArg : public KTSlot
+    {
+        public:
+            typedef XProcessorType processor_type;
+
+        private:
+            class KTInternalSlotFunction
+            {
+                public:
+                    KTInternalSlotFunction() {}
+                    virtual ~KTInternalSlotFunction() {}
+
+                    virtual Bool_t FindAndProcessData(const std::string* dataName, boost::shared_ptr<KTEvent> event) = 0;
+            };
+
+            template< class XReturnDataType, class XDataType >
+            class KTSpecifiedInternalSlotFunction : public KTInternalSlotFunction, public boost::noncopyable
+            {
+                public:
+                    KTSpecifiedInternalSlotFunction(KTDataSlotOneArg< XProcessorType, XReturnDataType, XDataType >* dataSlotPtr) :
+                        KTInternalSlotFunction(),
+                        fDataSlotPtr(dataSlotPtr)
+                    {}
+                    virtual ~KTSpecifiedInternalSlotFunction() {}
+
+                    virtual Bool_t FindAndProcessData(const std::string* dataName, boost::shared_ptr<KTEvent> event)
+                    {
+                        const XDataType* inputData = event->GetData< XDataType >(*dataName);
+                        if (inputData != NULL)
+                        {
+                            (*fDataSlotPtr)(inputData);
+                            return true;
+                        }
+                        return false;
+                    }
+
+                private:
+                    KTDataSlotOneArg< XProcessorType, XReturnDataType, XDataType >* fDataSlotPtr;
+            };
+
+        public:
+            KTEventSlotOneArg(XProcessorType* proc);
+            virtual ~KTEventSlotOneArg();
+
+            void operator()(boost::shared_ptr<KTEvent> event);
+
+            void RegisterSlot(const std::string& slotName, const std::string* inputDataName);
+
+            template< class XReturnDataType, class XDataType >
+            void AddDataSlot(KTDataSlotOneArg< XProcessorType, XReturnDataType, XDataType >* dataSlotPtr);
+
+        protected:
+            XProcessorType* fProcessor;
+
+            std::deque< KTInternalSlotFunction* > fSlotFunctions;
+
+            const std::string* fInputDataName;
+
+    };
+
+    template< class XProcessorType >
+    KTEventSlotOneArg< XProcessorType >::KTEventSlotOneArg(XProcessorType* proc) :
+            KTSlot(),
+            fProcessor(proc),
+            fSlotFunctions(),
+            fInputDataName(NULL)
+    {
+    }
+
+    template< class XProcessorType >
+    KTEventSlotOneArg< XProcessorType >::~KTEventSlotOneArg()
+    {
+        while (! fSlotFunctions.empty())
+        {
+            delete fSlotFunctions.back();
+            fSlotFunctions.pop_back();
+        }
+    }
+
+    template< class XProcessorType >
+    void KTEventSlotOneArg< XProcessorType >::operator()(boost::shared_ptr<KTEvent> event)
+    {
+        for (typename std::deque< KTInternalSlotFunction* >::iterator slotIt = fSlotFunctions.begin(); slotIt != fSlotFunctions.end(); slotIt++)
+        {
+            if ((*slotIt)->FindAndProcessData(fInputDataName, event))
+                break;
+        }
+        return;
+    }
+
+    template< class XProcessorType >
+    template< class XReturnDataType, class XDataType >
+    void KTEventSlotOneArg< XProcessorType >::AddDataSlot(KTDataSlotOneArg< XProcessorType, XReturnDataType, XDataType >* dataSlotPtr)
+    {
+        KTInternalSlotFunction* newSlotFcn = new KTSpecifiedInternalSlotFunction< XReturnDataType, XDataType >(dataSlotPtr);
+        fSlotFunctions.push_back(newSlotFcn);
+        return;
+    }
+
+    template< class XProcessorType >
+    void KTEventSlotOneArg< XProcessorType >::RegisterSlot(const std::string& slotName, const std::string* inputDataName)
+    {
+        fInputDataName = inputDataName;
+        fProcessor->RegisterSlot(slotName, this, &KTEventSlotOneArg< XProcessorType >::operator(), "void (boost::shared_ptr<KTEvent>)");
+        return;
+    }
+
 
 } /* namespace Katydid */
 #endif /* KTSLOT_HH_ */
