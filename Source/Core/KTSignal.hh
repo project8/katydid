7120abd9951bf9aba85f4a52441829d0c2185c30
Noah Oblath <nsoblath@mit.edu>
2013-01-15 18:48:46 -0500

    Added documentation for KTDataSlot. Changed the boost::signals2::signal<signature> typedef to boost_signal.

--- Source/Core/KTSignal.hh
+++ Source/Core/KTSignal.hh
@@ -15,12 +15,31 @@
 namespace Katydid
 {
 
+    /*!
+     @class KTDataSignal
+     @author N. S. Oblath
+
+     @brief Creates a signal that is emitted when the processor creates the Data object.
+
+     @details
+     The signal is emitted by calling EmitSignal.
+     If a KTDataSlot is being used, and the Slot has been given a pointer to this signal, the Slot will emit the Signal.
+
+     Usage:
+     In your Processor's header add a member variable of type KTDataSignal< ProcessorType, OutputDataType >.
+
+     Initialize the signal with the address of the Processor.
+
+     In your Processor's constructor, call the KTDataSignal::RegisterSlot function, passing the following as arguments:
+     - slot name
+     - the function signature as a string
+    */
     template< class XProcessorType, class XDataType >
     class KTDataSignal
     {
         public:
             typedef void (signature)(const XDataType*);
-            typedef boost::signals2::signal< signature > signal;
+            typedef boost::signals2::signal< signature > boost_signal;
             typedef typename boost::signals2::signal< signature >::slot_type slot_type;
 
         public:
@@ -35,7 +54,7 @@ namespace Katydid
         private:
             XProcessorType* fProcessor;
 
-            signal fSignal;
+            boost_signal fSignal;
 
     };
 
