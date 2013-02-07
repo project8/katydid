716b4a5aceda9db742ce37dc710b5aeb1a80ef40
Noah Oblath <nsoblath@mit.edu>
2013-02-07 18:06:41 -0500

    Added KTWaterfallCandidateData. Implementation not at all complete.

---
+++ Source/Candidates/KTWaterfallCandidateData.cc
@@ -0,0 +1,24 @@
+/*
+ * KTWaterfallCandidateData.cc
+ *
+ *  Created on: Feb 7, 2013
+ *      Author: nsoblath
+ */
+
+#include "KTWaterfallCandidateData.hh"
+
+namespace Katydid
+{
+
+    KTWaterfallCandidateData::KTWaterfallCandidateData()
+    {
+        // TODO Auto-generated constructor stub
+
+    }
+
+    KTWaterfallCandidateData::~KTWaterfallCandidateData()
+    {
+        // TODO Auto-generated destructor stub
+    }
+
+} /* namespace Katydid */
