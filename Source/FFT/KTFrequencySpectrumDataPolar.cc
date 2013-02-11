cbaf01679b1b2be0fbeb475a15f76aeee7a37db6
Noah Oblath <nsoblath@mit.edu>
2013-02-10 23:16:16 -0500

    Added KTFrequencySpectrumDataPolar.

---
+++ Source/FFT/KTFrequencySpectrumDataPolar.cc
@@ -0,0 +1,43 @@
+/*
+ * KTFrequencySpectrumDataPolar.cc
+ *
+ *  Created on: Aug 24, 2012
+ *      Author: nsoblath
+ */
+
+#include "KTFrequencySpectrumDataPolar.hh"
+
+#include "KTDataMap.hh"
+#include "KTFrequencySpectrumPolar.hh"
+#include "KTTIFactory.hh"
+#include "KTWriter.hh"
+
+namespace Katydid
+{
+    static KTDerivedTIRegistrar< KTDataMap, KTDerivedDataMap< KTFrequencySpectrumDataPolar > > sFSDMMRegistrar;
+
+    KTFrequencySpectrumDataPolar::KTFrequencySpectrumDataPolar(UInt_t nChannels) :
+            KTWriteableData(),
+            fSpectra(nChannels),
+            fTimeInRun(0.),
+            fSliceNumber(0)
+    {
+    }
+
+    KTFrequencySpectrumDataPolar::~KTFrequencySpectrumDataPolar()
+    {
+        while (! fSpectra.empty())
+        {
+            delete fSpectra.back();
+            fSpectra.pop_back();
+        }
+    }
+
+    void KTFrequencySpectrumDataPolar::Accept(KTWriter* writer) const
+    {
+        writer->Write(this);
+        return;
+    }
+
+} /* namespace Katydid */
+
---
+++ Source/FFT/KTFrequencySpectrumDataPolar.hh
@@ -0,0 +1,147 @@
+/*
+ * KTFrequencySpectrumDataPolar.hh
+ *
+ *  Created on: Aug 24, 2012
+ *      Author: nsoblath
+ */
+
+#ifndef KTFREQUENCYSPECTRUMDATAPOLAR_HH_
+#define KTFREQUENCYSPECTRUMDATAPOLAR_HH_
+
+#include "KTWriteableData.hh"
+
+#include "KTFrequencySpectrumPolar.hh"
+
+#ifdef ROOT_FOUND
+#include "TH1.h"
+#endif
+
+#include <vector>
+
+namespace Katydid
+{
+
+    class KTFrequencySpectrumDataPolar : public KTWriteableData
+    {
+        public:
+            KTFrequencySpectrumDataPolar(UInt_t nChannels=1);
+            virtual ~KTFrequencySpectrumDataPolar();
+
+            const KTFrequencySpectrumPolar* GetSpectrumPolar(UInt_t channelNum = 0) const;
+            KTFrequencySpectrumPolar* GetSpectrumPolar(UInt_t channelNum = 0);
+
+            const KTFrequencySpectrumPolar* GetSpectrum(UInt_t channelNum = 0) const;
+            KTFrequencySpectrumPolar* GetSpectrum(UInt_t channelNum = 0);
+            UInt_t GetNChannels() const;
+            Double_t GetTimeInRun() const;
+            ULong64_t GetSliceNumber() const;
+
+            void SetSpectrum(KTFrequencySpectrumPolar* record, UInt_t channelNum = 0);
+
+            void SetNChannels(UInt_t channels);
+            void SetTimeInRun(Double_t tir);
+            void SetSliceNumber(ULong64_t slice);
+
+            void Accept(KTWriter* writer) const;
+
+        protected:
+            std::vector< KTFrequencySpectrumPolar* > fSpectra;
+
+            Double_t fTimeInRun;
+            ULong64_t fSliceNumber;
+
+#ifdef ROOT_FOUND
+        public:
+            virtual TH1D* CreateMagnitudeHistogram(UInt_t channelNum = 0, const std::string& name = "hFrequencySpectrumMag") const;
+            virtual TH1D* CreatePhaseHistogram(UInt_t channelNum = 0, const std::string& name = "hFrequencySpectrumPhase") const;
+
+            virtual TH1D* CreatePowerHistogram(UInt_t channelNum = 0, const std::string& name = "hFrequencySpectrumPower") const;
+
+            virtual TH1D* CreatePowerDistributionHistogram(UInt_t channelNum = 0, const std::string& name = "hFrequencySpectrumPowerDist") const;
+#endif
+    };
+
+    inline const KTFrequencySpectrumPolar* KTFrequencySpectrumDataPolar::GetSpectrumPolar(UInt_t channelNum) const
+    {
+        return fSpectra[channelNum];
+    }
+
+    inline KTFrequencySpectrumPolar* KTFrequencySpectrumDataPolar::GetSpectrumPolar(UInt_t channelNum)
+    {
+        return fSpectra[channelNum];
+    }
+
+    inline const KTFrequencySpectrumPolar* KTFrequencySpectrumDataPolar::GetSpectrum(UInt_t channelNum) const
+    {
+        return fSpectra[channelNum];
+    }
+
+    inline KTFrequencySpectrumPolar* KTFrequencySpectrumDataPolar::GetSpectrum(UInt_t channelNum)
+    {
+        return fSpectra[channelNum];
+    }
+
+    inline UInt_t KTFrequencySpectrumDataPolar::GetNChannels() const
+    {
+        return UInt_t(fSpectra.size());
+    }
+
+    inline Double_t KTFrequencySpectrumDataPolar::GetTimeInRun() const
+    {
+        return fTimeInRun;
+    }
+
+    inline ULong64_t KTFrequencySpectrumDataPolar::GetSliceNumber() const
+    {
+        return fSliceNumber;
+    }
+
+    inline void KTFrequencySpectrumDataPolar::SetSpectrum(KTFrequencySpectrumPolar* record, UInt_t channelNum)
+    {
+        if (channelNum >= fSpectra.size()) fSpectra.resize(channelNum+1);
+        fSpectra[channelNum] = record;
+    }
+
+    inline void KTFrequencySpectrumDataPolar::SetNChannels(UInt_t channels)
+    {
+        fSpectra.resize(channels);
+        return;
+    }
+
+    inline void KTFrequencySpectrumDataPolar::SetTimeInRun(Double_t tir)
+    {
+        fTimeInRun = tir;
+        return;
+    }
+
+    inline void KTFrequencySpectrumDataPolar::SetSliceNumber(ULong64_t slice)
+    {
+        fSliceNumber = slice;
+        return;
+    }
+
+#ifdef ROOT_FOUND
+    inline TH1D* KTFrequencySpectrumDataPolar::CreateMagnitudeHistogram(UInt_t channelNum, const std::string& name) const
+    {
+        return fSpectra[channelNum]->CreateMagnitudeHistogram(name);
+    }
+    inline TH1D* KTFrequencySpectrumDataPolar::CreatePhaseHistogram(UInt_t channelNum, const std::string& name) const
+    {
+        return fSpectra[channelNum]->CreatePhaseHistogram(name);
+    }
+
+    inline TH1D* KTFrequencySpectrumDataPolar::CreatePowerHistogram(UInt_t channelNum, const std::string& name) const
+    {
+        return fSpectra[channelNum]->CreatePowerHistogram(name);
+    }
+
+    inline TH1D* KTFrequencySpectrumDataPolar::CreatePowerDistributionHistogram(UInt_t channelNum, const std::string& name) const
+    {
+        return fSpectra[channelNum]->CreatePowerDistributionHistogram(name);
+    }
+#endif
+
+
+} /* namespace Katydid */
+
+#endif /* KTFREQUENCYSPECTRUMDATAPOLAR_HH_ */
