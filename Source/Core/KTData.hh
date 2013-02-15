3253477924a4739c07f6934456b56cb4a25f0f75
Noah Oblath <nsoblath@mit.edu>
2013-02-15 00:38:11 -0500

    Complete revamping of KTData for the extensible struct system. Removed unnecessary classes from the build.

--- Source/Core/KTData.hh
+++ Source/Core/KTData.hh
@@ -8,58 +8,27 @@
 #ifndef KTDATA_HH_
 #define KTDATA_HH_
 
+#include "KTExtensibleStruct.hh"
+
 #include <string>
 
 namespace Katydid
 {
-    class KTBundle;
+    struct KTDataGroup {};
 
-    class KTData
+    template< class XDerivedType >
+    struct KTExtensibleData : KTExtensibleStruct< XDerivedType, KTDataGroup >
+    {};
+
+    struct KTData : KTExtensibleData< KTData >
     {
-        private:
-            friend class KTBundle;
-
-        public:
-            KTData();
-            virtual ~KTData();
-
-        public:
-            const std::string& GetName() const;
-            void SetName(const std::string& name);
-
-        protected:
-            std::string fName;
-
-        public:
-            /// Returns a pointer to the bundle from which this data object originates
-            virtual KTBundle* GetBundle() const;
-            void SetBundle(KTBundle* bundle);
-
-        protected:
-            KTBundle* fBundle; // not owned by this object
+        UInt_t fCounter;
+        Bool_t fLastData;
+        KTData() :
+            fCounter(0),
+            fLastData(false)
+        {};
     };
-
-    inline KTBundle* KTData::GetBundle() const
-    {
-        return fBundle;
-    }
-
-    inline void KTData::SetBundle(KTBundle* bundle)
-    {
-        fBundle = bundle;
-        return;
-    }
-
-    inline const std::string& KTData::GetName() const
-    {
-        return fName;
-    }
-
-    inline void KTData::SetName(const std::string& name)
-    {
-        fName = name;
-        return;
-    }
 
 } /* namespace Katydid */
 #endif /* KTDATA_HH_ */
