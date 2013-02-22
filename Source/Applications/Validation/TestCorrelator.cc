a2083cd892569430e9a95f1292adfc51f40813f5
Noah Oblath <nsoblath@mit.edu>
2013-02-22 18:34:42 -0500

    Better FFTW-type spectra in TestCorrelator.

--- Source/Applications/Validation/TestCorrelator.cc
+++ Source/Applications/Validation/TestCorrelator.cc
@@ -53,7 +53,7 @@ int main(int argc, char** argv)
     KTFrequencySpectrumDataFFTW* dataInput = new KTFrequencySpectrumDataFFTW();
     dataInput->SetNComponents(2);
 
-    KTFrequencySpectrumFFTW* spectrum0 = new KTFrequencySpectrumFFTW(10, 0, 10);
+    KTFrequencySpectrumFFTW* spectrum0 = new KTFrequencySpectrumFFTW(19, -10, 10);
     (*spectrum0)(0)[0] = 0.; (*spectrum0)(0)[1] = 0.;
     (*spectrum0)(1)[0] = 0.; (*spectrum0)(1)[1] = 0.;
     (*spectrum0)(2)[0] = 1.; (*spectrum0)(2)[1] = 0.;
@@ -64,8 +64,17 @@ int main(int argc, char** argv)
     (*spectrum0)(7)[0] = 6.; (*spectrum0)(7)[1] = 0.;
     (*spectrum0)(8)[0] = 0.; (*spectrum0)(8)[1] = 1.;
     (*spectrum0)(9)[0] = 0.; (*spectrum0)(9)[1] = 10.;
+    (*spectrum0)(10)[0] = 0.; (*spectrum0)(10)[1] = 1.;
+    (*spectrum0)(11)[0] = 6.; (*spectrum0)(11)[1] = 0.;
+    (*spectrum0)(12)[0] = 5.; (*spectrum0)(12)[1] = 0.;
+    (*spectrum0)(13)[0] = 4.; (*spectrum0)(13)[1] = 0.;
+    (*spectrum0)(14)[0] = 3.; (*spectrum0)(14)[1] = 0.;
+    (*spectrum0)(15)[0] = 2.; (*spectrum0)(15)[1] = 0.;
+    (*spectrum0)(16)[0] = 1.; (*spectrum0)(16)[1] = 0.;
+    (*spectrum0)(17)[0] = 0.; (*spectrum0)(17)[1] = 0.;
+    (*spectrum0)(18)[0] = 0.; (*spectrum0)(18)[1] = 0.;
 
-    KTFrequencySpectrumFFTW* spectrum1 = new KTFrequencySpectrumFFTW(10, 0, 10);
+    KTFrequencySpectrumFFTW* spectrum1 = new KTFrequencySpectrumFFTW(19, -10, 10);
     (*spectrum1)(0)[0] = 0.; (*spectrum1)(0)[1] = 0.;
     (*spectrum1)(1)[0] = 0.; (*spectrum1)(1)[1] = 0.;
     (*spectrum1)(2)[0] = 0.; (*spectrum1)(2)[1] = 0.;
@@ -76,16 +85,25 @@ int main(int argc, char** argv)
     (*spectrum1)(7)[0] = 0.; (*spectrum1)(7)[1] = 0.;
     (*spectrum1)(8)[0] = 2.; (*spectrum1)(8)[1] = 2.;
     (*spectrum1)(9)[0] = 2.; (*spectrum1)(9)[1] = 2.;
+    (*spectrum1)(10)[0] = 2.; (*spectrum1)(10)[1] = 2.;
+    (*spectrum1)(11)[0] = 0.; (*spectrum1)(11)[1] = 0.;
+    (*spectrum1)(12)[0] = 5.; (*spectrum1)(12)[1] = 0.;
+    (*spectrum1)(13)[0] = 5.; (*spectrum1)(13)[1] = 0.;
+    (*spectrum1)(14)[0] = 5.; (*spectrum1)(14)[1] = 0.;
+    (*spectrum1)(15)[0] = 5.; (*spectrum1)(15)[1] = 0.;
+    (*spectrum1)(16)[0] = 0.; (*spectrum1)(16)[1] = 0.;
+    (*spectrum1)(17)[0] = 0.; (*spectrum1)(17)[1] = 0.;
+    (*spectrum1)(18)[0] = 0.; (*spectrum1)(18)[1] = 0.;
     /**/
 
     dataInput->SetSpectrum(spectrum0, 0);
     dataInput->SetSpectrum(spectrum1, 1);
 
     KTINFO(corrtestlog, "Spectrum 0");
-    spectrum0->Print(0, 10);
+    spectrum0->Print(0, 19);
 
     KTINFO(corrtestlog, "Spectrum 1");
-    spectrum1->Print(0, 10);
+    spectrum1->Print(0, 19);
 
     // Do the correlations
     KTCorrelator* correlator = new KTCorrelator();
@@ -100,12 +118,13 @@ int main(int argc, char** argv)
     }
     KTCorrelationData& dataOutput = dataInput->Of< KTCorrelationData >();
 
-    KTINFO(corrtestlog, "There are " << dataOutput.GetNComponents() << " ouptut spectra");
+    KTINFO(corrtestlog, "There are " << dataOutput.GetNComponents() << " output spectra");
     for (unsigned iSpectrum=0; iSpectrum < dataOutput.GetNComponents(); iSpectrum++)
     {
         KTINFO(corrtestlog, "Output Spectrum " << iSpectrum << "; "
                 "pair (" << dataOutput.GetInputPair().first << ", " <<
                 dataOutput.GetInputPair().second << ")");
+        KTINFO(corrtestlog, "Size of spectrum: " << dataOutput.GetSpectrumPolar(iSpectrum)->size());
         dataOutput.GetSpectrumPolar(iSpectrum)->Print(0, 10);
     }
 
