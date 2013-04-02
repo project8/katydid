4f4ca093d70a29455c2b2b46f99c035d155c63e2
Noah Oblath <nsoblath@mit.edu>
2013-04-02 16:46:22 -0400

    Switched to generic "PackageBuilder" instead of KatydidBuild.cmake. Made the necessary name changes.

--- cmake/PackageBuilder.cmake
+++ cmake/PackageBuilder.cmake
@@ -1,12 +1,12 @@
-# KatydidBuild.cmake
+# PackageBuilder.cmake
 # Author: Noah Oblath
 # Parts of this script are based on work done by Sebastian Voecking and Marco Haag in the Kasper package
 # Convenient macros and default variable settings for the Katydid build.
 
 if ("${CMAKE_BUILD_TYPE}" STREQUAL "DEBUG")
-    add_definitions(-DKATYDID_DEBUG)
+    add_definitions(-D${PROJECT_NAME}_DEBUG)
 else ("${CMAKE_BUILD_TYPE}" STREQUAL "DEBUG")
-    remove_definitions(-DKATYDID_DEBUG)    
+    remove_definitions(-D${PROJECT_NAME}_DEBUG)    
 endif ("${CMAKE_BUILD_TYPE}" STREQUAL "DEBUG")
 
 # Setup the default install prefix
@@ -64,7 +64,7 @@ endif ("${isSystemDir}" STREQUAL "-1")
 ##########
 
 # This should be called immediately after setting the project name
-macro (katydid_prepare_project VERSION_MAJOR VERSION_MINOR REVISION)
+macro (pbuilder_prepare_project VERSION_MAJOR VERSION_MINOR REVISION)
     # get git revision information
     include (GetGitRevisionDescription)
     git_describe (GIT_REV)
@@ -89,35 +89,35 @@ macro (katydid_prepare_project VERSION_MAJOR VERSION_MINOR REVISION)
     endif (EXISTS ${PROJECT_SOURCE_DIR}/${PROJECT_NAME}Config.hh.in)
 endmacro ()
 
-macro (katydid_install_libraries)
+macro (pbuilder_install_libraries)
     install (TARGETS ${ARGN} EXPORT ${PROJECT_NAME}Targets DESTINATION ${LIB_INSTALL_DIR})
     #list (APPEND ${PROJECT_NAME}_LIBRARIES ${ARGN})
     set_property (GLOBAL APPEND PROPERTY ${PROJECT_NAME}_LIBRARIES ${ARGN})
     set_target_properties (${ARGN} PROPERTIES INSTALL_NAME_DIR ${LIB_INSTALL_DIR})
 endmacro ()
 
-macro (katydid_install_executables)
+macro (pbuilder_install_executables)
     install(TARGETS ${ARGN} EXPORT ${PROJECT_NAME}Targets DESTINATION ${BIN_INSTALL_DIR})
 endmacro ()
 
-macro (katydid_install_headers)
+macro (pbuilder_install_headers)
     install(FILES ${ARGN} DESTINATION ${INCLUDE_INSTALL_DIR})
 endmacro ()
 
-macro (katydid_install_config)
+macro (pbuilder_install_config)
     install(FILES ${ARGN} DESTINATION ${CONFIG_INSTALL_DIR})
 endmacro ()
 
-macro (katydid_install_files DEST_DIR)
+macro (pbuilder_install_files DEST_DIR)
     install(FILES ${ARGN} DESTINATION ${DEST_DIR})
 endmacro ()
 
 # This should be called AFTER all subdirectories with libraries have been called, and all include directories added.
-macro (katydid_install_config_files)
+macro (pbuilder_install_config_files)
     # Configuration header file
     if (EXISTS ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.hh)
         # Install location for the configuration header
-        katydid_install_headers (${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.hh)
+        pbuilder_install_headers (${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.hh)
     endif (EXISTS ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.hh)
 
     # CMake configuration file
