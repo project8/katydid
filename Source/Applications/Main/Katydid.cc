087e5d877021e14e2dea9176f45748922955adcd
Noah Oblath <nsoblath@mit.edu>
2013-04-08 15:50:29 -0400

    Added some printing to Katydid.cc.

--- Source/Applications/Main/Katydid.cc
+++ Source/Applications/Main/Katydid.cc
@@ -23,6 +23,36 @@ KTLOGGER(katydidlog, "katydid.applications.main");
 
 int main(int argc, char** argv)
 {
+    KTPROG(katydidlog, "Welcome to Katydid!");
+    KTDEBUG(katydidlog,
+            "\n" <<
+            "                                                                            ,=  \n" <<
+            "                                                                           =    \n" <<
+            "                                                                          7     \n" <<
+            "                                       .:.                               I      \n" <<
+            "                                          I?                           :~       \n" <<
+            "       +$                                   ~+                        I,        \n" <<
+            "        $Z        .,=IZ8NNND$INM,             :7.                   ,I          \n" <<
+            "         7OONDO7+~  ,=7$Z88$Z?=,?N               $                 :=           \n" <<
+            "             .,?NN?             ~IZMDO$?~         .7              =,            \n" <<
+            "          :ZM7,                       ~7, +7Z$$7$Z8NI            $              \n" <<
+            "        MN.                              Z=          D?,       .D               \n" <<
+            "          +M                                787,     .Z?:   . 7$                \n" <<
+            "            ,M$                                 ?Z    :D$   ,Z                  \n" <<
+            "               ,M=                                 +7.:7OO?$                    \n" <<
+            "                  7O,                                IZ78O: +M:                 \n" <<
+            "                     +O8D8~                        .,   M     ?M:               \n" <<
+            "                   ,M7     =:MNO,                 :DIDID:        M8M$           \n" <<
+            "                .+M:             ,:IIZZZ7==  ,?OMI +O                           \n" <<
+            "               7N                         IMO~      ZD.                         \n" <<
+            "            =M=                          M,          ~M=                        \n" <<
+            "          7D                            M             .IZ                       \n" <<
+            "        8$                            .Z:                M                      \n" <<
+            "    .ZN=                             .8I                . M=N                   \n" <<
+            "MDNNZ                                D=                                         \n" <<
+            "                                    $=                                          \n" <<
+            "                                    =                                           \n");
+
     KTApplication app(argc, argv);
     app.ReadConfigFile();
 
@@ -45,6 +75,8 @@ int main(int argc, char** argv)
 
     // Execute the run queue!
     Bool_t success = procTB.Run();
+
+    KTPROG(katydidlog, "That's all, folks!");
 
     if (! success) return -3;
     return 0;
