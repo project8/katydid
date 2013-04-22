06383d82ab6bcdc2d7ab473d4f0ec309cece7e6d
Noah Oblath <nsoblath@mit.edu>
2013-04-22 18:42:06 -0400

    Progress on KTCompareCandidates: - added a slot for receiving mc event data and candidate data - Added the function to compare the mc event data and truth data Does not compile Overlap calculation not yet implemented.

--- Source/Evaluation/KTCompareCandidates.cc
+++ Source/Evaluation/KTCompareCandidates.cc
@@ -7,17 +7,25 @@
 
 #include "KTCompareCandidates.hh"
 
+#include "KTAnalysisCandidates.hh"
 #include "KTFactory.hh"
+#include "KTMCTruthEvents.hh"
 #include "KTPStoreNode.hh"
 
+#include <map>
+#include <vector>
+
+using std::map;
 using std::string;
+using std::vector;
 
 namespace Katydid
 {
     static KTDerivedRegistrar< KTProcessor, KTCompareCandidates > sCompCandRegistrar("compare-candidates");
 
     KTCompareCandidates::KTCompareCandidates(const string& name) :
-            KTProcessor(name)
+            KTProcessor(name),
+            fTruthAndAnalysisSlot("truth-and-analysis", this, &KTCompareCandidates::CompareTruthAndAnalysis)
     {
     }
 
@@ -29,5 +37,45 @@ namespace Katydid
     {
         return true;
     }
+
+    Bool_t KTCompareCandidates::CompareTruthAndAnalysis(const KTMCTruthEvents& mcEventData, const KTAnalysisCandidates& candidateData)
+    {
+        const KTMCTruthEvents::EventSet& events = mcEventData.GetEvents();
+        const KTAnalysisCandidates::CandidateSet& candidates = candidateData.GetCandidates();
+
+        vector< UInt_t > eventMatches(events.size());
+        map< KTAnalysisCandidates::CandidateSet::const_iterator, UInt_t > candidateMatches(candidates.size());
+
+        KTAnalysisCandidates::CandidateSet::const_iterator candStartHere = candidates.begin();
+
+        UInt_t eventCounter = 0;
+        for (KTMCTruthEvents::EventSet::const_iterator truthIt = events.begin(); truthIt != events.end(); truthIt++)
+        {
+            UInt_t candidateCounter = 0;
+            for (KTAnalysisCandidates::CandidateSet::const_iterator candIt = candStartHere; candIt != candidates.end(); candidates++)
+            {
+                Int_t comparison = CompareAnEventToACandidate(*truthIt, *candIt);
+                if (comparison == 0)
+                {
+                    eventMatches[eventCounter] = eventMatches[eventCounter] + 1;
+                    candidateMatches[candIt] = candidateMatches[candIt] + 1;
+                }
+                else if (comparison < 0)
+                {
+                    candStartHere = candIt;
+                }
+                candidateCounter++;
+            }
+            eventCounter++;
+        }
+
+        return true;
+    }
+
+    Int_t KTCompareCandidates::CompareAnEventToACandidate(const KTMCTruthEvents::Event& event, const KTAnalysisCandidates::Candidate& candidate)
+    {
+        return 0;
+    }
+
 
 } /* namespace Katydid */
--- Source/Evaluation/KTCompareCandidates.hh
+++ Source/Evaluation/KTCompareCandidates.hh
@@ -10,8 +10,12 @@
 
 #include "KTProcessor.hh"
 
+#include "KTSlot.hh"
+
 namespace Katydid
 {
+    class KTAnalysisCandidates;
+    class KTMCTruthEvents;
     class KTPStoreNode;
 
     class KTCompareCandidates : public KTProcessor
@@ -22,6 +26,21 @@ namespace Katydid
 
             Bool_t Configure(const KTPStoreNode* node);
 
+        public:
+            Bool_t CompareTruthAndAnalysis(const KTMCTruthEvents& mcEventData, const KTAnalysisCandidates& candidateData);
+
+        private:
+            // Return values:
+            //   -1 if candidate occurs completely before event
+            //    0 if candidate and event overlap
+            //    1 if candidate occurs completely after event
+            Int_t CompareAnEventToACandidate(const KTMCTruthEvents::Event& event, const KTAnalysisCandidates::Candidate& candidate);
+
+            //***************
+            // Slots
+            //***************
+        private:
+            KTSlotDataTwoTypes< KTMCTruthEvents, KTAnalysisCandidates > fTruthAndAnalysisSlot;
 
     };
 
