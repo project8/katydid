a6783f4200c412c425c4b3c151eb5b27c5e08b9d
Noah Oblath <nsoblath@mit.edu>
2013-05-30 17:37:37 -0400

    Added KTJSONTypeWriterEvaluation.

---
+++ Source/Output/KTJSONTypeWriterEvaluation.hh
@@ -0,0 +1,33 @@
+/*
+ * KTJSONTypeWriterEvaluation.hh
+ *
+ *  Created on: May 30, 2013
+ *      Author: nsoblath
+ */
+
+#ifndef KTJSONTYPEWRITEREVALUATION_HH_
+#define KTJSONTYPEWRITEREVALUATION_HH_
+
+#include "KTJSONWriter.hh"
+
+#include <boost/shared_ptr.hpp>
+
+namespace Katydid
+{
+    class KTData;
+
+    class KTJSONTypeWriterEvaluation : public KTJSONTypeWriter
+    {
+        public:
+            KTJSONTypeWriterEvaluation();
+            virtual ~KTJSONTypeWriterEvaluation();
+
+            void RegisterSlots();
+
+        public:
+            void WriteCCResults(boost::shared_ptr< KTData > data);
+
+    };
+
+} /* namespace Katydid */
+#endif /* KTJSONTYPEWRITEREVALUATION_HH_ */
