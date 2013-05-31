b359aacf1a8553530e830473751bd05bd5a15296
Noah Oblath <nsoblath@mit.edu>
2013-05-31 16:21:11 -0400

    Added KTMultiFileJSONReader

---
+++ Source/Input/KTMultiFileJSONReader.cc
@@ -0,0 +1,191 @@
+/*
+ * KTMultiFileJSONReader.cc
+ *
+ *  Created on: May 31, 2013
+ *      Author: nsoblath
+ */
+
+#include "KTMultiFileJSONReader.hh"
+
+#include "KTAnalysisCandidates.hh"
+#include "KTNOFactory.hh"
+#include "KTLogger.hh"
+#include "KTMCTruthEvents.hh"
+#include "KTPStoreNode.hh"
+
+#include "filestream.h"
+
+using boost::shared_ptr;
+
+using std::deque;
+using std::string;
+
+namespace Katydid
+{
+    KTLOGGER(inlog, "katydid.input");
+
+    static KTDerivedNORegistrar< KTReader, KTMultiFileJSONReader > sMFJSONReaderRegistrar("multifile-json-reader");
+    static KTDerivedNORegistrar< KTProcessor, KTMultiFileJSONReader > sMFJSONRProcRegistrar("multifile-json-reader");
+
+    KTMultiFileJSONReader::KTMultiFileJSONReader(const std::string& name) :
+            KTReader(name),
+            fFilenames(),
+            fFileMode("r"),
+            fDataTypes(),
+            fCCResultsSignal("cc-results", this),
+            fAppendCCResultsSlot("append-cc-results", this, &KTMultiFileJSONReader::AppendCCResults, &fCCResultsSignal)
+    {
+    }
+
+    KTMultiFileJSONReader::~KTMultiFileJSONReader()
+    {
+    }
+
+    Bool_t KTMultiFileJSONReader::Configure(const KTPStoreNode* node)
+    {
+        // Config-file settings
+        if (node == NULL) return false;
+
+        KTPStoreNode::csi_pair itPair = node->EqualRange("input-file");
+        for (KTPStoreNode::const_sorted_iterator it = itPair.first; it != itPair.second; it++)
+        {
+            AddFilename(it->second.get_value<string>());
+            KTDEBUG(inlog, "Added filename <" << fFilenames.back() << ">");
+        }
+
+        SetFileMode(node->GetData<string>("file-mode", fFileMode));
+
+        KTPStoreNode::csi_pair itPair = node->EqualRange("file-type");
+        for (KTPStoreNode::const_sorted_iterator it = itPair.first; it != itPair.second; it++)
+        {
+            AddDataType(it->second.get_value<string>());
+            KTDEBUG(inlog, "Added filename <" << fFilenames.back() << ">");
+        }
+
+        return true;
+    }
+
+    Bool_t KTMultiFileJSONReader::AddDataType(const std::string& type)
+    {
+        if (type == "cc-results")
+        {
+            fDataTypes.push_back(DataType(type, &KTMultiFileJSONReader::AppendCCResults, &fCCResultsSignal));
+        }
+        /*
+        else if (type == "...")
+        {
+            fDataTypes.push_back(DataType(type, &KTMultiFileJSONReader::Append..., &f...Signal));
+        }
+        */
+        else
+        {
+            KTERROR(inlog, "Invalid run-data-type: " << type);
+            return false;
+        }
+
+        return true;
+    }
+
+    Bool_t KTMultiFileJSONReader::OpenAndParseFile(const string& filename, rapidjson::Document& document) const
+    {
+        FILE* file = fopen(filename.c_str(), fFileMode.c_str());
+        if (file == NULL)
+        {
+            KTERROR(inlog, "File did not open\n" <<
+                    "\tFilename: " << filename <<
+                    "\tMode: " << fFileMode);
+            return false;
+        }
+
+        rapidjson::FileStream fileStream(file);
+
+        if (document.ParseStream<0>(fileStream).HasParseError())
+        {
+            KTERROR(inlog, "Unable to parse file <" << filename << ">\n" <<
+                    "\tReason: " << document.GetParseError() << '\n' <<
+                    "\tLocation: character (sorry!) " << document.GetErrorOffset());
+            fclose(file);
+            return false;
+        }
+
+        fclose(file);
+
+        KTDEBUG(inlog, "Input file open and parsed: <" << filename << ">");
+
+        return true;
+    }
+
+    inline Bool_t KTMultiFileJSONReader::Run()
+    {
+        for (deque< string >::const_iterator fileIt = fFilenames.begin(); fileIt != fFilenames.end(); fileIt++)
+        {
+            rapidjson::Document document;
+            if (! OpenAndParseFile(*fileIt, document))
+            {
+                KTERROR(inlog, "A problem occurred while parsing file <" << *fileIt << ">");
+                return false;
+            }
+
+            shared_ptr< KTData > newData(new KTData());
+            for (deque< DataType >::const_iterator dtIt = fDataTypes.begin(); dtIt != fDataTypes.end(); dtIt++)
+            {
+                if (! (this->*(dtIt->fAppendFcn))(*(newData.get())))
+                {
+                    KTERROR(inlog, "Something went wrong while appending data of type <" << dtIt->fName << "> from <" << *fileIt << ">");
+                }
+                (*(dtIt->fSignal))(newData);
+            }
+        }
+        return true;
+    }
+
+    Bool_t KTMultiFileJSONReader::AppendCCResults(rapidjson::Document& document, KTData& appendToData)
+    {
+        const rapidjson::Value& ccResults = document["cc-results"];
+        if (! ccResults.IsObject())
+        {
+            KTERROR(inlog, "\"cc-results\" value is missing or is not an object");
+            return false;
+        }
+
+        KTCCResults& ccResultsData = appendToData.Of< KTCCResults >();
+        ccResultsData.SetNEvents(ccResults["n-events"].GetUint());
+        ccResultsData.SetNCandidates(ccResults["n-candidates"].GetUint());
+
+        const rapidjson::Value& newxcm = ccResults["n-events-with-x-cand-matches"];
+        if (! newxcm.IsArray())
+        {
+            KTERROR(inlog, "\"n-events-with-x-cand-matches\" value is either missing or is not an array");
+            return false;
+        }
+
+        UInt_t index = 0;
+        for (rapidjson::Value::ConstValueIterator newxcmIt = newxcm.Begin(); newxcmIt != newxcm.End(); newxcmIt++)
+        {
+            UInt_t thisNEvents = (*newxcmIt).GetUint();
+            ccResultsData.SetNEventsWithXCandidateMatches(index, (*newxcmIt).GetUint());
+            index++;
+        }
+
+        const rapidjson::Value& ncwxem = ccResults["n-cands-with-x-event-matches"];
+        if (! ncwxem.IsArray())
+        {
+            KTERROR(inlog, "\"n-events-with-x-cand-matches\" value is either missing or is not an array");
+            return false;
+        }
+
+        index = 0;
+        for (rapidjson::Value::ConstValueIterator ncwxemIt = ncwxem.Begin(); ncwxemIt != ncwxem.End(); ncwxemIt++)
+        {
+            UInt_t thisNEvents = (*ncwxemIt).GetUint();
+            ccResultsData.SetNEventsWithXCandidateMatches(index, (*ncwxemIt).GetUint());
+            index++;
+        }
+
+        ccResultsData.SetEfficiency(ccResults["efficiency"].GetDouble());
+        ccResultsData.SetFalseRate(ccResults["false-rate"].GetDouble());
+
+        return true;
+    }
+
+} /* namespace Katydid */
---
+++ Source/Input/KTMultiFileJSONReader.hh
@@ -0,0 +1,148 @@
+/**
+ @file KTMultiFileJSONReader.hh
+ @brief Contains KTMultiFileJSONReader
+ @details 
+ @author: N. S. Oblath
+ @date: May 31, 2013
+*/
+
+#ifndef KTMULTIFILEJSONREADER_HH_
+#define KTMULTIFILEJSONREADER_HH_
+
+#include "KTReader.hh"
+
+#include "KTSlot.hh"
+
+#include <boost/shared_ptr.hpp>
+
+#include "document.h"
+
+#include <cstdio>
+#include <deque>
+#include <string>
+
+namespace Katydid
+{
+    class KTData;
+
+    /*!
+     @class KTMultiFileJSONReader
+     @author N. S. Oblath
+
+     @brief JSON file reader for reading multiple files, and emitting a signal for each
+
+     @details
+     Multiple data-types can be read from each file by specifying multiple run-data-types. This only works if the processor is being used as a primary processor.
+
+     Configuration name: "multifile-json-reader"
+
+     Available configuration values:
+     - "input-file": string -- input filename (may be repeated)
+     - "file-mode": string -- cstdio FILE mode: r (default), a, r+, a+
+     - "run-data-type": string -- the type of file being read (may be repeated). This option is only necessary if the processor is being used as a primary processor.  See options below.
+
+     The run-data-type option determines the function used to read the file.
+     The available options are:
+     - "cc-results" -- Emits signal "cc-results" after a file is read
+
+     Slots:
+     - "append-cc-results": void (shared_ptr<KTData>) -- Add cc results data; Requires KTData; Adds KTCCResults; Emits signal "cc-results" upon successful file read.
+
+     Signals:
+     - "cc-results": void (shared_ptr<KTData>) -- Emitted after reading an mc-truth-events file; Guarantees KTMCTruthEvents.
+    */
+
+
+    class KTMultiFileJSONReader : public KTReader
+    {
+        public:
+            KTMultiFileJSONReader(const std::string& name = "oneshot-json-reader");
+            virtual ~KTMultiFileJSONReader();
+
+            Bool_t Configure(const KTPStoreNode* node);
+
+            const std::deque< std::string >& GetFilenames() const;
+            void AddFilename(const std::string& filename);
+
+            const std::string& GetFileMode() const;
+            void SetFileMode(const std::string& mode);
+
+            const std::deque< std::string >& GetDataTypes() const;
+            Bool_t AddDataType(const std::string& type);
+
+        private:
+            std::deque< std::string > fFilenames;
+            std::string fFileMode;
+
+
+        public:
+            virtual Bool_t Run();
+
+        private:
+            typedef Bool_t (KTMultiFileJSONReader::*AppendFcn)(KTData&);
+            struct DataType
+            {
+                    std::string fName;
+                    AppendFcn fAppendFcn;
+                    KTSignalData* fSignal;
+                    DataType(const std::string& name, AppendFcn fcn, KTSignalData* signal)
+                    {
+                        fName = name;
+                        fAppendFcn = fcn;
+                        fSignal = signal;
+                    }
+            };
+            std::deque< DataType > fDataTypes;
+
+            Bool_t OpenAndParseFile(const std::string& filename, rapidjson::Document& document) const;
+
+        public:
+            Bool_t AppendCCResults(rapidjson::Document& document, KTData& data);
+
+
+            //**************
+            // Signals
+            //**************
+        private:
+            KTSignalData fCCResultsSignal;
+
+            //**************
+            // Slots
+            //**************
+        private:
+            KTSlotDataOneType< KTData > fAppendCCResultsSlot;
+    };
+
+    inline const std::deque< std::string >& KTMultiFileJSONReader::GetFilenames() const
+    {
+        return fFilenames;
+    }
+
+    inline void KTMultiFileJSONReader::AddFilename(const std::string& filename)
+    {
+        fFilenames.push_back(filename);
+        return;
+    }
+
+    inline const std::string& KTMultiFileJSONReader::GetFileMode() const
+    {
+        return fFileMode;
+    }
+
+    inline void KTMultiFileJSONReader::SetFileMode(const std::string& mode)
+    {
+        if (mode == "w" || mode == "a" || mode == "r+" || mode == "w+" || mode == "a+")
+        {
+            fFileMode = mode;
+        }
+        return;
+    }
+
+    inline const std::deque< std::string >& KTMultiFileJSONReader::GetDataTypes() const
+    {
+        return fDataTypes;
+    }
+
+
+} /* namespace Katydid */
+#endif /* KTMULTIFILEJSONREADER_HH_ */
