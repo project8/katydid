563b6a3c4cc014387a1ccb4d4ce422c4b369f890
Ben LaRoque <benjamin.laroque@gmail.com>
2014-01-23 15:20:22 -0800

    linux compilability

---
+++ Source/Data/Egg/KTRawTimeSeries.hh
@@ -0,0 +1,31 @@
+/*
+ * KTRawTimeSeries.hh
+ *
+ *  Created on: Dec 23, 2013
+ *      Author: nsoblath
+ */
+
+#ifndef KTRAWTIMESERIES_HH_
+#define KTRAWTIMESERIES_HH_
+
+#include "KTPhysicalArray.hh"
+
+#include <stdint.h>
+
+namespace Katydid
+{
+
+    class KTRawTimeSeries :public KTPhysicalArray< 1, uint16_t >
+    {
+        public:
+            KTRawTimeSeries();
+            KTRawTimeSeries(size_t nBins, double rangeMin, double rangeMax);
+            KTRawTimeSeries(const KTRawTimeSeries& orig);
+            virtual ~KTRawTimeSeries();
+
+            KTRawTimeSeries& operator=(const KTRawTimeSeries& rhs);
+
+    };
+
+} /* namespace Katydid */
+#endif /* KTRAWTIMESERIES_HH_ */
