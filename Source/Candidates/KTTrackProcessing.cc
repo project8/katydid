31d99fddf74d6b1f7e0c097cd43a0dd67825db18
Noah Oblath <nsoblath@mit.edu>
2014-07-22 18:34:59 -0400

    Added initial skeleton version of KTTrackProcessing.

---
+++ Source/Candidates/KTTrackProcessing.cc
@@ -0,0 +1,49 @@
+/*
+ * KTTrackProcessing.cc
+ *
+ *  Created on: July 22, 2013
+ *      Author: N.S. Oblath & B. LaRoque
+ */
+
+#include "KTTrackProcessing.hh"
+
+#include "KTHoughData.hh"
+#include "KTLogger.hh"
+#include "KTNOFactory.hh"
+#include "KTParam.hh"
+#include "KTSparseWaterfallCandidateData.hh"
+
+using boost::shared_ptr;
+
+namespace Katydid
+{
+    KTLOGGER(tlog, "katydid.fft");
+
+    // Register the processor
+    KT_REGISTER_PROCESSOR(KTTrackProcessing, "track-proc");
+
+    KTTrackProcessing::KTTrackProcessing(const std::string& name) :
+            KTProcessor(name),
+            fTrackSignal("track", this),
+            fSWFAndHoughSlot("swf-and-hough", this, &KTTrackProcessing::ProcessTrack, &fTrackSignal)
+    {
+    }
+
+    KTTrackProcessing::~KTTrackProcessing()
+    {
+    }
+
+    bool KTTrackProcessing::Configure(const KTParamNode* node)
+    {
+        if (node == NULL) return false;
+
+        return true;
+    }
+
+    bool KTTrackProcessing::ProcessTrack(KTSparseWaterfallCandidateData& swfData, KTHoughData& htData)
+    {
+        return true;
+    }
+
+
+} /* namespace Katydid */
---
+++ Source/Candidates/KTTrackProcessing.hh
@@ -0,0 +1,76 @@
+/**
+ @file KTTrackProcessing.hh
+ @brief Contains KTTrackProcessing
+ @details Extracts physics-relevant information about tracks
+ @author: N.S. Oblath & B. LaRoque
+ @date: July 22, 2013
+ */
+
+#ifndef KTTRACKPROCESSING_HH_
+#define KTTRACKPROCESSING_HH_
+
+#include "KTProcessor.hh"
+
+#include "KTSlot.hh"
+
+
+
+namespace Katydid
+{
+    class KTHoughData;
+    class KTParamNode;
+    class KTSparseWaterfallCandidateData;
+
+    /*!
+     @class KTTrackProcessing
+     @author N.S. Oblath & B. LaRoque
+
+     @brief Extracts physics-relevant information about tracks
+
+     @details
+
+     Configuration name: "track-proc"
+
+     Available configuration values:
+     - "some-name": [type] -- [what it does]
+
+     Slots:
+     - "swf-and-hough": void (KTDataPr) -- [what it does]; Requires KTSparseWaterfallCandidateData and KTHoughData; Adds [output data type]; Emits signal "[signal-name]"
+
+     Signals:
+     - "track": void (KTDataPtr) -- Emitted when a track has been processed; Guarantees [output data type].
+    */
+
+    class KTTrackProcessing : public KTProcessor
+    {
+        public:
+            KTTrackProcessing(const std::string& name = "track-proc");
+            virtual ~KTTrackProcessing();
+
+            bool Configure(const KTParamNode* node);
+
+        private:
+
+        public:
+            bool ProcessTrack(KTSparseWaterfallCandidateData& swfData, KTHoughData& htData);
+
+        private:
+
+            //***************
+            // Signals
+            //***************
+
+        private:
+            KTSignalData fTrackSignal;
+
+            //***************
+            // Slots
+            //***************
+
+        private:
+            KTSlotDataTwoTypes< KTSparseWaterfallCandidateData, KTHoughData > fSWFAndHoughSlot;
+
+    };
+}
+ /* namespace Katydid */
+#endif /* KTTRACKPROCESSING_HH_ */
