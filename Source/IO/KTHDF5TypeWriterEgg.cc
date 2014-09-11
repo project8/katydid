08efb478c1b8494baad54ea37bd3eae07b89af27
Jared Kofron <jared.kofron@gmail.com>
2014-09-11 16:35:27 -0700

    write the data to a temporary buffer and then flush it to the HDF5 file. this is working correctly, although it's far from optimized in any way. responsibility for various aspects needs to be hammered out as well.

--- Source/IO/KTHDF5TypeWriterEgg.cc
+++ Source/IO/KTHDF5TypeWriterEgg.cc
@@ -42,26 +42,46 @@ namespace Katydid
 
     void KTHDF5TypeWriterEgg::RegisterSlots()
     {
-        fWriter->RegisterSlot("header", this, &KTHDF5TypeWriterEgg::WriteEggHeader);
+        fWriter->RegisterSlot("write_header", this, &KTHDF5TypeWriterEgg::WriteEggHeader);
+        fWriter->RegisterSlot("setup_from_header", this, &KTHDF5TypeWriterEgg::ProcessEggHeader);
+        fWriter->RegisterSlot("raw_ts", this, &KTHDF5TypeWriterEgg::WriteRawTimeSeriesData);
         return;
     }
 
 
-    //*********************
+    // *********************
     // Egg Header
-    //*********************
+    // *********************
 
-    void KTHDF5TypeWriterEgg::WriteEggHeader(KTEggHeader* header)
-    {
-        // TODO: clearly this is insufficient
-        // TODO: H5T_VARIABLE length for strings?
-        if (! fWriter->OpenAndVerifyFile()) return;
+    void KTHDF5TypeWriterEgg::ProcessEggHeader(KTEggHeader* header)
+    {   
+        // TODO(kofron): this should probably belong to the writer.
+        // TODO(kofron): storage size should be set here too, not just 8 bit.
+        /*
+            Inform the writer about the slice size so it can set up
+            its buffers and so on.
+        */
+        if (!fWriter->OpenAndVerifyFile()) return;
+        if(header != NULL) {
+            fWriter->SetComponents(header->GetNChannels());
+            fWriter->SetRawSliceSize(header->GetRawSliceSize());
+            fWriter->SetSliceSize(header->GetSliceSize());  
+
+            fWriter->CreateDataspaces();
+            fWriter->AddGroup("/raw_data");
+        }
+    }
+
+    void KTHDF5TypeWriterEgg::WriteEggHeader(KTEggHeader* header) {
+        // TODO(kofron): clearly this is insufficient
+        // TODO(kofron): H5T_VARIABLE length for strings?
+        if (!fWriter->OpenAndVerifyFile()) return;
 
         H5::Group* header_grp = fWriter->AddGroup("/metadata");
-        
+       
         // Write the timestamp
         std::string string_attr = header->GetTimestamp();
-        H5::StrType ts_type(H5::PredType::C_S1, string_attr.length() + 1); 
+        H5::StrType ts_type(H5::PredType::C_S1, string_attr.length() + 1);
         H5::DataSpace attr_space(H5S_SCALAR);
         H5::Attribute attr = header_grp->createAttribute("timestamp",
                                                         ts_type,
@@ -91,7 +111,6 @@ namespace Katydid
         attr.write(H5::PredType::NATIVE_UINT, &value);
 
         // Raw Slice Size, Slice Size, Slice Stride, and RecordSize
-        // TODO: we can use SliceSize here to set up the other datasets
         attr = header_grp->createAttribute("raw_slice_size",
                                             H5::PredType::NATIVE_UINT,
                                             attr_space);
@@ -185,7 +204,15 @@ namespace Katydid
     {
         if (! data) return;
 
+        // A buffer to work in.
+        unsigned buffer[fWriter->GetRawSliceSize()];
+
         uint64_t sliceNumber = data->Of<KTSliceHeader>().GetSliceNumber();
+        std::stringstream ss;
+        ss << "/raw_data/" << "slice_" << sliceNumber;
+        std::string slice_name;
+        ss >> slice_name;        
+        H5::DataSet* dset = fWriter->CreateRawTimeSeriesDataSet(slice_name);
 
         KTRawTimeSeriesData& tsData = data->Of<KTRawTimeSeriesData>();
         unsigned nComponents = tsData.GetNComponents();
@@ -194,10 +221,16 @@ namespace Katydid
 
         for (unsigned iComponent=0; iComponent<nComponents; ++iComponent)
         {
+           
             const KTRawTimeSeries* spectrum = tsData.GetTimeSeries(iComponent);
             if (spectrum != NULL)
             {
-                // write time series here
+                std::cout << spectrum[0](0) << std::endl;
+                for(int i=0; i < fWriter->GetRawSliceSize(); i++) {
+                    // TODO(kofron): wat
+                    buffer[i] = spectrum[0](i);
+                }
+                dset->write(buffer, H5::PredType::NATIVE_UINT);
             }
         }
         return;
