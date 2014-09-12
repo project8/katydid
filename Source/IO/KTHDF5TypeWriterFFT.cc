098524086cf516374a2529049d220f4dd6fe18e0
Jared Kofron <jared.kofron@gmail.com>
2014-09-12 14:38:23 -0700

    first commit of files for KTHDF5TypeWriterFFT

---
+++ Source/IO/KTHDF5TypeWriterFFT.cc
@@ -0,0 +1,116 @@
+/*
+ * KTHDF5TypeWriterFFT.cc
+ *
+ *  Created on: 9/12/2014
+ *      Author: J.N. Kofron
+ */
+
+#include "KTHDF5TypeWriterFFT.hh"
+#include "KTTIFactory.hh"
+#include "KTLogger.hh"
+#include "KTFrequencySpectrumPolar.hh"
+#include "KTFrequencySpectrumDataPolar.hh"
+#include "KTFrequencySpectrumDataFFTW.hh"
+#include "KTSliceHeader.hh"
+#include "KTMultiFSDataPolar.hh"
+#include "KTMultiFSDataFFTW.hh"
+#include "KTPowerSpectrum.hh"
+#include "KTPowerSpectrumData.hh"
+#include "KTTimeFrequencyDataPolar.hh"
+#include "KTTimeFrequencyPolar.hh"
+
+namespace Katydid {
+    KTLOGGER(publog, "KTHDF5TypeWriterFFT");
+
+    static KTTIRegistrar<KTHDF5TypeWriter, KTHDF5TypeWriterFFT> sH5TWFFTReg;
+
+    KTHDF5TypeWriterFFT::KTHDF5TypeWriterFFT() :
+        KTHDF5TypeWriter()
+    {}
+
+    KTHDF5TypeWriterFFT::~KTHDF5TypeWriterFFT()
+    {}
+
+    void KTHDF5TypeWriterFFT::RegisterSlots() {
+        fWriter->RegisterSlot("fs-polar", this, &KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataPolar);
+        fWriter->RegisterSlot("fs-fftw", this, &KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataFFTW);
+        fWriter->RegisterSlot("fs-polar-phase", this, &KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataPolarPhase);
+        fWriter->RegisterSlot("fs-fftw-phase", this, &KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataFFTWPhase);
+        fWriter->RegisterSlot("fs-polar-power", this, &KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataPolarPower);
+        fWriter->RegisterSlot("fs-fftw-power", this, &KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataFFTWPower);
+        fWriter->RegisterSlot("fs-polar-mag-dist", this, &KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataPolarMagnitudeDistribution);
+        fWriter->RegisterSlot("fs-fftw-mag-dist", this, &KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataFFTWMagnitudeDistribution);
+        fWriter->RegisterSlot("fs-polar-power-dist", this, &KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataPolarPowerDistribution);
+        fWriter->RegisterSlot("fs-fftw-power-dist", this, &KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataFFTWPowerDistribution);
+        fWriter->RegisterSlot("ps", this, &KTHDF5TypeWriterFFT::WritePowerSpectrum);
+        fWriter->RegisterSlot("psd", this, &KTHDF5TypeWriterFFT::WritePowerSpectralDensity);
+        fWriter->RegisterSlot("ps-dist", this, &KTHDF5TypeWriterFFT::WritePowerSpectrumDistribution);
+        fWriter->RegisterSlot("psd-dist", this, &KTHDF5TypeWriterFFT::WritePowerSpectralDensityDistribution);
+        fWriter->RegisterSlot("tf-polar", this, &KTHDF5TypeWriterFFT::WriteTimeFrequencyDataPolar);
+        fWriter->RegisterSlot("tf-polar-phase", this, &KTHDF5TypeWriterFFT::WriteTimeFrequencyDataPolarPhase);
+        fWriter->RegisterSlot("tf-polar-power", this, &KTHDF5TypeWriterFFT::WriteTimeFrequencyDataPolarPower);
+        fWriter->RegisterSlot("multi-fs-polar", this, &KTHDF5TypeWriterFFT::WriteMultiFSDataPolar);
+        fWriter->RegisterSlot("multi-fs-fftw", this, &KTHDF5TypeWriterFFT::WriteMultiFSDataFFTW);
+    }
+
+    void KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataPolar(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+  
+    void KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataFFTW(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataPolarPhase(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataFFTWPhase(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataPolarPower(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataFFTWPower(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataPolarMagnitudeDistribution(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataFFTWMagnitudeDistribution(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataPolarPowerDistribution(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteFrequencySpectrumDataFFTWPowerDistribution(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WritePowerSpectrum(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WritePowerSpectralDensity(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WritePowerSpectrumDistribution(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WritePowerSpectralDensityDistribution(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteTimeFrequencyDataPolar(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteTimeFrequencyDataPolarPhase(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteTimeFrequencyDataPolarPower(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteMultiFSDataPolar(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+    void KTHDF5TypeWriterFFT::WriteMultiFSDataFFTW(KTDataPtr data) {
+        KTDEBUG(publog, "NOT IMPLEMENTED");
+    }
+}
+
+
