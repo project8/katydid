340c3a72cc619b0e2636846530e1de95ed88785f
Jared Kofron <jared.kofron@gmail.com>
2014-09-12 11:12:53 -0700

    get rid of all ROOT cruft.  take responsibility for dataspaces and so on

--- Source/IO/KTHDF5TypeWriterEgg.hh
+++ Source/IO/KTHDF5TypeWriterEgg.hh
@@ -2,29 +2,22 @@
  * KTHDF5TypeWriterEgg.hh
  *
  *  Created on: Sept 9, 2014
- *      Author: nsoblath
+ *      Author: J.N. Kofron, N.S. Oblath
  */
 
 #ifndef KTHDF5TYPEWRITEREGG_HH_
 #define KTHDF5TYPEWRITEREGG_HH_
 
 #include "KTHDF5Writer.hh"
-#include "KTROOTTreeWriter.hh"
-
 #include "KTData.hh"
-
-#include "Rtypes.h"
-#include "TString.h"
 
 class TTree;
 
-namespace Katydid
-{
+namespace Katydid {
     class KTEggHeader;
 
 
-    class KTHDF5TypeWriterEgg : public KTHDF5TypeWriter
-    {
+    class KTHDF5TypeWriterEgg : public KTHDF5TypeWriter {
         public:
             KTHDF5TypeWriterEgg();
             virtual ~KTHDF5TypeWriterEgg();
@@ -39,7 +32,16 @@ namespace Katydid
             void WriteTimeSeriesData(KTDataPtr data);
 
         private:
-            // any data structures that need to be kept in the class?
+            H5::DataSpace* raw_time_slice_dspace;
+            H5::DataSpace* time_slice_dspace;
+            unsigned slice_size;
+            unsigned raw_slice_size;
+            unsigned n_components;
+
+            void CreateDataspaces();
+            H5::DataSet* CreateRawTSDSet(const std::string &name);
+            H5::Group* working_group;
+
     };
 
 } /* namespace Katydid */
