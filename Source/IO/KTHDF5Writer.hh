77ff7c36eba0a543285d253fe05d1dfea30cb133
Jared Kofron <jared.kofron@gmail.com>
2014-09-12 11:13:54 -0700

    no longer knows anything about the data.  responsibility is officially transferred.

--- Source/IO/KTHDF5Writer.hh
+++ Source/IO/KTHDF5Writer.hh
@@ -2,40 +2,36 @@
  * KTHDF5Writer.hh
  *
  *  Created on: Sept 5, 2014
- *      Author: J. Kofron, N.S. Oblath
+ *      Author: J.N. Kofron, N.S. Oblath
  */
 
 #ifndef KTHDF5WRITER_HH_
 #define KTHDF5WRITER_HH_
 
 #include <string>
-#include "KTWriter.hh"
-#include "KTEggHeader.hh"
-
-
-#include "KTMemberVariable.hh"
-
 #include <set>
 
+#include "KTWriter.hh"
+#include "KTEggHeader.hh"
+#include "KTMemberVariable.hh"
 #include "H5Cpp.h"
 
-namespace Katydid
-{
+namespace Katydid {
     class KTHDF5Writer;
 
     typedef KTDerivedTypeWriter< KTHDF5Writer > KTHDF5TypeWriter;
 
-    class KTHDF5Writer : public KTWriterWithTypists< KTHDF5Writer >//public KTWriter
-    {
+    class KTHDF5Writer : public KTWriterWithTypists< KTHDF5Writer > {
         public:
-            KTHDF5Writer(const std::string& name = "hdf5-writer");
+            explicit KTHDF5Writer(const std::string& name = "hdf5-writer");
             virtual ~KTHDF5Writer();
 
             bool Configure(const KTParamNode* node);
             void WriteEggHeader(KTEggHeader* header);
 
         public:
-            H5::H5File* OpenFile(const std::string& filename, const std::string& flag);
+            H5::H5File* OpenFile(const std::string& filename, 
+                                 const std::string& flag);
             void CloseFile();
 
             MEMBERVARIABLEREF(std::string, Filename);
@@ -47,41 +43,10 @@ namespace Katydid
              this is a no-op.
             */ 
             H5::Group* AddGroup(const std::string& groupname);
-
-            /*
-             Set component number.
-            */
-            void SetComponents(const unsigned n_channels);
-
-            /*
-             Set slice size and raw slice size.
-            */
-            unsigned GetSliceSize();
-            void SetSliceSize(const unsigned slice_size);
-            unsigned GetRawSliceSize();
-            void SetRawSliceSize(const unsigned raw_slice_size);
-
-            /*
-             Functions relevant to the creation and retrieval of the
-             dataspaces for slices.
-            */
-            void CreateDataspaces();
-            H5::DataSpace* GetRawTimeDataspace();
-            H5::DataSpace* GetTimeDataspace();
-
-            /*
-             Create datasets on demand for time series data.
-            */
-            H5::DataSet* CreateRawTimeSeriesDataSet(const std::string &name);
+         
 
         protected:
             H5::H5File* fFile;
-            unsigned slice_size;
-            unsigned raw_slice_size;
-            unsigned n_channels;
-            H5::DataSpace* raw_time_slice_dspace;
-            H5::DataSpace* time_slice_dspace;
-
     };
 
 
