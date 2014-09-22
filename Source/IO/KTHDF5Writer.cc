333fa09f47bd46f51e90e0438cf00ec3680de93e
Jared Kofron <jared.kofron@gmail.com>
2014-09-22 14:28:18 -0700

    detect and delete leading slash in metadata group names.  drop TODO.

--- Source/IO/KTHDF5Writer.cc
+++ Source/IO/KTHDF5Writer.cc
@@ -108,41 +108,49 @@ namespace Katydid
 
         H5::Group* header_grp = this->AddGroup("/metadata");
        
-        // Write the timestamp
-        std::string string_attr = header->GetTimestamp();
-        H5::StrType ts_type(H5::PredType::C_S1, string_attr.length() + 1);
-        H5::DataSpace attr_space(H5S_SCALAR);
-        H5::Attribute attr = header_grp->createAttribute("timestamp",
-                                                        ts_type,
-                                                        attr_space);
-        attr.write(ts_type, string_attr.c_str());
-
         // Write the header.
-        // TODO: this should go in a separate group called 'header' under
-        //       metadata.
-        this->AddMetadata("description", header->GetDescription());
-        this->AddMetadata("acquisition_mode", header->GetAcquisitionMode());
-        this->AddMetadata("n_channels", header->GetNChannels());        
-        this->AddMetadata("raw_slice_size",header->GetRawSliceSize());
-        this->AddMetadata("slice_size",header->GetSliceSize());
-        this->AddMetadata("slice_stride",header->GetSliceStride());
-        this->AddMetadata("record_size",header->GetRecordSize());
-        this->AddMetadata("run_duration",header->GetRunDuration());
-        this->AddMetadata("acquisition_rate",header->GetAcquisitionRate());
-        this->AddMetadata("run_type",header->GetRunType());
-        this->AddMetadata("format_mode",header->GetFormatMode());
-        this->AddMetadata("data_type_size",header->GetDataTypeSize());
-        this->AddMetadata("bit_depth",header->GetBitDepth());
-        this->AddMetadata("voltage_min",header->GetVoltageMin());
-        this->AddMetadata("voltage_range",header->GetVoltageRange());
+        this->AddMetadata("header/mantis_timestamp",header->GetTimestamp());
+        this->AddMetadata("header/description", header->GetDescription());
+        this->AddMetadata("header/acquisition_mode", header->GetAcquisitionMode());
+        this->AddMetadata("header/n_channels", header->GetNChannels());        
+        this->AddMetadata("header/raw_slice_size",header->GetRawSliceSize());
+        this->AddMetadata("header/slice_size",header->GetSliceSize());
+        this->AddMetadata("header/slice_stride",header->GetSliceStride());
+        this->AddMetadata("header/record_size",header->GetRecordSize());
+        this->AddMetadata("header/run_duration",header->GetRunDuration());
+        this->AddMetadata("header/acquisition_rate",header->GetAcquisitionRate());
+        this->AddMetadata("header/run_type",header->GetRunType());
+        this->AddMetadata("header/format_mode",header->GetFormatMode());
+        this->AddMetadata("header/data_type_size",header->GetDataTypeSize());
+        this->AddMetadata("header/bit_depth",header->GetBitDepth());
+        this->AddMetadata("header/voltage_min",header->GetVoltageMin());
+        this->AddMetadata("header/voltage_range",header->GetVoltageRange());
         return;
     }
 
     void KTHDF5Writer::WriteMetadata(std::string name, H5::DataType type, const void* value) {
+        std::string group_name = "/metadata";
+        std::stringstream group_name_builder;
+        group_name_builder << group_name << "/";
+        H5::Group grp = *(this->AddGroup(group_name));
+        std::string delimiter = "/";
+        size_t pos = 0;
+        std::string token;
+
+        if (name.at(0) == '/') {
+            name.erase(0, 1);
+        }
+        while ((pos = name.find(delimiter)) != std::string::npos) {
+            token = name.substr(0, pos);
+            group_name_builder << token;
+            group_name_builder >> group_name;
+            KTINFO(group_name);
+            grp = *(this->AddGroup(group_name));
+            name.erase(0, pos + delimiter.length());
+        }
         H5::DataSpace dspace(H5S_SCALAR);
-        H5::Group* grp = this->AddGroup("/metadata");
         H5::DSetCreatPropList plist;
-        H5::DataSet* dset = new H5::DataSet(grp->createDataSet(name.c_str(),
+        H5::DataSet* dset = new H5::DataSet(grp.createDataSet(name.c_str(),
                                                                type,
                                                                dspace,
                                                                plist));
