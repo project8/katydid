daf2cb9227e8bad6a0a5afcc9c3a583bcb5a2efb
Luiz de Viveiros <ldeviveiros@gmail.com>
2015-01-14 17:04:55 -0800

    Fixed KTCreateKDTree to reset tree after a record
    KTCreateKDTree ClearTree wasn’t called properly to clear out the tree
    after a record, because the previous command (MakeTree) was setting the
    variable fHaveNewData to false; fHaveNewData should remain unchanged
    until after ClearTree is called.

--- Source/SpectrumAnalysis/KTCreateKDTree.cc
+++ Source/SpectrumAnalysis/KTCreateKDTree.cc
@@ -87,9 +87,13 @@ namespace Katydid
 
     bool KTCreateKDTree::AddPoints(KTSliceHeader& slHeader, KTDiscriminatedPoints1DData& discPoints)
     {
+
+        KTDEBUG(kdlog, "Is this a new acquisition? fHaveNewData=" << fHaveNewData << " and GetIsNewAcquisition=" << slHeader.GetIsNewAcquisition());
         // first check to see if this is a new acquisition; if so, run clustering on the previous acquistion's data
         if (fHaveNewData && slHeader.GetIsNewAcquisition())
         {
+            KTDEBUG(kdlog, "New Acquisition - clear out old points from tree");
+
             if (! MakeTree(false))
             {
                 KTERROR(kdlog, "An error occurred while clustering from the previous acquisition");
@@ -127,7 +131,7 @@ namespace Katydid
                 newPoint.fAmplitude = pIt->second.fOrdinate;
                 fTreeData.AddPoint(newPoint, iComponent);
             }
-            KTDEBUG(kdlog, "Tree data (component " << iComponent << ") now has " << fTreeData.GetSetOfPoints(iComponent).size() << " points");
+            KTDEBUG(kdlog, "Tree data (component " << iComponent << ") now has " << fTreeData.GetSetOfPoints(iComponent).size() << " points (Slice Number: " << newPoint.fSliceNumber << ")");
         }
 
         ++fSliceInWindowCount;
@@ -204,17 +208,23 @@ namespace Katydid
         // yet another exception to the separation of normal function and signals/slots; sorry
         fKDTreeSignal(fDataPtr);
 
-        fHaveNewData = false;
+        // LdV 2015-01-14 - Commented the line below
+        //                - fHaveNewData has to remain unchanged until ClearTree; it shouldn't be changed here
+        //                - This probably wasn't noticed before because we weren't using multi-record files with the digitizer data  
+        //fHaveNewData = false;
         return true;
     }
 
     bool KTCreateKDTree::ClearTree(bool willContinue, uint64_t firstSliceKept)
     {
+        KTDEBUG(kdlog, "ClearTree");
         if (! fHaveNewData) return true;
 
         // firstSliceKept is only used if willContinue == true
         if (willContinue)
         {
+            KTDEBUG(kdlog, "ClearTree(true)");
+
             // clear data up to the
             unsigned nComponents = fTreeData.GetNComponents();
             for (unsigned iComponent = 0; iComponent != nComponents; ++iComponent)
@@ -232,6 +242,7 @@ namespace Katydid
         }
         else
         {
+            KTDEBUG(kdlog, "ClearTree(false)");
             // clear all data from the tree, but leave the memory intact
             unsigned nComponents = fTreeData.GetNComponents();
             for (unsigned iComponent = 0; iComponent != nComponents; ++iComponent)
