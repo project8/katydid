db74abe8b790e22ba7fd7ad422f1cf7e784bab1c
Luiz de Viveiros <ldeviveiros@gmail.com>
2015-02-03 00:38:15 -0800

    Modified HDF5 writer to write Processed Tracks
    Added Processed Tracks to KTHDF5TypeWriterCandidates.

--- Source/IO/KTHDF5TypeWriterCandidates.cc
+++ Source/IO/KTHDF5TypeWriterCandidates.cc
@@ -24,7 +24,9 @@ namespace Katydid {
     KTHDF5TypeWriterCandidates::KTHDF5TypeWriterCandidates() :
         KTHDF5TypeWriter(),
         fMTEDataBuffer(),
-        fFlushIdx(0) {
+        fPTDataBuffer(),
+        fFlushMTEIdx(0),
+        fFlushPTIdx(0) {
             /*
              * First we build the appropriate compound datatype for MTE events
              */
@@ -36,10 +38,18 @@ namespace Katydid {
                     MTEFieldOffsets[f],
                     MTEFieldTypes[f]);
             }
+            this->fPTType = new H5::CompType(PTSize);
+            for (int f = 0; f < 21; f++) {
+                this->fPTType->insertMember(
+                    PTFieldNames[f],
+                    PTFieldOffsets[f],
+                    PTFieldTypes[f]);
+            }
         }
 
     KTHDF5TypeWriterCandidates::~KTHDF5TypeWriterCandidates() {
         if(fMTEType) delete fMTEType;
+        if(fPTType) delete fPTType;
     }
 
     void KTHDF5TypeWriterCandidates::RegisterSlots() {
@@ -47,6 +57,7 @@ namespace Katydid {
         fWriter->RegisterSlot("waterfall-candidates", this, &KTHDF5TypeWriterCandidates::WriteWaterfallCandidate);
         fWriter->RegisterSlot("sparse-waterfall-candidates", this, &KTHDF5TypeWriterCandidates::WriteSparseWaterfallCandidate);
         fWriter->RegisterSlot("processed-track", this, &KTHDF5TypeWriterCandidates::WriteProcessedTrack);
+        fWriter->RegisterSlot("final-write-tracks",this, &KTHDF5TypeWriterCandidates::WritePTBuffer);
         fWriter->RegisterSlot("multi-track-event", this, &KTHDF5TypeWriterCandidates::WriteMultiTrackEvent);
         fWriter->RegisterSlot("final-write-events",this, &KTHDF5TypeWriterCandidates::WriteMTEBuffer);
         return;
@@ -63,6 +74,37 @@ namespace Katydid {
     }
     void KTHDF5TypeWriterCandidates::WriteProcessedTrack(KTDataPtr data) {
         KTDEBUG("NOT IMPLEMENTED");
+        KTDEBUG(publog, "Processing Tracks");
+        KTProcessedTrackData& ptData = data->Of< KTProcessedTrackData >();
+
+        PTData track;
+        track.Component = ptData.GetComponent();
+        track.TrackID = ptData.GetTrackID();
+        track.IsCut = ptData.GetIsCut();
+        track.StartTimeInAcq = ptData.GetStartTimeInAcq();
+        track.StartTimeInRunC = ptData.GetStartTimeInRunC();
+        track.EndTimeInRunC = ptData.GetEndTimeInRunC();
+        track.TimeLength = ptData.GetTimeLength();
+        track.StartFrequency = ptData.GetStartFrequency();
+        track.EndFrequency = ptData.GetEndFrequency();
+        track.FrequencyWidth = ptData.GetFrequencyWidth();
+        track.Slope = ptData.GetSlope();
+        track.Intercept = ptData.GetIntercept();
+        track.TotalPower = ptData.GetTotalPower();
+        track.StartTimeInRunCSigma = ptData.GetStartTimeInRunCSigma();
+        track.EndTimeInRunCSigma = ptData.GetEndTimeInRunCSigma();
+        track.TimeLengthSigma = ptData.GetTimeLengthSigma();
+        track.StartFrequencySigma = ptData.GetStartFrequencySigma();
+        track.EndFrequencySigma = ptData.GetEndFrequencySigma();
+        track.FrequencyWidthSigma = ptData.GetFrequencyWidthSigma();
+        track.SlopeSigma = ptData.GetSlopeSigma();
+        track.InterceptSigma = ptData.GetInterceptSigma();
+        track.TotalPowerSigma = ptData.GetTotalPowerSigma();
+
+        (this->fPTDataBuffer).push_back(track);
+
+        KTDEBUG("Done.");
+        return;
     }
     void KTHDF5TypeWriterCandidates::WriteMultiTrackEvent(KTDataPtr data) {
         KTDEBUG(publog, "Processing MTE");
@@ -112,14 +154,37 @@ namespace Katydid {
         // OK, create the dataset and write it down.
         std::stringstream namestream;
         std::string dsetname;
-        namestream << "candidates_" << this->fFlushIdx;
+        namestream << "candidates_" << this->fFlushMTEIdx;
         namestream >> dsetname;
         H5::DataSet* dset = new H5::DataSet(candidatesGroup->createDataSet(dsetname.c_str(),
                                                                            *(this->fMTEType),
                                                                            dspace));
         dset->write((this->fMTEDataBuffer).data(),*(this->fMTEType));
         this->fMTEDataBuffer.clear();
-        this->fFlushIdx++;
+        this->fFlushMTEIdx++;
+    }
+
+    void KTHDF5TypeWriterCandidates::WritePTBuffer() {
+        KTDEBUG("writing PT buffer.");
+        // Now create the dataspace we need
+        hsize_t* dims = new hsize_t(this->fPTDataBuffer.size());
+        H5::DataSpace dspace(1, dims);
+
+        if( !fWriter->OpenAndVerifyFile() ) return;
+        // Make a group
+        H5::Group* candidatesGroup = fWriter->AddGroup("tracks");
+
+        // OK, create the dataset and write it down.
+        std::stringstream namestream;
+        std::string dsetname;
+        namestream << "tracks_" << this->fFlushPTIdx;
+        namestream >> dsetname;
+        H5::DataSet* dset = new H5::DataSet(candidatesGroup->createDataSet(dsetname.c_str(),
+                                                                           *(this->fPTType),
+                                                                           dspace));
+        dset->write((this->fPTDataBuffer).data(),*(this->fPTType));
+        this->fPTDataBuffer.clear();
+        this->fFlushPTIdx++;
     }
 
 }  //  namespace Katydid
