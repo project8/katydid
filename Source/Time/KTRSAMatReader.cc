976f0f63c03067c2fb7a977bba02c2becfb22f3a
Noah Oblath <nsoblath@mit.edu>
2015-03-27 14:56:48 -0400

    Updated KTRSAMatReader for the KTEggHeader changes (untested)

--- Source/Time/KTRSAMatReader.cc
+++ Source/Time/KTRSAMatReader.cc
@@ -212,11 +212,12 @@ namespace Katydid
         // Write configuration from XML into fHeader variable
         fHeader.SetFilename(filename);
         fHeader.SetNChannels(1);
+        fHeader.SetChannelHeader(new KTChannelHeader());
         curr_node = data_node->first_node("NumberSamples");
-        fHeader.SetRecordSize((size_t) atoi(curr_node->value()));
+        fHeader.GetChannelHeader(0)->SetRecordSize((size_t) atoi(curr_node->value()));
         if (fSliceSize == 0)
         {
-            fSliceSize = fHeader.GetRecordSize();
+            fSliceSize = fHeader.GetChannelHeader(0)->GetRecordSize();
             fStride = fSliceSize;
         }
         //rapidxml::xml_node< >* prod_spec_node = doc.first_node("DataFile")->first_node("DataSetsCollection")->first_node("DataSets")->first_node("ProductSpecific");
@@ -231,13 +232,13 @@ namespace Katydid
         curr_node = data_node->first_node("SamplingFrequency");
         double fAcqBW = atof(curr_node->value());
         fHeader.SetAcquisitionRate(2. * fAcqBW);
-        fHeader.SetRunDuration(timeFromFirstToLastRecord + (double) fHeader.GetRecordSize() / fHeader.GetAcquisitionRate());
+        fHeader.SetRunDuration(timeFromFirstToLastRecord + (double) fHeader.GetChannelHeader(0)->GetRecordSize() / fHeader.GetAcquisitionRate());
         curr_node = data_node->first_node("DateTime");
         fHeader.SetTimestamp(curr_node->value());
         curr_node = data_node->first_node("NumberFormat");
         if (strcmp(curr_node->value(), "Int32") == 0)
         {
-            fHeader.SetDataTypeSize(sizeof(int32_t));
+            fHeader.GetChannelHeader(0)->SetDataTypeSize(sizeof(int32_t));
         }
         // The variables below could not be obtained from the XML configuration:
         //fHeader.SetDescription(monarchHeader->GetDescription());
@@ -253,13 +254,13 @@ namespace Katydid
         delete [] rsaxml_str;
 
         // Get configuration from JSON config file
-        fHeader.SetRawSliceSize(fSliceSize);
-        fHeader.SetSliceSize(fSliceSize);
-        fHeader.SetSliceStride(fStride);
+        fHeader.GetChannelHeader(0)->SetRawSliceSize(fSliceSize);
+        fHeader.GetChannelHeader(0)->SetSliceSize(fSliceSize);
+        fHeader.GetChannelHeader(0)->SetSliceStride(fStride);
 
         // A few last useful variables
 
-        fRecordSize = fHeader.GetRecordSize();
+        fRecordSize = fHeader.GetChannelHeader(0)->GetRecordSize();
         fBinWidth = 1. / fHeader.GetAcquisitionRate();
         fSliceNumber = 0; // Number of Slices saved
         fRecordsRead = 0; // Number of records read from file
@@ -356,7 +357,7 @@ namespace Katydid
         sliceHeader.SetSliceNumber(fSliceNumber);
         sliceHeader.SetStartRecordNumber(fRecordsRead);
         sliceHeader.SetStartSampleNumber(fSamplesRead);
-        sliceHeader.SetRecordSize(fHeader.GetRecordSize());
+        sliceHeader.SetRecordSize(fHeader.GetChannelHeader(0)->GetRecordSize());
         // Slice Header Variables that depend on channel number
         unsigned iChannel = 0;
         sliceHeader.SetAcquisitionID(fRecordsRead, iChannel);
