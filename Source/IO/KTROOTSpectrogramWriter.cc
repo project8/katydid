fbccbcd5170f107903c32a441c4e5d90c1dd92c0
Noah Oblath <nsoblath@mit.edu>
2015-06-18 08:43:11 -0400

    Added KTROOTSpectrogramWriter

---
+++ Source/IO/KTROOTSpectrogramWriter.cc
@@ -0,0 +1,70 @@
+/*
+ * KTROOTSpectrogramWriter.cc
+ *
+ *  Created on: Jun 18, 2015
+ *      Author: nsoblath
+ */
+
+#include "KTROOTSpectrogramWriter.hh"
+
+#include "KTCommandLineOption.hh"
+#include "KTParam.hh"
+
+using std::string;
+
+namespace Katydid
+{
+    KTLOGGER(publog, "KTROOTSpectrogramWriter");
+
+
+    KT_REGISTER_WRITER(KTROOTSpectrogramWriter, "root-spectrogram-writer");
+    KT_REGISTER_PROCESSOR(KTROOTSpectrogramWriter, "root-spectrogram-writer");
+
+    static KTCommandLineOption< string > sBRWFilenameCLO("ROOT Spectrogram Writer", "ROOT spectrogram writer filename", "rsw-file");
+
+    KTROOTSpectrogramWriter::KTROOTSpectrogramWriter(const std::string& name) :
+            KTWriterWithTypists< KTROOTSpectrogramWriter >(name),
+            fFilename("spect_output.root"),
+            fFileFlag("recreate"),
+            fFile(NULL)
+    {
+    }
+
+    KTROOTSpectrogramWriter::~KTROOTSpectrogramWriter()
+    {
+        CloseFile();
+    }
+
+    bool KTROOTSpectrogramWriter::Configure(const KTParamNode* node)
+    {
+        // Config-file settings
+        if (node != NULL)
+        {
+            SetFilename(node->GetValue("output-file", fFilename));
+            SetFileFlag(node->GetValue("file-flag", fFileFlag));
+        }
+
+        // Command-line settings
+        SetFilename(fCLHandler->GetCommandLineValue< string >("rsw-file", fFilename));
+
+        return true;
+    }
+
+    bool KTROOTSpectrogramWriter::OpenAndVerifyFile()
+    {
+        if (fFile == NULL)
+        {
+            fFile = new TFile(fFilename.c_str(), fFileFlag.c_str());
+        }
+        if (! fFile->IsOpen())
+        {
+            delete fFile;
+            fFile = NULL;
+            KTERROR(publog, "Output file <" << fFilename << "> did not open!");
+            return false;
+        }
+        fFile->cd();
+        return true;
+    }
+
+} /* namespace Katydid */
---
+++ Source/IO/KTROOTSpectrogramWriter.hh
@@ -0,0 +1,138 @@
+/**
+ @file KTROOTSpectrogramWriter.hh
+ @brief Contains KTROOTSpectrogramWriter
+ @details Writes 2-D histograms of spectrograms
+ @author: N. S. Oblath
+ @date: June 18, 2015
+ */
+
+#ifndef KTROOTSPECTROGRAMWRITER_HH_
+#define KTROOTSPECTROGRAMWRITER_HH_
+
+#include "KTWriter.hh"
+
+#include "TFile.h"
+
+namespace Katydid
+{
+    class KTROOTSpectrogramWriter;
+
+    typedef KTDerivedTypeWriter< KTROOTSpectrogramWriter > KTROOTSpectrogramTypeWriter;
+
+  /*!
+     @class KTROOTSpectrogramWriter
+     @author N. S. Oblath
+
+     @brief Outputs a spectrogram in the form of a 2D histogram to a ROOT file
+
+     @details 
+
+     Configuration name: "root-spectrogram-writer"
+
+     Available configuration values:
+     - "output-file": string -- output filename
+     - "file-flag": string -- TFile option: CREATE, RECREATE, or UPDATE
+
+     Slots:
+     - "aa":
+     - "aa-dist":
+     - "corr":
+     - "corr-dist":
+     - "corr-ts":
+     - "corr-ts-dist":
+     - "hough":
+     - "gain-var":
+     - "fs-fftw":
+     - "fs-polar":
+     - "fs-fftw-phase":
+     - "fs-polar-phase":
+     - "fs-fftw-power":
+     - "fs-polar-power":
+     - "fs-fftw-power-dist":
+     - "fs-polar-power-dist":
+     - "multi-fs-fftw":
+     - "multi-fs-polar":
+     - "norm-fs-fftw":
+     - "norm-fs-polar":
+     - "ts":
+     - "ts-dist":
+     - "wv":
+     - "wv-2d":
+     
+    */
+
+    class KTROOTSpectrogramWriter : public KTWriterWithTypists< KTROOTSpectrogramWriter >//public KTWriter
+    {
+        public:
+            KTROOTSpectrogramWriter(const std::string& name = "basic-root-writer");
+            virtual ~KTROOTSpectrogramWriter();
+
+            bool Configure(const KTParamNode* node);
+
+        public:
+            TFile* OpenFile(const std::string& filename, const std::string& flag);
+            void CloseFile();
+
+            const std::string& GetFilename() const;
+            void SetFilename(const std::string& filename);
+
+            const std::string& GetFileFlag() const;
+            void SetFileFlag(const std::string& flag);
+
+            TFile* GetFile();
+
+            bool OpenAndVerifyFile();
+
+        protected:
+            std::string fFilename;
+            std::string fFileFlag;
+
+            TFile* fFile;
+
+    };
+
+    inline TFile* KTROOTSpectrogramWriter::OpenFile(const std::string& filename, const std::string& flag)
+    {
+        CloseFile();
+        fFile = new TFile(filename.c_str(), flag.c_str());
+        return fFile;
+    }
+    inline void KTROOTSpectrogramWriter::CloseFile()
+    {
+        if (fFile != NULL)
+        {
+            fFile->Close();
+            delete fFile;
+            fFile = NULL;
+        }
+        return;
+    }
+
+    inline const std::string& KTROOTSpectrogramWriter::GetFilename() const
+    {
+        return fFilename;
+    }
+    inline void KTROOTSpectrogramWriter::SetFilename(const std::string& filename)
+    {
+        fFilename = filename;
+        return;
+    }
+
+    inline const std::string& KTROOTSpectrogramWriter::GetFileFlag() const
+    {
+        return fFileFlag;
+    }
+    inline void KTROOTSpectrogramWriter::SetFileFlag(const std::string& flag)
+    {
+        fFileFlag = flag;
+        return;
+    }
+
+    inline TFile* KTROOTSpectrogramWriter::GetFile()
+    {
+        return fFile;
+    }
+
+
+} /* namespace Katydid */
+#endif /* KTROOTSPECTROGRAMWRITER_HH_ */
