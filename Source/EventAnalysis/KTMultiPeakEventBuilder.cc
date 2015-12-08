b2816b128d108623eafac26c74d9272c3f6e8193
Noah Oblath <nsoblath@mit.edu>
2015-12-08 13:45:37 -0500

    Added KTMultiPeakEventBuilder with partial implementation.

---
+++ Source/EventAnalysis/KTMultiPeakEventBuilder.cc
@@ -0,0 +1,132 @@
+/*
+ * KTMultiPeakEventBuilder.cc
+ *
+ *  Created on: Dec 7, 2015
+ *      Author: N.S. Oblath
+ */
+
+#include "KTMultiPeakEventBuilder.hh"
+
+#include "KTLogger.hh"
+#include "KTMath.hh"
+#include "KTMultiTrackEventData.hh"
+#include "KTNOFactory.hh"
+#include "KTParam.hh"
+#include "KTProcessedTrackData.hh"
+
+#ifndef NDEBUG
+#include <sstream>
+#endif
+
+using std::set;
+using std::vector;
+
+namespace Katydid
+{
+    KTLOGGER(tclog, "KTMultiPeakEventBuilder");
+
+    KT_REGISTER_PROCESSOR(KTMultiPeakEventBuilder, "multi-peak-event-builder");
+
+    KTMultiPeakEventBuilder::KTMultiPeakEventBuilder(const std::string& name) :
+            KTPrimaryProcessor(name),
+            fSidebandTimeTolerance(0.),
+            fJumpTimeTolerance(0.),
+            fJumpFreqTolerance(1.),
+            fTimeBinWidth(1),
+            fFreqBinWidth(1.),
+            fCompTracks(1, vector< KTProcessedTrackData >()),
+            fMPTracks(1, vector< MultiPeakTrackRef >()),
+            fCandidates(),
+            fDataCount(0),
+            fEventSignal("event", this),
+            fEventsDoneSignal("clustering-done", this),
+            fTakeTrackSlot("track", this, &KTMultiPeakEventBuilder::TakeTrack)
+    //        fDoClusterSlot("do-cluster-trigger", this, &KTMultiPeakEventBuilder::Run)
+    {
+        RegisterSlot("do-clustering", this, &KTMultiPeakEventBuilder::DoClusteringSlot);
+    }
+
+    KTMultiPeakEventBuilder::~KTMultiPeakEventBuilder()
+    {
+    }
+
+    bool KTMultiPeakEventBuilder::Configure(const KTParamNode* node)
+    {
+        if (node == NULL) return false;
+
+        SetSidebandTimeTolerance(node->GetValue("sideband-time-tol", GetSidebandTimeTolerance()));
+        SetJumpTimeTolerance(node->GetValue("jump-time-tol", GetJumpTimeTolerance()));
+        SetJumpFreqTolerance(node->GetValue("jump-freq-tol", GetJumpFreqTolerance()));
+
+        return true;
+    }
+
+    bool KTMultiPeakEventBuilder::TakeTrack(KTProcessedTrackData& track)
+    {
+        // ignore the track if it's been cut
+        if (track.GetIsCut()) return true;
+
+        // verify that we have the right number of components
+        if (track.GetComponent() >= fCompTracks.size())
+        {
+            SetNComponents(track.GetComponent() + 1);
+        }
+
+        // copy the full track data
+        fCompTracks[track.GetComponent()].push_back(track);
+
+        KTDEBUG(tclog, "Taking track: (" << track.GetStartTimeInRunC() << ", " << track.GetStartFrequency() << ", " << track.GetEndTimeInRunC() << ", " << track.GetEndFrequency());
+
+        return true;
+    }
+
+    /*
+    bool KTMultiPeakEventBuilder::TakePoint(double time, double frequency *//*, double amplitude*//*, unsigned component)
+    {
+        if (component >= fCompPoints.size())
+        {
+            SetNComponents(component + 1);
+        }
+
+        KTDBScan::Point newPoint(fNDimensions);
+        newPoint(0) = time;
+        newPoint(1) = frequency;
+        fCompPoints[component].push_back(newPoint);
+
+        KTDEBUG(tclog, "Point " << fCompPoints[component].size()-1 << " is now " << fCompPoints[component].back());
+
+        return true;
+    }
+    */
+
+    void KTMultiPeakEventBuilder::DoClusteringSlot()
+    {
+        if (! Run())
+        {
+            KTERROR(tclog, "An error occurred while running the event clustering");
+        }
+        return;
+    }
+
+    bool KTMultiPeakEventBuilder::Run()
+    {
+        return DoClustering();
+    }
+
+    bool KTMultiPeakEventBuilder::DoClustering()
+    {
+
+
+        KTDEBUG(tclog, "Event building complete");
+        fEventsDoneSignal();
+
+        return true;
+    }
+
+    void KTMultiPeakEventBuilder::SetNComponents(unsigned nComps)
+    {
+        fCompTracks.resize(nComps, vector< KTProcessedTrackData >());
+        return;
+    }
+
+} /* namespace Katydid */
---
+++ Source/EventAnalysis/KTMultiPeakEventBuilder.hh
@@ -0,0 +1,180 @@
+/**
+ @file KTMultiPeakEventBuilder.hh
+ @brief Contains KTMultiPeakEventBuilder
+ @details Groups tracks into events
+ @author: N.S. Oblath
+ @date: Dec 7, 2015
+ */
+
+#ifndef KTMULTIPEAKEVENTBUILDER_HH_
+#define KTMULTIPEAKEVENTBUILDER_HH_
+
+#include "KTPrimaryProcessor.hh"
+
+#include "KTDBScan.hh"
+#include "KTDistanceMatrix.hh"
+#include "KTSlot.hh"
+#include "KTData.hh"
+#include "KTMemberVariable.hh"
+
+#include <algorithm>
+#include <set>
+#include <vector>
+
+namespace Nymph
+{
+    class KTParamNode;
+}
+;
+
+namespace Katydid
+{
+    using namespace Nymph;
+    class KTProcessedTrackData;
+
+    // Track distance
+    // Vector format for representing tracks: (tstart, fstart, tend, fend)
+    // Dimension t: for tstart_1 < tstart_2, Dt = max(0, tstart_2 - tend_1)
+    // Dimension f: Df = fstart_2 - fend_1
+    // Dist = sqrt(Dt^2 + Df^2)
+    template < typename VEC_T >
+    class TrackDistance
+    {
+        protected:
+            typedef VEC_T vector_type;
+
+            double GetDistance(const VEC_T v1, const VEC_T v2)
+            {
+                double deltaT, deltaF;
+                if (v1(0) < v2(0))
+                {
+                    deltaT = std::max(0., v2(0) - v1(2));
+                    deltaF = v2(1) - v1(3);
+                }
+                else
+                {
+                    deltaT = std::max(0., v1(0) - v2(2));
+                    deltaF = v1(1) - v2(3);
+                }
+                return sqrt(deltaT * deltaT + deltaF * deltaF);
+            };
+
+    };
+
+
+    /*!
+     @class KTMultiPeakEventBuilder
+     @author N.S. Oblath
+
+     @brief Builds tracks into events; identifies multi-peak tracks (i.e. tracks with sidebands), and then groups into events.
+
+     @details
+     Normalization of the axes:
+     The DBSCAN algorithm expects expects that all of the dimensions that describe a points will have the same scale,
+     such that a single radius parameter can describe a sphere in the parameter space that's used to cluster points together.
+     For track clustering, two radii are specified, one for the time dimension and one for the frequency dimension.
+     For clustering, a scaling factor is calculated for each axis such that the ellipse formed by the two radii is
+     scaled to a unit circle.  Those scaling factors are applied to every point before the data is passed to the
+     DBSCAN algorithm.
+
+     Configuration name: "multi-peak-event-builder"
+
+     Available configuration values:
+     - "radii": double[2] -- array used to describe the distances that will be used to cluster tracks together; [time, frequency]
+     - "min-points": unsigned int -- minimum number of tracks required to have a cluster
+
+     Slots:
+     - "track": void (shared_ptr<KTData>) -- If this is a new acquisition; Adds tracks to the internally-stored set of points; Requires KTSliceHeader and KTDiscriminatedPoints1DData.
+     - "do-clustering": void () -- Triggers clustering algorithm
+
+     Signals:
+     - "event": void (shared_ptr<KTData>) -- Emitted for each cluster found; Guarantees KT???Data.
+     - "clustering-done": void () -- Emitted when track clustering is complete
+    */
+
+    class KTMultiPeakEventBuilder : public KTPrimaryProcessor
+    {
+        public:
+            KTMultiPeakEventBuilder(const std::string& name = "multi-peak-event-builder");
+            virtual ~KTMultiPeakEventBuilder();
+
+            bool Configure(const KTParamNode* node);
+
+            MEMBERVARIABLE(double, SidebandTimeTolerance);
+
+            MEMBERVARIABLE(double, JumpTimeTolerance);
+            MEMBERVARIABLE(double, JumpFreqTolerance);
+
+        public:
+            // Store point information locally
+            bool TakeTrack(KTProcessedTrackData& track);
+            //bool TakeTrack(double startTime, double startFreq, double endTime, double endFreq, unsigned component=0);
+
+            void SetNComponents(unsigned nComps);
+            void SetTimeBinWidth(double bw);
+            void SetFreqBinWidth(double bw);
+
+            bool Run();
+
+            bool DoClustering();
+
+            const std::set< KTDataPtr >& GetCandidates() const;
+            unsigned GetDataCount() const;
+
+        private:
+
+            double fTimeBinWidth;
+            double fFreqBinWidth;
+
+            std::vector< std::vector< KTProcessedTrackData > > fCompTracks; // input tracks
+
+            typedef std::vector< unsigned > MultiPeakTrackRef;
+            std::vector< std::vector< MultiPeakTrackRef > > fMPTracks;
+
+            std::set< KTDataPtr > fCandidates;
+            unsigned fDataCount;
+
+            //***************
+            // Signals
+            //***************
+
+        private:
+            KTSignalData fEventSignal;
+            KTSignalOneArg< void > fEventsDoneSignal;
+
+            //***************
+            // Slots
+            //***************
+
+        private:
+            KTSlotDataOneType< KTProcessedTrackData > fTakeTrackSlot;
+            //KTSlotDataOneType< KTInternalSignalWrapper > fDoClusterSlot;
+
+            void DoClusteringSlot();
+
+    };
+
+    inline void KTMultiPeakEventBuilder::SetTimeBinWidth(double bw)
+    {
+        fTimeBinWidth = bw;
+        return;
+    }
+    inline void KTMultiPeakEventBuilder::SetFreqBinWidth(double bw)
+    {
+        fFreqBinWidth = bw;
+        return;
+    }
+
+    inline const std::set< KTDataPtr >& KTMultiPeakEventBuilder::GetCandidates() const
+    {
+        return fCandidates;
+    }
+    inline unsigned KTMultiPeakEventBuilder::GetDataCount() const
+    {
+        return fDataCount;
+    }
+
+
+}
+ /* namespace Katydid */
+#endif /* KTMULTIPEAKEVENTBUILDER_HH_ */
