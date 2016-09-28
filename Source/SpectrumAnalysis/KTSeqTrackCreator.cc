1dae97f1eaba23d5de6de9c6f6caa918e2e802cc
cclaessens <claessen@uni-mainz.de>
2016-09-28 12:20:37 +0200

    SeqTrackCreator loops over slices of scored spectrum, collects points in vector and loops over them.

--- Source/SpectrumAnalysis/KTSeqTrackCreator.cc
+++ Source/SpectrumAnalysis/KTSeqTrackCreator.cc
@@ -16,6 +16,7 @@
 #include "KTLogger.hh"
 #include "KTParam.hh"
 
+#include <numeric>
 #include <cmath>;
 
 using std::string;
@@ -23,8 +24,10 @@ using std::string;
 
 namespace Katydid
 {
-    KTLOGGER(ctlog, "KTConsensusThresholding");
+	KTLOGGER(sdlog, "KTSeqTrackCreator");
 
+/* KTLOGGER(ctlog, "KTConsensusThresholding");
+*/
     KT_REGISTER_PROCESSOR(KTSeqTrackCreator, "seq-clustering");
 
     KTSeqTrackCreator::KTSeqTrackCreator(const std::string& name) :
@@ -33,8 +36,16 @@ namespace Katydid
             fTimeDistance(2),
             fBinDelta(4),
 			fSigma(-3),
+			fScoreThreshold(10.),
+			fMinBin(0),
+			fMaxBin(1),
+			fMinFrequency(0.),
+			fMaxFrequency(1.),
+			fCalculateMinBin(true),
+			fCalculateMaxBin(true),
             fSeqTrackSignal("lines-out", this),
-			fSeqTrackSlot("disc-in", this, &KTSeqTrackCreator::PointLineAssignment, &fSeqTrackSignal)
+	//		fSeqTrackSlot("disc-in", this, &KTSeqTrackCreator::PointLineAssignment, &fSeqTrackSignal),
+    		fSeqTrackSlot("scores_in", this, &KTSeqTrackCreator::PointLineAssignment, &fSeqTrackSignal)
     {
     }
 
@@ -42,19 +53,45 @@ namespace Katydid
     {
     }
 
-    bool KTSeqTrackCreator::Configure(const KTParamNode* node)
+    bool KTSeqTrackCreator::Configure(const scarab::param_node* node)
     {
         if (node == NULL) return false;
 
-        SetFrequencyRadius(node->GetValue<double>("frequency-radius"));
-        SetTimeDistance(node->GetValue<double>("max-component-distance"));
-        SetBinDelta(node->GetValue<int>("max-bin-distance"));
+        if (node->has("min-frequency"))
+        {
+        	SetMinFrequency(node->get_value< double >("min-frequency"));
+        }
+        if (node->has("max-frequency"))
+        {
+        	SetMaxFrequency(node->get_value< double >("max-frequency"));
+        }
+        if (node->has("min-bin"))
+        {
+            SetMinBin(node->get_value< unsigned >("min-bin"));
+        }
+        if (node->has("max-bin"))
+        {
+            SetMaxBin(node->get_value< unsigned >("max-bin"));
+        }
+        if (node->has("minimum-line-frequency-distance"))
+		{
+        	SetFrequencyRadius(node->get_value<double>("minimum-line-frequency-distance"));
+		}
+        if (node->has("max-time-gap-in-track"))
+        {
+        	SetTimeDistance(node->get_value<double>("max-time-gap-in-track"));
+        }
+        if (node->has("searched-bin-radius"))
+        {
+        	SetBinDelta(node->get_value<int>("searched-bin-radius"));
+        }
 
 
         return true;
     }
 
-    bool KTSeqTrackCreator::PointLineAssignment(KTSliceHeader& slHeader, KTPowerSpectrumData& slice,  KTDiscriminatedPoints1DData& discPoints)
+
+  /*  bool KTSeqTrackCreator::PointLineAssignment(KTSliceHeader& slHeader, KTPowerSpectrumData& slice,  KTDiscriminatedPoints1DData& discPoints)
     {
     	KTDEBUG(kdlog, "Is this a new acquisition? fHaveNewData=" << fHaveNewData << " and GetIsNewAcquisition=" << slHeader.GetIsNewAcquisition());
     	// first check to see if this is a new acquisition; if so, run clustering on the previous acquistion's data
@@ -98,8 +135,97 @@ namespace Katydid
         }
         return true;
     }
+*/
 
-    bool KTSeqTrackCreator::LoopOverDiscriminatedPoints(const KTDiscriminatedPoints1DData::SetOfPoints&  incomingPts, KTPowerSpectrum& slice, double& TimeInAcq, double* new_trimming_limits)
+    bool KTSeqTrackCreator::PointLineAssignment(KTSliceHeader& slHeader, KTScoredSpectrumData& spectrum)
+        {
+
+            unsigned nComponents = slHeader.GetNComponents();
+
+            if (fCalculateMinBin)
+            {
+            	SetMinBin(spectrum.GetSpectrum(0)->FindBin(fMinFrequency));
+            	KTDEBUG(sdlog, "Minimum bin set to " << fMinBin);
+            }
+            if (fCalculateMaxBin)
+            {
+            	SetMaxBin(spectrum.GetSpectrum(0)->FindBin(fMaxFrequency));
+            	KTDEBUG(sdlog, "Maximum bin set to " << fMaxBin);
+            }
+
+
+            for (unsigned iComponent = 0; iComponent < nComponents; ++iComponent)
+            {
+            	std::vector<unsigned> CandidateBins;
+            	KTScoredSpectrum slice=spectrum.GetSpectrum(iComponent);
+            	double new_trimming_limits = 0.0;
+            	double new_TimeInAcq = (slHeader.GetTimeInAcq() + 0.5 * slHeader.GetSliceLength());
+
+            	if (slice == NULL)
+            	{
+            		KTERROR(sdlog, "Frequency spectrum pointer (component " << slice << ") is NULL!");
+            	    return false;
+            	}
+
+            	unsigned nBins = fMaxBin - fMinBin + 1;
+            	//double freqMin = slice->GetBinLowEdge(fMinBin);
+            	//double freqMax = slice->GetBinLowEdge(fMaxBin) + slice->GetBinWidth();
+
+          	    // loop over bins, checking against the threshold
+           	    double threshold, value;
+           	    for (unsigned iBin=fMinBin; iBin<=fMaxBin; ++iBin)
+            	{
+            	 	(new_trimming_limits) += slice(iBin)/nBins;
+            	   	value = (slice)(iBin);
+            	    threshold = fScoreThreshold;
+
+            	    if (value >= threshold)
+           	        {
+           	        	CandidateBins.push_back(iBin);
+           	        }
+            	}
+
+            	this->LoopOverHighPowerPoints(slice, CandidateBins, new_TimeInAcq, new_trimming_limits);
+            }
+            return true;
+        }
+
+    bool KTSeqTrackCreator::LoopOverHighPowerPoints(KTScoredSpectrum& slice, std::vector<unsigned>& Points, double& TimeInAcq, double& new_trimming_limits)
+     {
+    	double freq = 0.0, PointFreq= 0.0, new_score=0.0;
+    	double BinWidth = slice.GetBinWidth();
+
+         for (unsigned iPoint=0; iPoint<Points.size(); iPoint++)
+         {
+        	 PointFreq = BinWidth * ((double)Points(iPoint) + 0.5);
+
+         	if (PointFreq - freq > fFDelta)
+         	{
+         		bool match = false;
+         		this->SearchAreaForBetterPoints(Points(iPoint), slice, PointFreq);
+
+         		freq = PointFreq;
+
+         		for (KTLines::SetOfLines::const_iterator LineIt =fLines.begin(); LineIt != fLines.end(); ++LineIt)
+         			{
+         			if (LineIt.InvestigatePoint((double)Points(iPoint), TimeInAcq, new_trimming_limits))
+         				{
+         				match = true;
+         				break;
+         				}
+         			}
+         		if (!match)
+         		{
+         			KTSeqLine new_Line (nLines, Points(iPoint), TimeInAcq, new_freq, SNR(Points(iPoint)), *new_trimming_limits);
+         			nLines=fLines.GetNLines()+1;
+
+         		 }
+
+         	}
+         }
+     }
+
+ /*   bool KTSeqTrackCreator::LoopOverDiscriminatedPoints(const KTDiscriminatedPoints1DData::SetOfPoints&  incomingPts, KTPowerSpectrum& slice, double& TimeInAcq, double& TrimmingLimits)
     {
     	double freq = 0.0;
 
@@ -129,8 +255,53 @@ namespace Katydid
         	}
         }
     }
+*/
 
-    bool KTSeqTrackCreator::VetoPoint(KTDiscriminatedPoints1DData::SetOfPoints::const_iterator Point, KTPowerSpectrum& slice, double& freq)
+    void KTSeqTrackCreator::SearchAreaForBetterPoints(unsigned& PointBin, KTScoredSpectrum& slice, double& PointFreq)
+    {
+    	double dF = slice.GetBinWidth();
+    	double LocalDelta = fFDelta+dF;
+    	int loop_counter = 0;
+    	double OldFreq = 0.0;
+
+
+    	while(LocalDelta > 2*dF && loop_counter <10)
+    	{
+    		loop_counter++;
+    		OldFreq = PointFreq;
+
+    		if (PointBin > fMinBin && PointBin <fMaxBin)
+    		{
+    			this-> WeightedAverage(slice, PointFreq, PointBin);
+    		}
+
+    		LocalDelta = abs(OldFreq-PointFreq);
+    	}
+    }
+
+    inline void KTSeqTrackCreator::WeightedAverage(KTScoredSpectrum& slice, double& PointFreq, unsigned& PointBin)
+    {
+    	double BinWidth = slice.GetBinWidth();
+
+    	double new_Point = 0.0;
+    	double f_average = 0.0;
+    	double weighted_Bin = slice(PointBin)*PointFreq;
+    	double wsum = 0.0;
+
+    	for (int iBin=-fBinDelta/2; iBin<fBinDelta/2; iBin++)
+    	{
+    		weighted_Bin = double(PointBin+iBin)*slice(PointBin+iBin);
+    		wsum +=slice(PointBin+iBin);
+    		new_Point += weighted_Bin;
+    	}
+    	new_Point = unsigned(new_Point/wsum);
+    	f_average = BinWidth * ((double)new_Point + 0.5);
+
+    	PointBin = unsigned(new_Point);
+    	PointFreq = f_average;
+    }
+
+/*    bool KTSeqTrackCreator::VetoPoint(KTDiscriminatedPoints1DData::SetOfPoints::const_iterator Point, KTPowerSpectrum& slice, double& freq)
     {
     	double new_freq = Point->second.fAbscissa;
     	double new_Amp = Point->second.fOrdinate;
@@ -168,7 +339,7 @@ namespace Katydid
 		}
     }
 
-
+*/
 
 
 
--- Source/SpectrumAnalysis/KTSeqTrackCreator.hh
+++ Source/SpectrumAnalysis/KTSeqTrackCreator.hh
@@ -10,8 +10,6 @@
 #define KTSEQTRACKCREATOR_HH_
 
 #include "KTProcessor.hh"
-
-#include "KTKDTreeData.hh"
 #include "KTMemberVariable.hh"
 #include "KTSlot.hh"
 
@@ -33,7 +31,7 @@ namespace Katydid
             KTSeqTrackCreator(const std::string& name = "seq-clustering");
             virtual ~KTSeqTrackCreator();
 
-            bool Configure(const scarab::param_node* node);
+            bool KTSeqTrackCreator::Configure(const KTParamNode* node);
             void SetFrequencyRadius(double FreqRad);
             void SetTimeDistance(double TimeDistance);
             void SetBinDelta(int BinDelta);
@@ -42,15 +40,41 @@ namespace Katydid
             MEMBERVARIABLE(int, TimeDistance);
             MEMBERVARIABLE(unsigned, Sigma);
             MEMBERVARIABLE(unsigned, BinDelta);
+            MEMBERVARIABLE(unsigned, MinBin);
+            MEMBERVARIABLE(bool, CalculateMinBin);
+            MEMBERVARIABLE(unsigned, MaxBin);
+            MEMBERVARIABLE(bool, CalculateMaxBin);
+            double fMinFrequency;
+            double fMaxFrequency;
+            double fScoreThreshold;
+
 
 
         public:
-            bool Configure(const KTParamNode* node);
+            bool Configure(const scarab::param_node* node);
+            unsigned GetMinBin() const;
+            void SetMinBin(unsigned bin);
+            unsigned GetMaxBin() const;
+            void SetMaxBin(unsigned bin);
+            double GetMinFrequency() const;
+            void SetMinFrequency(double freq);
+            double GetMaxFrequency() const;
+            void SetMaxFrequency(double freq);
+            void SetFrequencyRadius(double FreqRad);
+            void SetTimeDistance(double TimeDistance);
+            void SetBinDelta(int BinDelta);
+
+
+
             bool PointLineAssignment(KTSliceHeader& slHeader, KTPowerSpectrumData& slice,  KTDiscriminatedPoints1DData& discPoints);
+            bool PointLineAssignment(KTSliceHeader& slHeader, KTScoredSpectrumData& spectrum);
+            bool LoopOverHighPowerPoints(KTScoredSpectrum& slice, std::vector<unsigned>& Points, double& TimeInAcq, double& TrimmingLimits);
+            void SearchAreaForBetterPoints(unsigned& PointBin, KTScoredSpectrum& slice, double& PointFreq);
+            void WeightedAverage(KTScoredSpectrum& slice, double& PointFreq, unsigned& PointBin)
 
         private:
-            bool LoopOverDiscriminatedPoints(const KTDiscriminatedPoints1DData::SetOfPoints&  incomingPts, KTPowerSpectrum& slice, double& TimeInAcq, double* new_trimming_limits);
-            bool KTSeqTrackCreator::VetoPoint(KTDiscriminatedPoints1DData::SetOfPoints::const_iterator Point, KTPowerSpectrum& slice, double& freq);
+       //     bool LoopOverDiscriminatedPoints(const KTDiscriminatedPoints1DData::SetOfPoints&  incomingPts, KTPowerSpectrum& slice, double& TimeInAcq, double& TrimmingTimits);
+       //     bool KTSeqTrackCreator::VetoPoint(KTDiscriminatedPoints1DData::SetOfPoints::const_iterator Point, KTPowerSpectrum& slice, double& freq);
 
 
 
@@ -67,7 +91,7 @@ namespace Katydid
             //***************
 
         private:
-            Nymph::KTSlotDataTwoTypes< KTSliceHeader, KTDiscriminatedPoints1DData > fSeqTrackSlot;
+            Nymph::KTSlotDataTwoTypes< KTSliceHeader, KTScoredSpectrumData > fSeqTrackSlot;
 
     };
     inline void KTSeqTrackCreator::SetFrequencyRadius(double FreqRad)
@@ -81,6 +105,29 @@ namespace Katydid
     inline void KTSeqTrackCreator::SetBinDelta(int BinDelta)
     {
        	fBinDelta = BinDelta;
+    }
+    inline unsigned KTSeqTrackCreator::GetMinBin() const
+    {
+        return fMinBin;
+    }
+
+    inline void KTSeqTrackCreator::SetMinBin(unsigned bin)
+    {
+        fMinBin = bin;
+        fCalculateMinBin = false;
+        return;
+    }
+
+    inline unsigned KTSeqTrackCreator::GetMaxBin() const
+    {
+        return fMaxBin;
+    }
+
+    inline void KTSeqTrackCreator::SetMaxBin(unsigned bin)
+    {
+        fMaxBin = bin;
+        fCalculateMaxBin = false;
+        return;
     }
 
 } /* namespace Katydid */
