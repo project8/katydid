08c4f160ad1fd3fac1dd25280653cba522d3e380
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2016-10-10 18:07:28 -0700

    MutliPeakEvent builder

--- Source/EventAnalysis/KTMultiPeakEventBuilder.hh
+++ Source/EventAnalysis/KTMultiPeakEventBuilder.hh
@@ -26,7 +26,7 @@
 
 namespace Katydid
 {
-    
+
     /*!
      @class KTMultiPeakEventBuilder
      @author N.S. Oblath
@@ -34,19 +34,12 @@ namespace Katydid
      @brief Builds tracks into events; identifies multi-peak tracks (i.e. tracks with sidebands), and then groups into events.
 
      @details
-     Normalization of the axes:
-     The DBSCAN algorithm expects expects that all of the dimensions that describe a points will have the same scale,
-     such that a single radius parameter can describe a sphere in the parameter space that's used to cluster points together.
-     For track clustering, two radii are specified, one for the time dimension and one for the frequency dimension.
-     For clustering, a scaling factor is calculated for each axis such that the ellipse formed by the two radii is
-     scaled to a unit circle.  Those scaling factors are applied to every point before the data is passed to the
-     DBSCAN algorithm.
 
      Configuration name: "multi-peak-event-builder"
 
      Available configuration values:
-     - "radii": double[2] -- array used to describe the distances that will be used to cluster tracks together; [time, frequency]
-     - "min-points": unsigned int -- minimum number of tracks required to have a cluster
+     - "sideband-time-tol": 
+     - "jump-time-tol":
 
      Slots:
      - "track": void (shared_ptr<KTData>) -- If this is a new acquisition; Adds tracks to the internally-stored set of points; Requires KTSliceHeader and KTDiscriminatedPoints1DData.
