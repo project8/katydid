580a2c9dd75b06cf5c1e9ca9a4171eff0b983564
Evan Zayas <ezayas@mit.edu>
2016-10-13 12:05:33 -0400

    Added slot for KTProcessedTrackData in KTDataCutter

--- Source/EventAnalysis/KTDataCutter.cc
+++ Source/EventAnalysis/KTDataCutter.cc
@@ -24,8 +24,20 @@ namespace Katydid
             KTProcessor(name),
             fMinSidebandSeparation(-1e12),
             fMaxSidebandSeparation(1e12),
-            fMinMagnetronFreq(0),
+            fMinMagnetronFreq(0.),
             fMaxMagnetronFreq(1e12),
+            fMinTrackID(0),
+            fMaxTrackID(1e12),
+            fCutBogusTracks(false),
+            fIsCut(false),
+            fMinStartTimeInRunC(0.),
+            fMaxStartTimeInRunC(1.),
+            fMinTimeLength(0.),
+            fMaxTimeLength(1.),
+            fMinStartFreq(0.),
+            fMaxStartFreq(1e12),
+            fMinSlope(-1e15),
+            fMaxSlope(1e15),
             fLinearFitSignal("fit-result", this)
     {
         RegisterSlot( "fit-result", this, &KTDataCutter::SlotFunctionFitResult );
@@ -43,6 +55,18 @@ namespace Katydid
         SetMaxSidebandSeparation(node->get_value< double >("max-sideband-separation", fMaxSidebandSeparation));
         SetMinMagnetronFreq(node->get_value< double >("min-magnetron-freq", fMinMagnetronFreq));
         SetMaxMagnetronFreq(node->get_value< double >("max-magnetron-freq", fMaxMagnetronFreq));
+        SetMinTrackID(node->get_value< unsigned >("min-track-id", fMinTrackID));
+        SetMaxTrackID(node->get_value< unsigned >("max-track-id", fMaxTrackID));
+        SetCutBogusTracks(node->get_value< bool >("cut-bogus-tracks", fCutBogusTracks));
+        SetIsCut(node->get_value< bool >("is-cut", fIsCut));
+        SetMinStartTimeInRunC(node->get_value< double >("min-start-time-in-run-c", fMinStartTimeInRunC));
+        SetMaxStartTimeInRunC(node->get_value< double >("max-start-time-in-run-c", fMaxStartTimeInRunC));
+        SetMinTimeLength(node->get_value< double >("min-time-length", fMinTimeLength));
+        SetMaxTimeLength(node->get_value< double >("max-time-length", fMaxTimeLength));
+        SetMinStartFreq(node->get_value< double >("min-start-freq", fMinStartFreq));
+        SetMaxStartFreq(node->get_value< double >("max-start-freq", fMaxStartFreq));
+        SetMinSlope(node->get_value< double >("min-slope", fMinSlope));
+        SetMaxSlope(node->get_value< double >("max-slope", fMaxSlope));
         
         return true;
     }
@@ -63,6 +87,61 @@ namespace Katydid
             return false;
         }
         if( fitData.GetFFT_peak( 0 ) > fMaxMagnetronFreq )
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    bool KTDataCutter::CutProcessedTrack( KTProcessedTrackData& trackData )
+    {
+        if( trackData.GetTrackID() < fMinTrackID )
+        {
+            return false;
+        }
+        if( trackData.GetTrackID() > fMaxTrackID )
+        {
+            return false;
+        }
+
+        if( fCutBogusTracks && trackData.GetIsCut() != fIsCut )
+        {
+            return false;
+        }
+
+        if( trackData.GetStartTimeInRunC() < fMinStartTimeInRunC )
+        {
+            return false;
+        }
+        if( trackData.GetStartTimeInRunC() > fMaxStartTimeInRunC )
+        {
+            return false;
+        }
+
+        if( trackData.GetTimeLength() < fMinTimeLength )
+        {
+            return false;
+        }
+        if( trackData.GetTimeLength() > fMaxTimeLength )
+        {
+            return false;
+        }
+
+        if( trackData.GetStartFrequency() < fMinStartFreq )
+        {
+            return false;
+        }
+        if( trackData.GetStartFrequency() > fMaxStartFreq )
+        {
+            return false;
+        }
+
+        if( trackData.GetSlope() < fMinSlope )
+        {
+            return false;
+        }
+        if( trackData.GetSlope() > fMaxSlope )
         {
             return false;
         }
--- Source/EventAnalysis/KTDataCutter.hh
+++ Source/EventAnalysis/KTDataCutter.hh
@@ -41,12 +41,26 @@ namespace Katydid
      - "max-sideband-separation": double -- maximum sideband separation to accept from KTLinearFitResult
      - "min-magnetron-freq": double -- minimum magnetron frequency to accept from KTLinearFitResult
      - "max-magnetron-freq": double -- maximum magnetron frequency to accept from KTLinearFitResult
+     - "min-track-id": unsigned -- minimum track ID to accept from KTProcessedTrackData
+     - "max-track-id": unsigned -- maximum track ID to accept from KTProcessedTrackData
+     - "cut-bogus-tracks": bool -- perform a cut on IsCut from KTProcessedTrackData
+     - "is-cut": bool -- value of IsCut to accept from KTProcessedTrackData
+     - "min-start-time-in-run-c": double -- minimum start time to accept from KTProcessedTrackData
+     - "max-start-time-in-run-c": double -- maximum start time to accept from KTProcessedTrackData
+     - "min-time-length": double -- minimum time length to accept from KTProcessedTrackData
+     - "max-time-length": double -- maximum time length to accept from KTProcessedTrackData
+     - "min-start-freq": double -- minimum starting frequency to accept from KTProcessedTrackData
+     - "max-start-freq": double -- maximum starting frequency to accept from KTProcessedTrackData
+     - "min-slope": double -- minimum slope to accept from KTProcessedTrackData
+     - "max-slope": double -- maximum slope to accept from KTProcessedTrackData
 
      Slots:
      - "fit-result": void (Nymph::KTDataPtr) -- Cuts on a linear fit result; Requires KTLinearFitResult; Adds nothing
+     - "track": void (Nymph::KTDataPtr) -- Cuts on a processed track; Requires KTProcessedTrackData; Adds nothing
 
      Signals:
      - "fit-result": void (Nymph::KTDataPtr) -- Emitted upon passing of a cut on KTLinearFitResult; Guarantees KTLinearFitResult
+     - "track": void (Nymph::KTDataPtr) -- Emitted upon passing of a cut on KTProcessedTrackData; Guarantees KTProcessedTrackData
     */
 
     class KTDataCutter : public Nymph::KTProcessor
@@ -69,14 +83,63 @@ namespace Katydid
             double GetMaxMagnetronFreq() const;
             void SetMaxMagnetronFreq(double freq);
 
+            unsigned GetMinTrackID() const;
+            void SetMinTrackID(unsigned id);
+
+            unsigned GetMaxTrackID() const;
+            void SetMaxTrackID(unsigned id);
+
+            bool GetCutBogusTracks() const;
+            void SetCutBogusTracks(bool cut);
+
+            bool GetIsCut() const;
+            void SetIsCut(bool cut);
+
+            double GetMinStartTimeInRunC() const;
+            void SetMinStartTimeInRunC(double t);
+
+            double GetMaxStartTimeInRunC() const;
+            void SetMaxStartTimeInRunC(double t);
+
+            double GetMinTimeLength() const;
+            void SetMinTimeLength(double t);
+
+            double GetMaxTimeLength() const;
+            void SetMaxTimeLength(double t);
+
+            double GetMinStartFreq() const;
+            void SetMinStartFreq(double freq);
+
+            double GetMaxStartFreq() const;
+            void SetMaxStartFreq(double freq);
+
+            double GetMinSlope() const;
+            void SetMinSlope(double m);
+
+            double GetMaxSlope() const;
+            void SetMaxSlope(double m);
+
         private:
             double fMinSidebandSeparation;
             double fMaxSidebandSeparation;
             double fMinMagnetronFreq;
             double fMaxMagnetronFreq;
+            unsigned fMinTrackID;
+            unsigned fMaxTrackID;
+            bool fCutBogusTracks;
+            bool fIsCut;
+            double fMinStartTimeInRunC;
+            double fMaxStartTimeInRunC;
+            double fMinTimeLength;
+            double fMaxTimeLength;
+            double fMinStartFreq;
+            double fMaxStartFreq;
+            double fMinSlope;
+            double fMaxSlope;
 
         public:
             bool CutLinearFitResult( KTLinearFitResult& fitData );
+            bool CutProcessedTrack( KTProcessedTrackData& trackData );
 
             //***************
             // Signals
@@ -84,6 +147,7 @@ namespace Katydid
 
         private:
             Nymph::KTSignalData fLinearFitSignal;
+            Nymph::KTSignalData fTrackSignal;
 
             //***************
             // Slots
@@ -91,6 +155,7 @@ namespace Katydid
 
         private:
             void SlotFunctionFitResult( Nymph::KTDataPtr data );
+            void SlotFunctionProcessedTrack( Nymph::KTDataPtr data );
 
     };
 
@@ -138,6 +203,138 @@ namespace Katydid
         return;
     }
 
+    inline unsigned KTDataCutter::GetMinTrackID() const
+    {
+        return fMinTrackID;
+    }
+
+    inline void KTDataCutter::SetMinTrackID(unsigned id)
+    {
+        fMinTrackID = id;
+        return;
+    }
+
+    inline unsigned KTDataCutter::GetMaxTrackID() const
+    {
+        return fMaxTrackID;
+    }
+
+    inline void KTDataCutter::SetMaxTrackID(unsigned id)
+    {
+        fMaxTrackID = id;
+        return;
+    }
+
+    inline bool KTDataCutter::GetCutBogusTracks() const
+    {
+        return fCutBogusTracks;
+    }
+
+    inline void KTDataCutter::SetCutBogusTracks(bool cut)
+    {
+        fCutBogusTracks = cut;
+        return;
+    }
+
+    inline double KTDataCutter::GetIsCut() const
+    {
+        return fIsCut;
+    }
+
+    inline void KTDataCutter::SetIsCut(bool cut)
+    {
+        fIsCut = cut;
+        return;
+    }
+
+    inline double KTDataCutter::GetMinStartTimeInRunC() const
+    {
+        return fMinStartTimeInRunC;
+    }
+
+    inline void KTDataCutter::SetMinStartTimeInRunC(double t)
+    {
+        fMinStartTimeInRunC = t;
+        return;
+    }
+
+    inline double KTDataCutter::GetMaxStartTimeInRunC() const
+    {
+        return fMaxStartTimeInRunC;
+    }
+
+    inline void KTDataCutter::SetMaxStartTimeInRunC(double t)
+    {
+        fMaxStartTimeInRunC = t;
+        return;
+    }
+
+    inline double KTDataCutter::GetMinTimeLength() const
+    {
+        return fMinTimeLength;
+    }
+
+    inline void KTDataCutter::SetMinTimeLength(double t)
+    {
+        fMinTimeLength = t;
+        return;
+    }
+
+    inline double KTDataCutter::GetMaxTimeLength() const
+    {
+        return fMaxTimeLength;
+    }
+
+    inline void KTDataCutter::SetMaxTimeLength(double t)
+    {
+        fMaxTimeLength = t;
+        return;
+    }
+
+    inline double KTDataCutter::GetMinStartFreq() const
+    {
+        return fMinStartFreq;
+    }
+
+    inline void KTDataCutter::SetMinStartFreq(double freq)
+    {
+        fMinStartFreq = freq;
+        return;
+    }
+
+    inline double KTDataCutter::GetMaxStartFreq() const
+    {
+        return fMaxStartFreq;
+    }
+
+    inline void KTDataCutter::SetMaxStartFreq(double freq)
+    {
+        fMaxStartFreq = freq;
+        return;
+    }
+
+    inline double KTDataCutter::GetMinSlope() const
+    {
+        return fMinSlope;
+    }
+
+    inline void KTDataCutter::SetMinSlope(double m)
+    {
+        fMinSlope = m;
+        return;
+    }
+
+    inline double KTDataCutter::GetMaxSlope() const
+    {
+        return fMaxSlope;
+    }
+
+    inline void KTDataCutter::SetMaxSlope(double m)
+    {
+        fMaxSlope = m;
+        return;
+    }
+
     void KTDataCutter::SlotFunctionFitResult( Nymph::KTDataPtr data )
     {
         // Standard data slot pattern:
@@ -157,6 +354,29 @@ namespace Katydid
 
         // Emit signal
         fLinearFitSignal( data );
+    
+        return;
+    }
+
+    void KTDataCutter::SlotFunctionProcessedTrack( Nymph::KTDataPtr data )
+    {
+        // Standard data slot pattern:
+        // Check to ensure that the required data types are present
+        if (! data->Has< KTProcessedTrackData >())
+        {
+            KTERROR(avlog_hh, "Data not found with type < KTProcessedTrackData >!");
+            return;
+        }
+
+        // Call function
+        if( !CutProcessedTrack( data->Of< KTProcessedTrackData >() ) )
+        {
+            KTINFO(avlog_hh, "Processed track failed cut; suppressing signal");
+            return;
+        }
+
+        // Emit signal
+        fTrackSignal( data );
     
         return;
     }
