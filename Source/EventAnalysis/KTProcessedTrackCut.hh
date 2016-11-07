c2a685f07e8e3e1b0dbee466a6b72c1db589969d
Evan Zayas <ezayas@mit.edu>
2016-11-07 14:29:17 -0500

    Minor documentation fixes

--- Source/EventAnalysis/KTProcessedTrackCut.hh
+++ Source/EventAnalysis/KTProcessedTrackCut.hh
@@ -25,25 +25,28 @@ namespace Katydid
      KTProcessedTrackData objects must have fIsCut == false to pass the cut
 
      Configuration name: "track-cut"
+
+     Available configuration values:
+     (none)
     */
 
     class KTProcessedTrackCut : public Nymph::KTCutOneArg< KTProcessedTrackData >
     {
 
-     public:
-         struct Result : Nymph::KTExtensibleCutResult< Result >
-         {
-             static const std::string sName;
-         };
+    public:
+        struct Result : Nymph::KTExtensibleCutResult< Result >
+        {
+            static const std::string sName;
+        };
 
-     public:
-         KTProcessedTrackCut(const std::string& name = "track-cut");
-         ~KTProcessedTrackCut();
+    public:
+        KTProcessedTrackCut(const std::string& name = "track-cut");
+        ~KTProcessedTrackCut();
 
-         bool Configure(const scarab::param_node* node);
+        bool Configure(const scarab::param_node* node);
 
-     public:
-         bool Apply(Nymph::KTData& data, KTProcessedTrackData& trackData);
+    public:
+        bool Apply(Nymph::KTData& data, KTProcessedTrackData& trackData);
 
     };
 } // namespace Katydid
