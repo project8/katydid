b5eafb391d0894db9c5bfa526b46ad8ec77f740d
Evan Zayas <ezayas@mit.edu>
2017-02-07 16:36:01 -0500

    filled out processor body

--- Source/EventAnalysis/KTTrackPairComparer.cc
+++ Source/EventAnalysis/KTTrackPairComparer.cc
@@ -99,7 +99,99 @@ namespace Katydid
 
     bool KTTrackPairComparer::CompareTracks()
     {
-        
+        int trackIndex1 = 0, trackIndex2 = 0; // indices of a track in a loop through the set
+
+        // parameters to calculate
+
+        double deltaF = 0.;
+        double oldPower = 0.;
+        double newPower = 0.;
+        double deltaQ = 0.;
+        double deltaTHead = 0.;
+        double deltaTTail = 0.;
+
+        for( int iComponent = 0; iComponent < fCompTracks.size(); ++iComponent )
+        {   // loop over components
+
+            trackIndex1 = 0;
+            for( TrackSetCIt trackIt = fCompTracks[iComponent].begin(); trackIt != fCompTracks[iComponent].end(); ++trackIt )
+            {
+                trackIndex2 = 0;
+                for( TrackSetCIt secondTrackIt = fCompTracks[iComponent].begin(); secondTrackIt != fCompTracks[iComponent].end(); ++secondTrackIt )
+                {
+                    // now we can do a pair-wise comparison
+
+                    // no need to compare a track with itself
+                    if( trackIndex1 == trackIndex2 )
+                    {
+                        ++trackIndex2;
+                        ++secondTrackIt;
+                        continue;
+                    }
+
+                    // create data object
+                    Nymph::KTDataPtr data( new Nymph::KTData() );
+                    KTMultiTrackComparisonData& newData = data->Of< KTMultiTrackComparisonData >();
+
+                    // fill component and tracks
+                    newData.SetComponent( iComponent );
+                    newData.AddToMPTrack( trackIt );
+                    newData.SetSingleTrack( *secondTrackIt );
+
+                    // calculate frequency difference
+                    deltaF = secondTrackIt->GetStartFrequency() - trackIt->GetStartFrequency();
+
+                    // calculate powers
+                    oldPower = trackIt->GetTotalPower();
+                    newPower = secondTrackIt->GetTotalPower();
+
+                    // calculate slope difference
+                    deltaQ = secondTrackIt->GetSlope() - trackIt->GetSlope();
+
+                    // calculate track start/end differences
+                    deltaTHead = secondTrackIt->GetStartTimeInRunC() - trackIt->GetStartTimeInRunC();
+                    deltaTTail = secondTrackIt->GetEndTimeInRunC() - trackIt->GetEndTimeInRunC();
+
+                    // fill calculations into data object
+
+                    newData.SetMinDeltaFRatio( deltaF );
+                    newData.SetMaxDeltaFRatio( deltaF );
+
+                    newData.SetMeanPowerOld( oldPower );
+                    newData.SetMinPowerOld( oldPower );
+                    newData.SetMaxPowerOld( oldPower );
+
+                    newData.SetPowerNew( newPower );
+
+                    newData.SetMeanDeltaQ( deltaQ );
+                    newData.SetMinDeltaQ( deltaQ );
+                    newData.SetMaxDeltaQ( deltaQ );
+
+                    newData.SetMeanDeltaTHead( deltaTHead );
+                    newData.SetMinDeltaTHead( deltaTHead );
+                    newData.SetMaxDeltaTHead( deltaTHead );
+
+                    newData.SetMeanDeltaTTail( deltaTTail );
+                    newData.SetMinDeltaTTail( deltaTTail );
+                    newData.SetMaxDeltaTTail( deltaTTail );
+
+                    // Emit signal
+                    fMTCompSignal( data );
+
+                    // Increment index
+                    ++trackIndex2;
+
+                } // end loop over secondTrackIt
+
+                // Increment index
+                ++trackIndex1;
+
+            } // end loop over trackIt
+
+        }  // end loop over components
+
+        // Emit done signal
+        fDoneSignal();
 
         return true;
     }
