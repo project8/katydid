98df7803713dffd20a0cd62fe7aeb3dfd6a8a81e
Ben LaRoque <benjamin.laroque@gmail.com>
2017-04-25 13:29:06 -0700

    builds some const references fixed

--- Source/Time/KTEggProcessorPy.hh
+++ Source/Time/KTEggProcessorPy.hh
@@ -11,18 +11,36 @@
 
 #include "KTEggProcessor.hh"
 
+/*
+boost::python::list GetFilenames_wrapper()
+{
+    boost::python::list result;
+    std::vector<boost::filesystem::path, std::allocator<boost::filesystem::path>> v = Katydid::KTEggProcessor::GetFilenames();
+    std::vector<boost::filesystem::path, std::allocator<boost::filesystem::path>>::iterator it;
+    for (it = v.begin(); it != v.end(); ++it){
+        result.append(*it);
+    }
+    return result;
+}
+*/
+
+//typedef std::vector<boost::filesystem::path, std::allocator<boost::filesystem::path> > PyListType;
 
 void export_KTEggProcessorPy()
 {
     using namespace Katydid;
     using namespace boost::python;
+
+    //class_<PyListType>("FilenameList").def(vector_indexing_suite<PyListType>() );
+
     class_< Katydid::KTEggProcessor, boost::noncopyable, bases<Nymph::KTProcessor> >("KTEggProcessor")
         .add_property("NSlices", &KTEggProcessor::GetNSlices, &KTEggProcessor::SetNSlices)
         .add_property("ProgressReportInterval", &KTEggProcessor::GetProgressReportInterval, &KTEggProcessor::SetProgressReportInterval)
 
-        //TODO build error casting the arg/return types
-        //.add_property("Filenames", &KTEggProcessor::GetFilenames, &KTEggProcessor::SetFilenames)
-        //.add_property("EggReaderType", &KTEggProcessor::GetEggReaderType, &KTEggProcessor::SetEggReaderType)
+        //TODO The Filenames type is an std::vector which i don't have a way to deal with yet
+        .add_property("Filenames", make_function( &KTEggProcessor::GetFilenames, return_value_policy<copy_const_reference>()), &KTEggProcessor::SetFilenames)
+
+        .add_property("EggReaderType", make_function( &KTEggProcessor::GetEggReaderType, return_value_policy<copy_const_reference>()), &KTEggProcessor::SetEggReaderType)
 
         .add_property("SliceSize", &KTEggProcessor::GetSliceSize, &KTEggProcessor::SetSliceSize)
         .add_property("Stride", &KTEggProcessor::GetStride, &KTEggProcessor::SetStride)
