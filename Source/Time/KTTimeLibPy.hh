4f84d6c4d8ff3ae35fedc5cdad8af4d5f78ffe95
Ben LaRoque <benjamin.laroque@gmail.com>
2017-04-26 10:12:45 -0700

    only need EggProcessor, not the readers

--- Source/Time/KTTimeLibPy.hh
+++ Source/Time/KTTimeLibPy.hh
@@ -8,16 +8,10 @@
 #ifndef KTTIMELIBPY_HH_
 #define KTTIMELIBPY_HH_
 
-#include "KTEggReaderPy.hh"
-#include "KTEgg3ReaderPy.hh"
-#include "KTRSAMatReaderPy.hh"
 #include "KTEggProcessorPy.hh"
 
 void export_KTTimePy()
 {
-    export_KTEggReaderPy();
-    export_KTEgg3ReaderPy();
-    export_KTRSAMatReaderPy();
     export_KTEggProcessorPy();
 }
 #endif /* KTTIMELIBPY_HH_ */
