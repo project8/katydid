2d4b5e05178e0254049c3e8dbf2387d1ab52c124
Evan Zayas <ezayas@mit.edu>
2017-07-27 12:13:11 -0400

    copied KTQuadraticPhaseShift from wrong branch

---
+++ Source/Transform/KTQuadraticPhaseShift.cc
@@ -0,0 +1,105 @@
+/*
+ * KTQuadraticPhaseShift.cc
+ *
+ *  Created on: Jan 17, 2017
+ *      Author: ezayas
+ */
+
+#include "KTQuadraticPhaseShift.hh"
+
+#include "KTLogger.hh"
+
+#include "KTSliceHeader.hh"
+#include "KTTimeSeriesData.hh"
+#include "KTTimeSeriesFFTW.hh"
+#include "KTProcessedTrackData.hh"
+#include "KTMath.hh"
+
+#include <cmath>
+
+namespace Katydid
+{
+    KTLOGGER(evlog, "KTQuadraticPhaseShift");
+
+    // Register the processor
+    KT_REGISTER_PROCESSOR(KTQuadraticPhaseShift, "quadratic-phase");
+
+    KTQuadraticPhaseShift::KTQuadraticPhaseShift(const std::string& name) :
+            KTProcessor(name),
+            fSlope(0.),
+            fTSSignal("time-series", this)
+    {
+        RegisterSlot( "ts", this, &KTQuadraticPhaseShift::SlotFunctionTimeSeries );
+        RegisterSlot( "track", this, &KTQuadraticPhaseShift::SlotFunctionTrack );
+    }
+
+    KTQuadraticPhaseShift::~KTQuadraticPhaseShift()
+    {
+    }
+
+    bool KTQuadraticPhaseShift::Configure(const scarab::param_node* node)
+    {
+        if (node == NULL) return false;
+
+        SetSlope(node->get_value< double >("slope", fSlope));
+
+        return true;
+    }
+
+    bool KTQuadraticPhaseShift::ProcessTimeSeries( KTTimeSeriesData& tsData, KTSliceHeader& slice )
+    {
+        KTDEBUG(evlog, "Receiving time series for quadratic phase shift");
+
+        KTTimeSeriesFFTW* ts;   // time series from data
+
+        // Slice and TS parameters
+
+        double t = slice.GetTimeInAcq();    // time value of the current bin
+        double dt = slice.GetBinWidth();    // time step
+        double norm = 0.;                   // norm of the current TS value
+        double phase = 0.;                  // argument of current TS value
+        double q = GetSlope();              // q-value to determine phase shift
+
+        KTINFO(evlog, "Set up time series and slice parameters.");
+        KTDEBUG(evlog, "Initial t = " << t);
+        KTDEBUG(evlog, "Time step = " << dt);
+        KTDEBUG(evlog, "Slope = " << q);
+        
+        for( unsigned iComponent = 0; iComponent < tsData.GetNComponents(); ++iComponent )
+        {
+            KTDEBUG(evlog, "Processing component: " << iComponent);
+
+            // get TS from data object and make the new TS
+            ts = dynamic_cast< KTTimeSeriesFFTW* >(tsData.GetTimeSeries( iComponent ));
+            
+            // Loop through all bins
+            for( unsigned iBin = 0; iBin < ts->GetNTimeBins(); ++iBin )
+            {
+                // Obtain norm and phase from components
+                norm = sqrt( (*ts)(iBin)[0] * (*ts)(iBin)[0] + (*ts)(iBin)[1] * (*ts)(iBin)[1] );
+                phase = atan2( (*ts)(iBin)[1], (*ts)(iBin)[0] );
+
+                // Shift phase
+                phase -= q * t * t;
+
+                // Assign components from norm and new phase
+                (*ts)(iBin)[0] = norm * cos( phase );
+                (*ts)(iBin)[1] = norm * sin( phase );
+
+                // Increment time value
+                t += dt;
+            }
+        }
+
+        return true;
+    }
+
+    bool KTQuadraticPhaseShift::AssignPhase( KTProcessedTrackData& trackData )
+    {
+        SetSlope( trackData.GetSlope() * KTMath::Pi() );
+        KTINFO(evlog, "Set q-value: " << GetSlope());
+
+        return true;
+    }
+
+} // namespace Katydid
---
+++ Source/Transform/KTQuadraticPhaseShift.hh
@@ -0,0 +1,147 @@
+/*
+ * KTQuadraticPhaseShift.hh
+ *
+ *  Created on: Jan 17, 2017
+ *      Author: ezayas
+ */
+
+#ifndef KTQUADRATICPHSAESHIFT_HH_
+#define KTQUADRATICPHSAESHIFT_HH_
+
+#include "KTProcessor.hh"
+#include "KTData.hh"
+
+#include "KTSlot.hh"
+#include "KTLogger.hh"
+
+
+namespace Katydid
+{
+    
+    KTLOGGER(avlog_hh, "KTQuadraticPhaseShift.hh");
+
+    class KTSliceHeader;
+    class KTProcessedTrackData;
+    class KTTimeSeriesData;
+
+    /*
+     @class KTQuadraticPhaseShift
+     @author E. Zayas
+
+     @brief Multiplies a time series by a quadratic phase exp( -i q t^2 ) where q is the slope of a track
+
+     @details
+     This processor performs the transformation (t, w) -> (t, w + q t) in time/frequency space by multiplying a quadratic phase
+     by a time series. The value of q can be acquired from a track, or fixed in the config file (the latter should probably only
+     be used for testing).
+
+     Configuration name: "quadratic-phase"
+
+     Available configuration values:
+     - "slope": double -- value of q, in Hz/s
+
+     Slots:
+     - "track": void (Nymph::KTDataPtr) -- Sets the value of q equal to the slope of a track; Requires KTProcessedTrackData; Adds nothing
+     - "ts": void (Nymph::KTDataPtr) -- Multiplies a time series by the quadratic phase; Requires KTTimeSeriesData and KTSliceHeader; Adds nothing
+
+     Signals:
+     - "time-series": void (Nymph::KTDataPtr) -- Emitted upon successful time series processing; Guarantees KTTimeSeriesData and KTSliceHeader
+    */
+
+    class KTQuadraticPhaseShift : public Nymph::KTProcessor
+    {
+        public:
+            KTQuadraticPhaseShift(const std::string& name = "quadratic-phase");
+            virtual ~KTQuadraticPhaseShift();
+
+            bool Configure(const scarab::param_node* node);
+
+            double GetSlope() const;
+            void SetSlope(double q);
+
+        private:
+            double fSlope;
+
+
+        public:
+            bool AssignPhase( KTProcessedTrackData& trackData );
+            bool ProcessTimeSeries( KTTimeSeriesData& tsData, KTSliceHeader& slice );
+
+            //***************
+            // Signals
+            //***************
+
+        private:
+            Nymph::KTSignalData fTSSignal;
+
+            //***************
+            // Slots
+            //***************
+
+        private:
+            void SlotFunctionTimeSeries( Nymph::KTDataPtr data );
+            void SlotFunctionTrack( Nymph::KTDataPtr data );
+
+    };
+
+    inline double KTQuadraticPhaseShift::GetSlope() const
+    {
+        return fSlope;
+    }
+
+    inline void KTQuadraticPhaseShift::SetSlope(double q)
+    {
+        fSlope = q;
+        return;
+    }
+
+    void KTQuadraticPhaseShift::SlotFunctionTimeSeries( Nymph::KTDataPtr data )
+    {
+        // Standard data slot pattern:
+        // Check to ensure that the required data types are present
+        if (! data->Has< KTTimeSeriesData >())
+        {
+            KTERROR(avlog_hh, "Data not found with type < KTTimeSeriesData >!");
+            return;
+        }
+        if (! data->Has< KTSliceHeader >())
+        {
+            KTERROR(avlog_hh, "Data not found with type < KTSliceHeader >!");
+            return;
+        }
+
+        // Use a copy of the original data pointer rather than alter it
+        Nymph::KTDataPtr newData = data;
+
+        // Call function
+        if(! ProcessTimeSeries( newData->Of< KTTimeSeriesData >(), newData->Of< KTSliceHeader >() ))
+        {
+            KTERROR(avlog_hh, "Something went wrong while analyzing time series data!");
+            return;
+        }
+
+        // Emit signal
+        fTSSignal( newData );
+    
+        return;
+    }
+
+    void KTQuadraticPhaseShift::SlotFunctionTrack( Nymph::KTDataPtr data )
+    {
+        // Standard data slot pattern:
+        // Check to ensure that the required data types are present
+        if (! data->Has< KTProcessedTrackData >())
+        {
+            KTERROR(avlog_hh, "Data not found with type < KTProcessedTrackData >!");
+            return;
+        }
+
+        if(! AssignPhase( data->Of< KTProcessedTrackData >() ))
+        {
+            KTERROR(avlog_hh, "Something went wrong setting the phase slope!");
+            return;
+        }
+    }
+}
+
+#endif /* KTQUADRATICPHASESHIFT_HH_ */
