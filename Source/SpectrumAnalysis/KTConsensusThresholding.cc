6a197a80f401230158d54bdab968de663e17e956
Noah Oblath <nsoblath@gmail.com>
2017-08-21 18:01:09 -0700

    Fixing delta-t and delta-f calculation for vertical lines in the by-radius version of CT.

--- Source/SpectrumAnalysis/KTConsensusThresholding.cc
+++ Source/SpectrumAnalysis/KTConsensusThresholding.cc
@@ -99,7 +99,7 @@ namespace Katydid
         double timeDelta, frequencyDelta;
         for (unsigned iPoint = 0; iPoint < nPoints; ++iPoint)
         {
-            KTDEBUG(ctlog, "checking point (" << iPoint + 1 << "/" << nPoints << ")");
+            KTDEBUG(ctlog, "checking point (" << iPoint << "/" << nPoints << "); coords: (" << setOfPoints[iPoint].fCoords[0] << ", " << setOfPoints[iPoint].fCoords[1] << ")");
             //FindDeltasFirstNeighbor(kdTree, setOfPoints, iPoint, timeDelta, frequencyDelta);
             (this->*fFindDeltasPtr)(kdTree, setOfPoints, iPoint, timeDelta, frequencyDelta);
 
@@ -115,11 +115,14 @@ namespace Katydid
                 double k = 1.0;
                 while (closeEnough)
                 {
+#ifndef NDEBUG
                     test_pt[0] = setOfPoints[iPoint].fCoords[0] + k * timeDelta;
                     test_pt[1] = setOfPoints[iPoint].fCoords[1] + k * frequencyDelta;
-                    KTDEBUG(ctlog, "x = "<< test_pt[0] <<"\t" << "y = "<< test_pt[1]);
+#endif
+                    KTDEBUG(ctlog, "x = "<< test_pt[0] << " = " << setOfPoints[iPoint].fCoords[0] << " + " << k << " * " << timeDelta <<"\t" << "y = "<< test_pt[1] << " = " << setOfPoints[iPoint].fCoords[1] << " + " << k << " * " << frequencyDelta );
 
                     kdTree->RadiusSearch(test_pt, fMembershipRadius, indicesDist, nanoflann::SearchParams(32, 0, true));
+                    KTDEBUG(ctlog, "Number of points in radius search: " << indicesDist.size());
                     if (indicesDist.size() > 0)
                     {
                         k += 1.0;
@@ -136,10 +139,14 @@ namespace Katydid
                 k = -1.0;
                 while (closeEnough)
                 {
+#ifndef NDEBUG
                     test_pt[0] = setOfPoints[iPoint].fCoords[0] + k * timeDelta;
                     test_pt[1] = setOfPoints[iPoint].fCoords[1] + k * frequencyDelta;
-                    KTDEBUG(ctlog, "x = "<< test_pt[0] <<"\t" << "y = "<< test_pt[1]);
+#endif
+                    KTDEBUG(ctlog, "x = "<< test_pt[0] << " = " << setOfPoints[iPoint].fCoords[0] << " + " << k << " * " << timeDelta <<"\t" << "y = "<< test_pt[1] << " = " << setOfPoints[iPoint].fCoords[1] << " + " << k << " * " << frequencyDelta );
+
                     kdTree->RadiusSearch(test_pt, fMembershipRadius, indicesDist, nanoflann::SearchParams(32, 0, true));
+                    KTDEBUG(ctlog, "Number of points in radius search: " << indicesDist.size());
                     if (indicesDist.size() > 0)
                     {
                         k -= 1.0;
@@ -172,30 +179,68 @@ namespace Katydid
     {
         // Find the neighbors within a circle around a given point (#pid), and use them to extract a slope/trend to be returned
         KTTreeIndex< double >::Neighbors ne = kdTree->NearestNeighborsByRadius(pid, fMembershipRadius);
+        //KTWARN(ctlog, "Find neighbors in radius around pid = " << pid);
+        //std::stringstream neistr;
+        //for (unsigned iNe = 0; iNe < ne.size(); ++iNe)
+        //{
+        //    neistr << ne[iNe] << ", ";
+        //}
+        //KTWARN(ctlog, "Neighbors: " << neistr.str());
         unsigned nNeighbors = ne.size();
 
-        if (nNeighbors > 1)
+        if (nNeighbors > 2)
         {
-            double sumX = 0 , sumY = 0, sumX2 = 0, sumXY = 0;
+            double xCoord = 0, yCoord = 0;
+            double sumX = 0, sumY = 0, sumX2 = 0, sumXY = 0;
+            double minY = std::numeric_limits< double >::max(), maxY = std::numeric_limits< double >::min();
             for (unsigned iNe = 0; iNe < nNeighbors; ++iNe)
             {
-                sumX += setOfPoints[ne[iNe]].fCoords[0];
-                sumY += setOfPoints[ne[iNe]].fCoords[1];
-                sumX2 += setOfPoints[ne[iNe]].fCoords[0] * setOfPoints[ne[iNe]].fCoords[0];
-                sumXY += setOfPoints[ne[iNe]].fCoords[0] * setOfPoints[ne[iNe]].fCoords[1];
+                xCoord = setOfPoints[ne[iNe]].fCoords[0];
+                yCoord = setOfPoints[ne[iNe]].fCoords[1];
+                //KTWARN(ctlog, "pidNeighbor: " <<  pidNeighbor << "   xCoord = " << xCoord << ",   yCoord = " << yCoord);
+                sumX += xCoord;
+                //KTWARN(ctlog, "sumX: " << sumX << " -- added " << xCoord);
+                sumY += yCoord;
+                //KTWARN(ctlog, "sumY: " << sumY << " -- added " << yCoord);
+                sumX2 += xCoord * xCoord;
+                //KTWARN(ctlog, "sumX2: " << sumX2 << " -- added " << xCoord * xCoord);
+                sumXY += xCoord * yCoord;
+                //KTWARN(ctlog, "sumXY: " << sumXY << " -- added " << xCoord * yCoord);
+                if (yCoord > maxY) maxY = yCoord;
+                if (yCoord < minY) minY = yCoord;
+                //KTWARN(ctlog, "minY: " << minY << "    maxY: " << maxY);
             }
-            double slope = (sumXY - sumX * sumY / (double)nNeighbors) / (sumX2 - sumX * sumX / (double)nNeighbors);
-            deltaTime = sqrt(fMembershipRadius * fMembershipRadius / (1. + slope*slope));
-            deltaFreq = slope * deltaTime;
+            //KTWARN(ctlog, "### sumXY = " << sumXY << "   sumX = " << sumX << "   sumY = " << sumY << "   sumX2 = " << sumX2 << "   double(nNeighbors) = " << double(nNeighbors));
+            //KTWARN(ctlog, "### sumXY = " << sumXY << "   sumX * sumY = " << sumX * sumY << "   (sumXY - sumX * sumY / double(nNeighbors) = " << (sumXY - sumX * sumY / double(nNeighbors)));
+            //KTWARN(ctlog, "### sumX2 = " << sumX2 << "   sumX * sumX = " << sumX * sumX << "   (sumX2 - sumX * sumX / double(nNeighbors) = " << (sumX2 - sumX * sumX / double(nNeighbors)));
+            //KTWARN(ctlog, "### (sumXY - sumX * sumY / double(nNeighbors)) / (sumX2 - sumX * sumX / double(nNeighbors)) = " << (sumXY - sumX * sumY / double(nNeighbors)) / (sumX2 - sumX * sumX / double(nNeighbors)));
+            if (double(nNeighbors) * sumX2 != sumX * sumX)
+            {
+                double slope = (sumXY - sumX * sumY / double(nNeighbors)) / (sumX2 - sumX * sumX / double(nNeighbors));
+                //KTWARN(ctlog, "sumX2 - sumX*sumX = " << sumX2 - sumX*sumX << "    double(nNeighbors) = " << double(nNeighbors));
+                // Pythagorean theorem to get deltaTime: radius^2 = delta-t^2 + delta-f^2
+                deltaTime = sqrt(fMembershipRadius * fMembershipRadius / (1. + slope*slope));
+                deltaFreq = slope * deltaTime;
+                //KTWARN(ctlog, "slope = " << slope << "    deltaTime = " << deltaTime << "    deltaFreq = " << deltaFreq);
+            }
+            else
+            {
+                deltaTime = 0.;
+                deltaFreq = maxY - minY;
+                //KTWARN(ctlog, "vertical    deltaTime = " << deltaTime << "    deltaFreq = " << deltaFreq);
+            }
         }
-        else if (nNeighbors == 1)
+        else if (nNeighbors == 2)
         {
             // in this case there isn't a group of neighbors to use to get a trend, so we'll use the same method as in FindDeltasNearestNeighbor
-            deltaTime = setOfPoints[ne[0]].fCoords[0] - setOfPoints[pid].fCoords[0];
-            deltaFreq = setOfPoints[ne[0]].fCoords[1] - setOfPoints[pid].fCoords[1];
+            deltaTime = setOfPoints[ne[1]].fCoords[0] - setOfPoints[ne[0]].fCoords[0];
+            deltaFreq = setOfPoints[ne[1]].fCoords[1] - setOfPoints[ne[0]].fCoords[1];
+            //KTWARN(ctlog, "(2 points) -- deltaTime = " << deltaTime << "    deltaFreq = " << deltaFreq);
         }
-        else // nNeighbors = 0
+        else // nNeighbors = 0 or 1
         {
+            // 0 should never happen
+            // 1 means nothing else is within the radius
             deltaTime = 0;
             deltaFreq = 0;
         }
