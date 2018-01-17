831d0e6b4a11c5437d2da1b6818f7bcd77b55e0e
Noah Oblath <nsoblath@gmail.com>
2018-01-16 16:10:05 -0800

    Doc build test: removed all directories in Source except Utility

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -52,7 +52,7 @@ ms = make_source.site_builder()
 #   1: directory in which to make the documentation (recommendation: leave as '.')
 #   2: list of directories in which to look for source files
 #   3: list of directories to exclude from the search for source files
-ms.build('.', ['../Source'], ['../Source/Time/Monarch'])
+ms.build('.', ['../Source'], ['../Source/Time/Monarch'], ['../Source/Evaluation'], ['../Source/Simulation'], ['../Source/Data'], ['../Source/EventAnalysis'], ['../Source/Executables'], ['../Source/IO'], ['../Source/SpectrumAnalysis'], ['../Source/Time'], ['../Source/Transform'])
 call(['echo', '====== make source complete ====='])
 
 call(['cat', 'index.rst'])
