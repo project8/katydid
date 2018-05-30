e14cf292cdc988e4dbe86ef28d841bc78d6da8b3
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2018-05-30 08:30:00 -0700

    Correcting paths

--- continuous_integration/build.sh
+++ continuous_integration/build.sh
@@ -3,3 +3,11 @@ cd build
 cmake ..
 make -j3 
 make install
+
+export PATH=$(pwd)/bin:${PATH}
+export INCLUDE_PATH=$(pwd)/include:${INCLUDE_PATH}
+export LIBRARY_PATH=$(pwd)/lib:${LIBRARY_PATH}
+export LD_LIBRARY_PATH=$(pwd)/lib:${LD_LIBRARY_PATH}
+export LIBDIR=$(pwd)/lib:${LIBDIR}
+
+cd ..
