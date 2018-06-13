3210bb81e284593149215f029c5b456a9ca27d88
cclaessens <claessen@uni-mainz.de>
2018-06-13 11:39:45 +0200

    added classes for sequential line nup cuts

---
+++ Source/EventAnalysis/KTSequentialLineNUPCut.cc
@@ -0,0 +1,78 @@
+/*
+ * KTSequentialLineNUPCut.cc
+ *
+ *  Created on: June 13, 2018
+ *      Author: C. Claessens
+ */
+
+#include "KTSequentialLineNUPCut.hh"
+#include "KTSequentialLineData.hh"
+
+#include "KTLogger.hh"
+
+namespace Katydid
+{
+    KTLOGGER(sqlcutlog, "KTSequentialLineNUPCut");
+
+    const std::string KTSequentialLineNUPCut::Result::sName = "seq-line-nup-cut";
+
+    KT_REGISTER_CUT(KTSequentialLineNUPCut);
+
+    KTSequentialLineNUPCut::KTSequentialLineNUPCut(const std::string& name) :
+         KTCutOneArg(name),
+         fMinTotalNUP(0.0),
+         fMinAverageNUP(0.0),
+         fWideOrNarrowLine("wide")
+    {}
+
+    KTSequentialLineNUPCut::~KTSequentialLineNUPCut()
+    {}
+
+    bool KTSequentialLineNUPCut::Configure(const scarab::param_node* node)
+    {
+        if (node == NULL) return true;
+
+        SetMinTotalNUP( node->get_value< double >( "min-total-nup", GetMinTotalNUP() ) );
+        SetMinAverageNUP( node->get_value< double >( "min-average-nup", GetMinAverageNUP() ) );
+        SetWideOrNarrowLine( node->get_value("wide-or-narrow", GetWideOrNarrowLine() ) );
+
+        return true;
+    }
+
+    bool KTSequentialLineNUPCut::Apply( Nymph::KTData& data, KTSequentialLineData& seqLineData )
+    {
+        bool isCut = false;
+        seqLineData.CalculateTotalNUP();
+
+        if (fWideOrNarrowLine == "narrow")
+        {
+            if( seqLineData.GetTotalNUP() < GetMinTotalNUP() )
+            {
+                isCut = true;
+            }
+            if( seqLineData.GetTotalNUP() / ( seqLineData.GetEndTimeInRunC() - seqLineData.GetStartTimeInRunC() ) < GetMinAverageNUP() )
+            {
+                isCut = true;
+            }
+        }
+        else if (fWideOrNarrowLine == "wide")
+        {
+            if( seqLineData.GetTotalWideNUP() < GetMinTotalNUP() )
+            {
+                isCut = true;
+            }
+            if( seqLineData.GetTotalWideNUP() / ( seqLineData.GetEndTimeInRunC() - seqLineData.GetStartTimeInRunC() ) < GetMinAverageNUP() )
+            {
+                isCut = true;
+            }
+        }
+        else
+        {
+            KTERROR(sqlcutlog, "Invalid string for fWideOrNarrowLine: "<< fWideOrNarrowLine);
+        }
+        data.GetCutStatus().AddCutResult< KTSequentialLineNUPCut::Result >(isCut);
+
+        return isCut;
+    }
+
+} // namespace Katydid
---
+++ Source/EventAnalysis/KTSequentialLineNUPCut.hh
@@ -0,0 +1,59 @@
+/*
+ * KTSequentialLineNUPCut.hh
+ *
+ *  Created on: June 13, 2018
+ *      Author: C. Claessens
+ */
+
+#ifndef KTSEQUENTIALLINENUPCUT_HH_
+#define KTSEQUENTIALLINENUPCUT_HH_
+
+#include "KTCut.hh"
+
+namespace Katydid
+{
+
+    class KTSequentialLineData;
+
+    /*
+     @class KTSequentialLineNUPCut
+     @author C. Claessens
+
+     @brief Cuts on total and average NUP of KTSequentialLineData
+
+     @details
+     KTSequentialLineData objects must have an NUP and average NUP above a set value to pass the cut
+
+     Configuration name: "sq-line-nup-cut"
+
+     Available configuration values:
+     - "min-total-nup": double -- minimum summed NUP to accept
+     - "min- average-nup": double -- minimum average NUP to accept
+    */
+
+    class KTSequentialLineNUPCut : public Nymph::KTCutOneArg< KTSequentialLineData >
+    {
+
+    public:
+        struct Result : Nymph::KTExtensibleCutResult< Result >
+        {
+            static const std::string sName;
+        };
+
+    public:
+        KTSequentialLineNUPCut(const std::string& name = "seq-line-nup-cut");
+        ~KTSequentialLineNUPCut();
+
+        bool Configure(const scarab::param_node* node);
+
+        MEMBERVARIABLE(double, MinTotalNUP);
+        MEMBERVARIABLE(double, MinAverageNUP);
+        MEMBERVARIABLE(std::string, WideOrNarrowLine);
+
+    public:
+        bool Apply(Nymph::KTData& data, KTSequentialLineData& seqLineData);
+
+    };
+} // namespace Katydid
+
+#endif /* KTSequentialLineNUPCut_HH_ */
