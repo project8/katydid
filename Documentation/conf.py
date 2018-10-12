00340398b8c7828261c3ddaa2c64bf64d248ba36
Noah Oblath <nsoblath@gmail.com>
2018-10-12 15:15:34 -0700

    Removed commented-out text from conf.py

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -52,16 +52,6 @@ call(['doxygen', '../Nymph/Scarab/documentation/cpp/Doxyfile'])
 call(['mv', './user_doxygen_out/html', './_static'])
 
 
-#html_theme = 'sphinxcontrib.contentui'
-#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-#if on_rtd:
-#  html_theme = 'default'
-#else:
-#  import sphinx_rtd_theme
-#  html_theme = "sphinx_rtd_theme"
-#  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
