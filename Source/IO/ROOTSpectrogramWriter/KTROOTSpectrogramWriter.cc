b44cd2f8e588df7400256e387216b490aa7f29a3
Evan Zayas <ezayas@mit.edu>
2019-04-30 10:06:52 -0400

    fixed an unrelated stupid

--- Source/IO/ROOTSpectrogramWriter/KTROOTSpectrogramWriter.cc
+++ Source/IO/ROOTSpectrogramWriter/KTROOTSpectrogramWriter.cc
@@ -216,6 +216,9 @@ namespace Katydid
                     dataBundle.fHistCount = 0;
                 }
 
+                dataBundle.fTimeAxisMin = timeInRun;
+                KTPROG( publog, "fTimeAxisMin = " << dataBundle.fTimeAxisMin );
+
                 // calculate the properties of the time axis
                 if (mode == KTROOTSpectrogramWriter::kSingle)
                 {
@@ -225,7 +228,7 @@ namespace Katydid
                 {
                     dataBundle.fNTimeBins = fWriter->GetNTimeBins();
                 }
-                dataBundle.fTimeAxisMin = timeInRun;
+
                 dataBundle.fTimeAxisMax = dataBundle.fTimeAxisMin + sliceLength * (double)dataBundle.fNTimeBins;
                 KTWARN(publog, dataBundle.fNTimeBins << "  " << dataBundle.fTimeAxisMin << "  " << dataBundle.fTimeAxisMax);
 
