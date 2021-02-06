c5ee7218667d16f130dc27ba99a5c1df6bc15913
Florian Thomas <flthomas@students.uni-mainz.de>
2021-02-06 03:36:24 +0100

    Modify TestVectorComplex
    Extend the validations to include tests for each function.

--- Source/Executables/Validation/TestVectorComplex.cc
+++ Source/Executables/Validation/TestVectorComplex.cc
@@ -8,24 +8,54 @@
 #include "KTPhysicalArrayComplex.hh"
 
 using namespace Katydid;
+using Complex = std::complex<double>;
+using ArrayType = KTPhysicalArray< 1, Complex >;
+
+template <typename f_type>
+void operation(ArrayType& v1, ArrayType& v2, f_type f)
+{
+    f(v1, v2);
+    std::cout << v1 << '\n' << std::endl;
+    std::cout << v2 << '\n' << std::endl;
+}
 
 int main ()
 {
-    using complex = std::complex<double>;
+
     
     // 1D physical array test
 
-    KTPhysicalArray< 1, complex > v1(3, 0., 1.), v2(3, 0., 1.);
+    ArrayType v1(3, 0., 1.), v2(3, 0., 1.);
+    
+    operation( v1, v2, [](ArrayType& v1, ArrayType& v2){
+                for (unsigned i = 0; i < std::min(v1.size(), v2.size()); i++)
+                {
+                    v1(i) = v2(i) = Complex{static_cast<double>(i)+1, static_cast<double>(i)-1};
+                }
+        } );
 
-    for (unsigned i = 0; i < std::min(v1.size(), v2.size()); i++)
+    operation( v1, v2, [](ArrayType& v1, ArrayType& v2){ std::cout << v1 + v2 << '\n' << std::endl;});
+    
+    operation( v1, v2, [](ArrayType& v1, ArrayType& v2){ std::cout << v1 - v2 << '\n' << std::endl;});
+    
+    operation( v1, v2, [](ArrayType& v1, ArrayType& v2){ std::cout << v1 * v2 << '\n' << std::endl;});
+    
+    operation( v1, v2, [](ArrayType& v1, ArrayType& v2){ std::cout << v1 / v2 << '\n' << std::endl;});
+    
+    operation( v1, v2, [](ArrayType& v1, ArrayType& v2){ v1 += v2;});
+    
+    operation( v1, v2, [](ArrayType& v1, ArrayType& v2){ v1 -= v2;});
+    
+    operation( v1, v2, [](ArrayType& v1, ArrayType& v2){ v1 *= v2;});
+    
+    operation( v1, v2, [](ArrayType& v1, ArrayType& v2){ v1 /= v2;});
+    
+    for (auto && a: v1)
     {
-        v1(i) = v2(i) = i+1;
+        std::cout << a << " ";
     }
 
-    std::cout << v1 + v2 << std::endl;
-    std::cout << v1 - v2 << std::endl;
-    v1 /= v2;
-    std::cout << v1 << std::endl;
+    std::cout << '\n';
 
     // 2D physical array test
 /*
