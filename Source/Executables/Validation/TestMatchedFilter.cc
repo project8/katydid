62b870d5d308adfb914608b692fe9635f86dab2f
Florian Thomas <flthomas@students.uni-mainz.de>
2021-05-12 21:44:58 +0200

    Update MatchedFilter test

--- Source/Executables/Validation/TestMatchedFilter.cc
+++ Source/Executables/Validation/TestMatchedFilter.cc
@@ -13,12 +13,16 @@
 
 #include "KTMatrixAggregator.hh"
 #include "KTConvertToTemplate.hh"
+#include "KTInnerProduct.hh"
+#include "KTInnerProductOptimizer.hh"
 
 #include "KTTimeSeriesData.hh"
 #include "KTTimeSeriesFFTW.hh"
 
 #include "KTAggregatedTSMatrixData.hh"
 #include "KTAggregatedTemplateMatrixData.hh"
+#include "KTInnerProductData.hh"
+#include "KTInnerProductOptimizerData.hh"
 
 #include "KTLogger.hh"
 
@@ -32,9 +36,11 @@ int main()
     // Create and setup processors
     KTMatrixAggregator tAgg {};
     KTConvertToTemplate tConvert {};
+    KTInnerProduct tInnerProd {};
+    KTInnerProductOptimizer tOpt {};
     
-    unsigned maxCols = 3;
-    unsigned nCols = 5;
+    unsigned maxCols = 5;
+    unsigned nCols = 19;
     unsigned nChannels = 3;
     unsigned nTimeBins = 4;
 
@@ -80,14 +86,38 @@ int main()
 
     // Test conversion to template
     tConvert.Convert(dataMatrix);
-
     KTAggregatedTemplateMatrixData& templateMatrix = data->Of< KTAggregatedTemplateMatrixData >();
-
     KTDEBUG(testlog, "Template Matrix: " << templateMatrix);
 
-    KTDEBUG(testlog, "Norm" << sqrt((dataMatrix.GetData()*conj(dataMatrix.GetData())).colwise().sum()));
+    tInnerProd.SetTemplates(templateMatrix);
+    tInnerProd.Multiply(dataMatrix);
 
-    KTDEBUG(testlog, " Product: " << (templateMatrix.GetData().transpose()*conj(dataMatrix.GetData())).colwise().sum());
+    KTInnerProductData& product = data->Of< KTInnerProductData >();
+    KTDEBUG(testlog, "Product matrix: " << product);
+
+    Eigen::ArrayXd norm = dataMatrix.GetData().matrix().colwise().norm();
+
+    tOpt.FindOptimum(product);
+
+    KTInnerProductOptimizerData& res = data->Of< KTInnerProductOptimizerData >();
+
+    KTDEBUG(testlog, "Arg: " << res.fMaxInds);
+    KTDEBUG(testlog, "Vals: " << res.fMaxVals);
+    KTDEBUG(testlog, "Norm: " << norm);
+
+    //checking results
+    Eigen::ArrayXi indices = Eigen::ArrayXi::LinSpaced(dataMatrix.cols(), 0, dataMatrix.cols()-1);
+
+    if(indices.matrix() == res.fMaxInds.matrix().cast<int>() &&
+    		norm.isApprox(res.fMaxVals, 1e-7))
+    {
+    	KTPROG(testlog, "Result matches!");
+    } else {
+    	KTPROG(testlog, "Result does not match!");
+    	return 1;
+    }
+
+    KTPROG(testlog, "Test passed!");
 
     return 0;
 }
