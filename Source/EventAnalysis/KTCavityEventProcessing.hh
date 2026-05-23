0dcee67b1b7c0f1905063e5afe6e34fcc502f3f6
Junior Pena <junior@Juniors-MacBook-Pro.local>
2026-05-22 20:40:02 -0400

    Added a more precise description of the KTCavityEventProcessing processor in brief and details.

--- Source/EventAnalysis/KTCavityEventProcessing.hh
+++ Source/EventAnalysis/KTCavityEventProcessing.hh
@@ -20,9 +20,14 @@ namespace Katydid
     /*
      @class KTCavityEventProcessing
      @author J. I. Pena
-     @brief Assigns initial cyclotron frequency for events from symmetric-trap CCA cavity TE011 data
-     @details
+     @brief Reconstructs axial and cyclotron frequency for all multi-peak-tracks in an event
      
+     @details
+     Iterates through all MPTs in an event by sequence ID
+     Reconstructs axial frequency by calculating frequency distance between different bands in MPTs at distinct band start and end times
+        Assumes an axially symmetric trap and cavity mode map such that sideband separation is 2*n*f_ax
+     Reconstructs cyclotron frequency by classifying MPT topology based on relative power of bands to the band in the MPT with the highest power
+         Relative power threshold parameter used 2 decide between 2 classes in the cases of 2 and 3 band MPTs
 
      Available configuration values:
      - "max-rel-power-thresh": float -- Relative power threshold which is criteria in classifying bands in MPTs
